3e08af3c4fd282faaafaa3811158151d201ae373
Ivan Nebokrai <ivan.nebokrai@columbiaroad.com>
2022-11-19 17:25:56 +0200

    add initial svelte setup + tailwind and daisyUI

---
+++ frontend/src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+	import '../app.css';
+</script>
+
+<div>
+	<h1 class="text-3xl font-medium ml-2.5 mt-14 leading-6 text-gray-500">
+		Welcome to Voting Advice Application!
+	</h1>
+	<span class="ml-4 mt-4 badge"> DaisyUI Badge </span>
+</div>
