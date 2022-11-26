b6b746bac781a6a30f2aabce13659c4e7e6147d4
Jhosimar Aguacia <corbin026@hotmail.com>
2022-11-26 20:36:18 +0200

    test: Add test framework for Strapi

---
+++ backend/vaa-strapi/config/env/test/database.js
@@ -0,0 +1,10 @@
+module.exports = ({ env }) => ({
+  connection: {
+    client: 'postgres',
+    connection: {
+      filename: env('DATABASE_FILENAME', '.tmp/test.db'),
+    },
+    useNullAsDefault: true,
+    debug: false
+  },
+});
