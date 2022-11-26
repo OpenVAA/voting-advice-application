c5d6d975380059edc7575a9c427a7b1d3c3310e1
Jhosimar Aguacia <corbin026@hotmail.com>
2022-11-26 20:36:18 +0200

    chore: Add test framework for Strapi

---
+++ backend/vaa-strapi/jest.config.js
@@ -0,0 +1,12 @@
+const config = {
+  verbose: true,
+  testPathIgnorePatterns: [
+    "/node_modules/",
+    ".tmp",
+    ".cache",
+    "dist"
+  ],
+  testEnvironment: "node"
+};
+
+module.exports = config;
