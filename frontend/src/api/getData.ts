43ed07e1c3c1abc346eb4edb08ca357e2c16e822
Johanna Laitila <johanna.maria.laitila@gmail.com>
2022-11-28 11:21:24 +0200

    feat: Type checking for frontend constants

--- frontend/src/api/getData.ts
+++ frontend/src/api/getData.ts
@@ -1,8 +1,11 @@
 // Quick test function to get data from Strapi
 // To be refactored (GraphQL in the future?)
+
+import {constants} from "../utils/constants";
+
 export const getData = async (endpoint: string): Promise<any> => {
     try{
-        const url = `${import.meta.env.VITE_BACKEND_URL}/${endpoint}`;
+        const url = `${constants.BACKEND_URL}/${endpoint}`;
         return await fetch(url).then(response => {
             return response.json();
         })
---
+++ frontend/src/utils/constants.ts
@@ -0,0 +1,3 @@
+export const constants: Record<string, string> = {
+    BACKEND_URL: import.meta.env.VITE_BACKEND_URL || ""
+}
