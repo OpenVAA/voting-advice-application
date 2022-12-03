a0aff4ab596c498782a5702a756fd251badfd04d
Jhosimar Aguacia <corbin026@hotmail.com>
2022-12-03 15:53:57 +0200

    build: update config files

--- frontend/capacitor.config.ts
+++ frontend/capacitor.config.ts
@@ -1,16 +1,16 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-	appId: 'com.voting.advice.app',
-	appName: 'voting-advice-application',
-	webDir: 'build',
-	bundledWebRuntime: false,
+  appId: 'com.voting.advice.app',
+  appName: 'voting-advice-application',
+  webDir: 'build',
+  bundledWebRuntime: false,
 
-	// change the url value to the IP address that your workstation is available through internet.
-	server: {
-		url: 'http://192.168.101.143:5173',
-		cleartext: true
-	}
+  // change the url value to the IP address that your workstation is available through internet.
+  server: {
+    url: 'http://192.168.101.143:5173',
+    cleartext: true
+  }
 };
 
 export default config;
