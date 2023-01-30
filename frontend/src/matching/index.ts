c1aad23ab7169c9cd73ea12dc94d96e2123dec6b
kallelongjuhani <kalle.jarvenpaa@gmail.com>
2023-01-30 18:32:30 +0200

    refactor: add public api exports

--- frontend/src/matching/index.ts
+++ frontend/src/matching/index.ts
@@ -1,11 +1,18 @@
 /**
- * Utility exports for items users of the matching functions might need.
+ * Utility exports for items users of the matching functions might need so that their
+ * imports are not broken if internal structure is changed.
  */
 
+export {imputeMissingValues, MissingValueBias, MissingValueDistanceMethod} from "./algorithms/imputeMissingValues";
+export {Match} from "./algorithms/match";
+export {MatchingAlgorithm, MatchingAlgorithmBase} from "./algorithms/matchingAlgorithm";
+export {MatchingSpaceProjector} from "./algorithms/matchingSpaceProjector";
+export {DistanceMetric, measureDistance} from "./algorithms/measureDistance";
+export {assertUnsignedNormalized, assertSignedNormalized, NORMALIZED_DISTANCE_EXTENT,
+    UnsignedNormalizedDistance, SignedNormalizedDistance} from "./core/distances";
 export {HasMatchableAnswers, MatchableAnswer} from "./core/hasMatchableAnswers";
-export {MatchingAlgorithmBase} from "./algorithms/matchingAlgorithm";
-export {MISSING_VALUE} from "./core/matchableValue";
+export {MatchableValue, MissingValue, MISSING_VALUE, NonmissingValue} from "./core/matchableValue";
+export {MatchingSpace} from "./core/matchingSpace";
+export {MatchingSpaceCoordinate, MatchingSpacePosition} from "./core/matchingSpacePosition";
 export {MatchableQuestion} from "./questions/matchableQuestion";
 export {MultipleChoiceQuestion} from "./questions/multipleChoiceQuestion";
-export {DistanceMetric} from "./algorithms/measureDistance";
-export {MissingValueDistanceMethod} from "./algorithms/imputeMissingValues";
