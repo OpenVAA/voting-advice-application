ed8375644c782bb66237dd33f0f4db25c405705f
Johanna Laitila <johanna.maria.laitila@gmail.com>
2023-05-02 00:01:39 +0300

    fix: Make algorithm tests run

--- frontend/vitest.config.ts
+++ frontend/vitest.config.ts
@@ -1,10 +1,10 @@
-import { defineConfig } from 'vitest/config';
-import { svelte } from '@sveltejs/vite-plugin-svelte';
+import {defineConfig} from 'vitest/config';
+import {svelte} from '@sveltejs/vite-plugin-svelte';
 
 export default defineConfig({
-  plugins: [svelte({ hot: !process.env.VITEST })],
+  plugins: [svelte({hot: !process.env.VITEST})],
   test: {
-    include: ['tests/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+    include: ['**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
     globals: true,
     environment: 'jsdom'
   }
