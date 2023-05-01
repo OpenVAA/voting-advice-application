8e2a16e34b8871d9d8a6f54e023d3eaa61f5d9a5
Johanna Laitila <johanna.maria.laitila@gmail.com>
2023-05-01 19:27:52 +0300

    fix: Add locale query to backend

--- frontend/src/utils/i18n.ts
+++ frontend/src/utils/i18n.ts
@@ -1,18 +1,29 @@
-import {browser} from '$app/environment';
-import {init, register} from 'svelte-i18n';
+import {getLocaleFromNavigator, init, register} from 'svelte-i18n';
 
 const supportedLanguages = ['en'];
-export const defaultLocale = supportedLanguages[0];
+export const defaultLocale = 'en';
+let currentLocale = defaultLocale;
+export function getCurrentLocale() {
+  return currentLocale;
+}
 
 function setInitialLocale() {
-  let initialLocale = defaultLocale;
-  if (browser) {
-    const browserLang = window.navigator.language.split('-')[0];
-    if (supportedLanguages.includes(browserLang)) {
-      initialLocale = browserLang;
-    }
+  let languageToUse = defaultLocale;
+  const languageFromNavigator = getLocaleFromNavigator();
+  if (languageFromNavigator) {
+    languageToUse = languageFromNavigator.split('-')[0];
   }
-  return initialLocale;
+  setNewLocale(languageToUse);
+  return currentLocale;
+}
+
+function setNewLocale(newLocale: string) {
+  if (supportedLanguages.includes(newLocale)) {
+    currentLocale = newLocale;
+  } else {
+    console.error('Requested locale is not supported, falling back to default');
+    currentLocale = defaultLocale;
+  }
 }
 
 register('en', () => import('../i18n/en.json'));
