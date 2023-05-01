0c8ca4699850de89faa632ab42086ea802807f30
Johanna Laitila <johanna.maria.laitila@gmail.com>
2023-05-01 23:35:26 +0300

    rebase github-actions-v1

--- backend/vaa-strapi/tests/election/index.js
+++ backend/vaa-strapi/tests/election/index.js
@@ -1,10 +1,10 @@
 const request = require('supertest');
 
-it("should return something", async () => {
+it('should return something', async () => {
   await request(strapi.server.httpServer)
-    .get("/api/election")
+    .get('/api/election')
     .expect(404) // Expect response http code 404 since the election route is not defined yet
     .then((data) => {
       expect(JSON.parse(data.text).data).toBeNull(); // expect the response text.data to be null
-    })
+    });
 });
