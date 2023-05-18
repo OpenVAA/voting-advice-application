d02facabe129026278d9c42ea9c55c1d4e95f3d4
Jhosimar Aguacia <corbin026@hotmail.com>
2023-05-18 22:09:50 +0300

    feat: Add main error page

---
+++ frontend/src/routes/+error.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  import {page} from '$app/stores';
+</script>
+
+<div class="flex h-screen flex-col justify-center">
+  <h1 class="text-center text-2xl">{$page.status}</h1>
+  <h2 class="text-center text-xl">{$page.error?.message}</h2>
+</div>
