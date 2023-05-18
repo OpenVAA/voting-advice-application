4986cd40194d6f158c286d3aa0f87a6a3022b644
Johanna Laitila <johanna.maria.laitila@gmail.com>
2023-05-18 20:52:18 +0300

    WIP Questions answering interface

---
+++ frontend/src/components/questions/LikertScaleAnsweringButtons.svelte
@@ -0,0 +1,23 @@
+<script>
+  import {_} from 'svelte-i18n';
+  import LikertScaleAnsweringButton from './LikertScaleAnsweringButton.svelte';
+  export let use5pointScale;
+  export let onClick;
+</script>
+
+<section class="flex items-center justify-center">
+  <div class="grid {use5pointScale ? 'grid-cols-5' : 'grid-cols-4'}">
+    <LikertScaleAnsweringButton
+      onClick={() => onClick(0)}
+      text={$_('questions.scale.fullyDisagree')} />
+    <LikertScaleAnsweringButton onClick={() => onClick(1)} text={$_('questions.scale.disagree')} />
+    {#if use5pointScale}
+      <LikertScaleAnsweringButton onClick={() => onClick(2)} text={$_('questions.scale.neutral')} />
+    {/if}
+    <LikertScaleAnsweringButton onClick={() => onClick(3)} text={$_('questions.scale.agree')} />
+    <LikertScaleAnsweringButton
+      onClick={() => onClick(4)}
+      text={$_('questions.scale.fullyAgree')} />
+  </div>
+  <div class="absolute -z-50 m-auto h-1 bg-secondary max-md:mb-12 max-md:w-60 md:mb-8 md:w-96" />
+</section>
--- frontend/src/routes/questions/+page.svelte
+++ frontend/src/routes/questions/+page.svelte
@@ -4,6 +4,7 @@
   import {currentQuestion, answeredQuestions} from '../../utils/stores';
   import {getQuestion, getNumberOfQuestions} from '../../api/getQuestion';
   import {calculateCandidateCompatibilities} from '../../candidateRanking/calculateCompatibility';
+  import LikertScaleAnsweringButtons from '../../components/questions/LikertScaleAnsweringButtons.svelte';
   export let data;
 
   let currentQuestionNumber = 1;
@@ -17,8 +18,13 @@
     currentQuestionNumber = value;
   });
 
-  // Get next question from backend
-  function nextQuestion() {
+  // Store question number and answer value in a store
+  function answerQuestion(answer) {
+    answeredQuestions.update((questions) => [
+      ...questions,
+      {question: currentQuestionNumber, answer: answer}
+    ]);
+    // TODO: Placeholder for testing, remove when we have radio buttons
     if (currentQuestionNumber < getNumberOfQuestions()) {
       currentQuestion.update((n) => n + 1);
       currentQuestionText = getQuestion(currentQuestionNumber);
@@ -28,38 +34,50 @@
       });
     }
   }
-
-  // Store question number and answer value in a store
-  function answerQuestion(answer) {
-    answeredQuestions.update((questions) => [
-      ...questions,
-      {question: currentQuestionNumber, answer: answer}
-    ]);
-    nextQuestion(); // TODO: Placeholder for testing, remove when we have radio buttons
-  }
 </script>
 
-<p>{currentQuestionNumber}/{getNumberOfQuestions()}</p>
-<h2>{currentQuestionText}</h2>
-<br />
+<section class="flex h-screen flex-col">
+  <section class="bg-secondary pb-3 pt-3 max-md:px-3 md:px-10">
+    <div class="flex pb-3">
+      <a class="text-primary">{$_('questions.previous')}</a>
+      <div class="flex-auto">
+        <h2 class="flex justify-center text-xl font-bold max-md:hidden">Your opinions</h2>
+      </div>
+      <a class="text-primary">{$_('questions.skip')}</a>
+    </div>
+    <div class="flex justify-center">
+      Sample theme 1 | Sample theme 2 | Sample theme 3 | Sample theme 4
+    </div>
+  </section>
 
-<!-- TODO: Don't hardcode number of answer options in the future -->
+  <section class="grid flex-1 content-center px-3">
+    <div class="flex-auto">
+      <div class="flex justify-center pb-3">Example theme</div>
+      <div class="flex justify-center pb-8 text-center text-2xl font-bold">
+        {currentQuestionText}
+      </div>
+      <div class="flex justify-center pb-8">
+        <!--        TODO: Check question type here-->
+        <LikertScaleAnsweringButtons onClick={answerQuestion} />
+      </div>
+      <div class="flex justify-center font-semibold text-primary">
+        <a>Read More About This Issue</a>
+      </div>
+    </div>
+  </section>
 
-<button on:click={() => answerQuestion(0)} aria-label={$_('questions.scale.stronglyDisagree')}
-  >{$_('questions.scale.stronglyDisagree')}</button>
--
-<button on:click={() => answerQuestion(1)} aria-label={$_('questions.scale.disagree')}
-  >{$_('questions.scale.disagree')}</button>
--
-<button on:click={() => answerQuestion(2)} aria-label={$_('questions.scale.agree')}
-  >{$_('questions.scale.agree')}</button>
--
-<button on:click={() => answerQuestion(3)} aria-label={$_('questions.scale.stronglyAgree')}
-  >{$_('questions.scale.stronglyAgree')}</button>
-
-<hr />
-<br />
-<button on:click={() => nextQuestion()} aria-label={$_('questions.nextQuestion')}
-  >{$_('questions.nextQuestion')}</button>
-<br />
-<a href="/results">{$_('questions.goToResults')}</a>
+  <section>
+    <div
+      class="bg-secondary md:card md:float-right md:my-8 md:mr-8 md:w-96 md:overflow-clip md:drop-shadow-xl">
+      <div class="w-full rounded-full">
+        <div
+          class="rounded-full bg-primary p-0.5 leading-none"
+          style="width: {((currentQuestionNumber - 1) / getNumberOfQuestions()) * 100}%" />
+      </div>
+      <div class="card-body items-center text-center">
+        {currentQuestionNumber - 1}/{getNumberOfQuestions()} statements answered <br />
+        <a href="/results" class="font-semibold text-primary">{$_('questions.jumpToResults')}</a>
+      </div>
+    </div>
+  </section>
+</section>
