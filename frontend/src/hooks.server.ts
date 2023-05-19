957cf24ca97d1eea27ef0160d56ee13550d89d02
Jhosimar Aguacia <corbin026@hotmail.com>
2023-05-19 19:30:05 +0300

    feat: Add Docker environment variable to connect FE with BE
    The environment variable VITE_BACKEND_URL_DOCKER holds the URL of the
    backend service. This variable is needed to send server requests from
    Svelte to Strapi.

--- frontend/src/hooks.server.ts
+++ frontend/src/hooks.server.ts
@@ -1,11 +1,23 @@
 // hooks.server.ts
-import type {Handle} from '@sveltejs/kit';
+import {browser} from '$app/environment';
+import {constants} from './utils/constants';
+import type {Handle, HandleFetch} from '@sveltejs/kit';
 import {locale} from 'svelte-i18n';
 
-export const handle: Handle = async ({event, resolve}) => {
+export const handle: Handle = (async ({event, resolve}) => {
   const lang = event.request.headers.get('accept-language')?.split(',')[0];
   if (lang) {
     locale.set(lang);
   }
   return resolve(event);
-};
+}) satisfies Handle;
+
+export const handleFetch: HandleFetch = (async ({request, fetch}) => {
+  if (!browser) {
+    request = new Request(
+      request.url.replace(constants.BACKEND_URL, constants.BACKEND_URL_DOCKER),
+      request
+    );
+  }
+  return fetch(request);
+}) satisfies HandleFetch;
--- frontend/src/utils/constants.ts
+++ frontend/src/utils/constants.ts
@@ -1,4 +1,5 @@
 export const constants: Record<string, string> = {
   BACKEND_URL: import.meta.env.VITE_BACKEND_URL || '',
+  BACKEND_URL_DOCKER: import.meta.env.VITE_BACKEND_URL_DOCKER || '',
   STRAPI_TOKEN: import.meta.env.VITE_STRAPI_TOKEN || ''
 };
