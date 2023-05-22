d1d03a7c89d31b32c443d181f43a9d88851ba894
Jhosimar Aguacia <corbin026@hotmail.com>
2023-05-22 11:43:25 +0300

    feat: Add footer and header to the main +layout.svelte

--- frontend/src/routes/+layout.ts
+++ frontend/src/routes/+layout.ts
@@ -1,11 +1,27 @@
 import {browser} from '$app/environment';
-import '../utils/i18n.ts';
+import {error} from '@sveltejs/kit';
 import {locale, waitLocale} from 'svelte-i18n';
 import type {LayoutLoad} from './$types';
+import {getSingleTypeData} from '../api/getData';
 
-export const load: LayoutLoad = async () => {
+export const load: LayoutLoad = (async ({fetch, params, route, url}) => {
   if (browser) {
     locale.set(window.navigator.language);
   }
+
   await waitLocale();
-};
+  const layoutSingleType = await getSingleTypeData({url, fetch, endpoint: 'api/layout?populate=*'})
+    .then((result) => {
+      return result;
+    })
+    .catch((error) => {
+      console.error('Error in getting layout data from Strapi: ', ' - - - ', error);
+    });
+
+  if (layoutSingleType.error) {
+    console.error('layoutSingleType error', layoutSingleType.error);
+    throw error(layoutSingleType.error.status, {message: layoutSingleType.error.message});
+  }
+
+  return layoutSingleType.data;
+}) satisfies LayoutLoad;
