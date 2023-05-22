fdfda791a3a9b46b8a0eb5c141503afac7bf7647
Jhosimar Aguacia <corbin026@hotmail.com>
2023-05-22 09:57:46 +0300

    feat: Add single-type for the layout of the frontend

---
+++ backend/vaa-strapi/src/api/layout/routes/layout.ts
@@ -0,0 +1,7 @@
+/**
+ * layout router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::layout.layout');
---
+++ backend/vaa-strapi/src/api/layout/services/layout.ts
@@ -0,0 +1,7 @@
+/**
+ * layout service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::layout.layout');
