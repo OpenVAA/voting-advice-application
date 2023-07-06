d17204a581c4ed84cf084e228c718020e841c3df
Jhosimar Aguacia <corbin026@hotmail.com>
2023-07-06 20:28:49 +0300

    feat: modify root page and layout

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,8 +1,16 @@
 <div>
-  <h1 class="ml-2.5 mt-14 text-3xl font-medium leading-6 text-gray-500">
-    Welcome to Voting Advice Application!
-  </h1>
-  <a href="/questions"><span class="badge ml-4 mt-4">Answer questions</span></a> <br />
-  <a href="/candidates"><span class="badge ml-4 mt-4">View candidates</span></a> <br />
-  <a href="/parties"><span class="badge ml-4 mt-4">View parties</span></a>
+  <div class="hero">
+    <img class="w-full lg:w-10/12 xl:w-8/12 2xl:w-6/12" src="/images/hero.png" alt="" srcset="" />
+  </div>
+  <div class="flex flex-col flex-nowrap items-center gap-y-3">
+    <h1 class="text-center text-app-title">Election Compass</h1>
+    <h2 class="text-center">Colombian Parliamentary Elections 2023</h2>
+    <p class="text-center">
+      With this application you can compare candidates in the elections on 20 Dec 2023 based on
+      their opinions, parties and other data.
+    </p>
+    <a href="/navigation" class="btn">Start Finding Candidates!</a>
+    <a href="/information" class="link-hover link">Information about the elections</a>
+    <a href="/about" class="link-hover link">How does this app work</a>
+  </div>
 </div>
