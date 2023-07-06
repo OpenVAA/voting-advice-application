fd356ab6e4e76f1593e52cecb0ddf8181b76b12f
Jhosimar Aguacia <corbin026@hotmail.com>
2023-07-06 19:59:17 +0300

    feat: Group routes under voter group layout

---
+++ frontend/src/routes/(voters)/navigation/+page.svelte
@@ -0,0 +1,34 @@
+<div class="flex flex-col items-center gap-4">
+  <h1 class="ml-2.5 mt-14 text-3xl font-medium leading-6 text-gray-500">
+    Welcome to Voting Advice Application!
+  </h1>
+  <div class="grid grid-cols-1 gap-4">
+    <div class="card w-96 bg-base-100 shadow-xl">
+      <div class="card-body">
+        <h2 class="card-title">Questions</h2>
+        <p>Answer questions to find suggestions about your candidates.</p>
+        <div class="card-actions justify-end">
+          <a href="/questions" class="btn-primary btn">Answer Questions</a>
+        </div>
+      </div>
+    </div>
+    <div class="card w-96 bg-base-100 shadow-xl">
+      <div class="card-body">
+        <h2 class="card-title">Candidates</h2>
+        <p>View the list of candidates.</p>
+        <div class="card-actions justify-end">
+          <a href="/candidates" class="btn-primary btn">View Candidates</a>
+        </div>
+      </div>
+    </div>
+    <div class="card w-96 bg-base-100 shadow-xl">
+      <div class="card-body">
+        <h2 class="card-title">Parties</h2>
+        <p>View the list of parties.</p>
+        <div class="card-actions justify-end">
+          <a href="/parties" class="btn-primary btn">View Parties</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
