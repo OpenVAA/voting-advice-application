1d57dcc0393be386498c3c863645cb349868d402
Jhosimar Aguacia <corbin026@hotmail.com>
2023-07-06 09:01:24 +0300

    refactor: Update getData for route/candidates/[slug]+page.ts

--- frontend/src/routes/candidates/[slug]/+page.ts
+++ frontend/src/routes/candidates/[slug]/+page.ts
@@ -2,27 +2,22 @@ import {getData} from '../../../api/getData';
 import {error} from '@sveltejs/kit';
 import type {LoadEvent} from '@sveltejs/kit';
 
-export async function load({params}: LoadEvent) {
+export async function load({route, url, fetch, params}: LoadEvent) {
   //TODO: Check if we use Svelte object id or some predefined schema for getting candidate from the backend
   const id = Number(params.slug);
   if (!isNaN(id)) {
-    const candidates = await getData(
-      `api/candidates/${id}`,
-      new URLSearchParams({populate: '*'})
-    ).then((result) => {
+    const candidates = await getData({fetch, endpoint: `api/candidates/${id}`}).then((result) => {
       if (result?.data?.attributes) return result.data.attributes;
       if (result?.error?.status === 404) {
         throw error(404, 'Candidate not found');
       }
     });
-    const themes = await getData('api/categories', new URLSearchParams({populate: '*'})).then(
-      (result) => {
-        if (result?.data) return result.data;
-        if (result?.error?.status === 404) {
-          throw error(404, 'Themes not found');
-        }
+    const themes = await getData({fetch, endpoint: 'api/categories'}).then((result) => {
+      if (result?.data) return result.data;
+      if (result?.error?.status === 404) {
+        throw error(404, 'Themes not found');
       }
-    );
+    });
     candidates.themes = themes;
     return candidates;
   } else {
