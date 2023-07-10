60010a94b588b6ff87eb42ce05835d68d0c67965
Jhosimar Aguacia <corbin026@hotmail.com>
2023-07-10 09:54:24 +0300

    refactor: change DisplayName in Strapi

--- backend/vaa-strapi/src/api/election-app-label/controllers/election-app-label.ts
+++ backend/vaa-strapi/src/api/election-app-label/controllers/election-app-label.ts
@@ -2,6 +2,6 @@
  * election-app-label controller
  */
 
-import { factories } from '@strapi/strapi'
+import { factories } from '@strapi/strapi';
 
 export default factories.createCoreController('api::election-app-label.election-app-label');
