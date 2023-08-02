df8862b4ff27b552f4b09cb3d4e5c6d59091c044
Jhosimar Aguacia <corbin026@hotmail.com>
2023-08-02 19:57:02 +0300

    feat: add scoreGauge to the CandidateDetailCard

--- frontend/src/lib/components/CandidateDetailsCard.svelte
+++ frontend/src/lib/components/CandidateDetailsCard.svelte
@@ -1,9 +1,12 @@
 <script lang="ts">
+  import {_} from 'svelte-i18n';
+  import type {CompatibilityScore} from '$lib/types/compatibilityScore.type';
   import Tabs from '$lib/shared/Tabs.svelte';
   import CandidateBasicInfo from '$lib/components/CandidateBasicInfo.svelte';
   import CandidateOpinions from '$lib/components/CandidateOpinions.svelte';
-  import ScoreGauge, {ScoreGaugeType} from '$lib/components/scoreGauge';
+  import {ScoreGauge} from '$lib/components/scoreGauge/index';
   export let candidate;
+  export let compatibilityScore: CompatibilityScore = {};
 
   let tabs = ['Basic info', 'Opinions'];
   let activeItem = tabs[0];
@@ -21,6 +24,16 @@
       <span class="badge-lg bg-default-party p-1 text-white">{candidate.electionListShort}</span>
     </p>
   </div>
+  <!-- {#} -->
+  {#if compatibilityScore?.policyTopics}
+    <div
+      class="mx-4 mb-6 grid grid-flow-row grid-cols-2 gap-x-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6">
+      {#each compatibilityScore.policyTopics as policyTopic}
+        <!-- TODO: replace the progressBarColor color with the color of the party -->
+        <ScoreGauge score={policyTopic.score} shape="radial" label={policyTopic.name} />
+      {/each}
+    </div>
+  {/if}
   <Tabs on:changeTab={handleChangeTab} {tabs} {activeItem} />
   {#if tabs[0] === activeItem}
     <CandidateBasicInfo {candidate} />
