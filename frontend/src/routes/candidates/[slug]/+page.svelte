ea82ba6a3ac31cb2c8dd525d9c935e62619a7ac0
Jhosimar Aguacia <corbin026@hotmail.com>
2023-08-02 20:00:31 +0300

    feat: add compatibilityScore to candidate/[slug] page

--- frontend/src/routes/candidates/[slug]/+page.svelte
+++ frontend/src/routes/candidates/[slug]/+page.svelte
@@ -1,14 +1,18 @@
 <script lang="ts">
   import {_} from 'svelte-i18n';
+  import {candidateRankings} from '$lib/utils/stores';
   import {GetFullNameInOrder} from '$lib/utils/internationalisation';
   import CandidateDetailsCard from '$lib/components/CandidateDetailsCard.svelte';
+  import type {CompatibilityScore} from '$types/compatibilityScore.type';
 
   export let data;
 
   let party = data?.party?.data;
+  let compatibilityScore: CompatibilityScore;
 
   // TODO: create a more dynamic way to create the candidate object
   const candidate = {
+    id: data.id,
     age: 35,
     name: GetFullNameInOrder(data.firstName, data.lastName),
     electionListShort: party.attributes.partyAbbreviation,
@@ -17,8 +21,12 @@
     motherTongues: data?.motherTongues?.data.map((item) => item.attributes.language),
     themes: data?.themes.map((item) => item.attributes.name)
   };
+
+  candidateRankings.subscribe((value) => {
+    compatibilityScore = value.find((score) => score.id === candidate.id);
+  });
 </script>
 
 {#if data}
-  <CandidateDetailsCard {candidate} />
+  <CandidateDetailsCard {candidate} {compatibilityScore} />
 {/if}
