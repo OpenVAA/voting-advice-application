08bc906c1fad1bb0ae593781d905a793075f886f
kallelongjuhani <kalle.jarvenpaa@gmail.com>
2023-08-04 11:18:08 +0300

    chore: updated utility exports file

--- frontend/src/matching/index.ts
+++ frontend/src/matching/index.ts
@@ -2,15 +2,16 @@
  * Utility exports for items users of the matching functions might need so that their
  * imports are not broken if internal structure is changed.
  */
-
+import {createSubspace} from './algorithms/createSubspace';
 import {
   imputeMissingValues,
   MissingValueBias,
   MissingValueDistanceMethod
 } from './algorithms/imputeMissingValues';
-import {Match} from './algorithms/match';
+import type {MissingValueImputationOptions} from './algorithms/imputeMissingValues';
+import {Match, MatchBase, SubMatch} from './algorithms/match';
 import {MatchingAlgorithmBase} from './algorithms/matchingAlgorithm';
-import type {MatchingAlgorithm} from './algorithms/matchingAlgorithm';
+import type {MatchingAlgorithm, MatchingOptions} from './algorithms/matchingAlgorithm';
 import type {MatchingSpaceProjector} from './algorithms/matchingSpaceProjector';
 import {DistanceMetric, measureDistance} from './algorithms/measureDistance';
 import {
@@ -25,33 +26,40 @@ import type {MatchableValue, MissingValue, NonmissingValue} from './core/matchab
 import {MatchingSpace} from './core/matchingSpace';
 import {MatchingSpacePosition} from './core/matchingSpacePosition';
 import type {MatchingSpaceCoordinate} from './core/matchingSpacePosition';
+import type {HasMatchableQuestions} from './questions/hasMatchableQuestions';
 import {MatchableQuestion} from './questions/matchableQuestion';
 import {MultipleChoiceQuestion} from './questions/multipleChoiceQuestion';
 
 export {
+  assertSignedNormalized,
+  assertUnsignedNormalized,
+  createSubspace,
+  DistanceMetric,
+  HasMatchableAnswers,
+  HasMatchableQuestions,
   imputeMissingValues,
+  Match,
+  MatchableAnswer,
+  MatchableQuestion,
+  MatchableValue,
+  MatchBase,
+  MatchingAlgorithm,
+  MatchingAlgorithmBase,
+  MatchingOptions,
+  MatchingSpace,
+  MatchingSpaceCoordinate,
+  MatchingSpacePosition,
+  MatchingSpaceProjector,
+  measureDistance,
+  MISSING_VALUE,
+  MissingValue,
   MissingValueBias,
   MissingValueDistanceMethod,
-  Match,
-  MatchingAlgorithmBase,
-  MatchingAlgorithm,
-  MatchingSpaceProjector,
-  DistanceMetric,
-  measureDistance,
-  assertUnsignedNormalized,
-  assertSignedNormalized,
-  NORMALIZED_DISTANCE_EXTENT,
-  UnsignedNormalizedDistance,
-  SignedNormalizedDistance,
-  HasMatchableAnswers,
-  MatchableAnswer,
-  MISSING_VALUE,
-  MatchableValue,
-  MissingValue,
+  MissingValueImputationOptions,
+  MultipleChoiceQuestion,
   NonmissingValue,
-  MatchingSpace,
-  MatchingSpacePosition,
-  MatchingSpaceCoordinate,
-  MatchableQuestion,
-  MultipleChoiceQuestion
+  NORMALIZED_DISTANCE_EXTENT,
+  SignedNormalizedDistance,
+  SubMatch,
+  UnsignedNormalizedDistance
 };
