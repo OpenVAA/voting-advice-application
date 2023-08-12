bc395eca0f3dce74f1db290c8ae906b9b77f8248
Jhosimar Aguacia <corbin026@hotmail.com>
2023-08-12 18:30:00 +0300

    refactor: add new relationships to category

---
+++ backend/vaa-strapi/src/api/question-category/routes/question-category.ts
@@ -0,0 +1,7 @@
+/**
+ * question-category router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::question-category.question-category');
---
+++ backend/vaa-strapi/src/api/question-category/services/question-category.ts
@@ -0,0 +1,7 @@
+/**
+ * question-category service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::question-category.question-category');
