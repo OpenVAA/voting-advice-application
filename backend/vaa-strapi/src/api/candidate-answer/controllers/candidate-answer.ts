bd597172ad8ccf23f19f6a010b6ed01907100654
Jhosimar Aguacia <corbin026@hotmail.com>
2023-08-12 18:31:23 +0300

    refactor: delete candidate-answer object

--- backend/vaa-strapi/src/api/candidate-answer/controllers/candidate-answer.ts
+++
@@ -1,7 +0,0 @@
-/**
- * candidate-answer controller
- */
-
-import { factories } from '@strapi/strapi'
-
-export default factories.createCoreController('api::candidate-answer.candidate-answer');
--- backend/vaa-strapi/src/api/candidate-answer/services/candidate-answer.ts
+++
@@ -1,7 +0,0 @@
-/**
- * candidate-answer service
- */
-
-import { factories } from '@strapi/strapi';
-
-export default factories.createCoreService('api::candidate-answer.candidate-answer');
