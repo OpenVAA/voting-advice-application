46d8d50f6699143375fec7ff3a28267106e38773
kallelongjuhani <kalle.jarvenpaa@gmail.com>
2023-08-16 11:29:39 +0300

    feat: loading indicator component

---
+++ frontend/src/lib/components/LoadingIndicator.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  export let message = '';
+</script>
+
+<div class="center flex flex-col">
+  <span class="loading-spinner loading-md loading" />
+  {#if message}
+    <span class="text-secondary">{{message}}</span>
+  {/if}
+</div>
