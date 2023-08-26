e3b505f40113553c0a382e26d2738986e93a5dec
Jhosimar Aguacia <corbin026@hotmail.com>
2023-08-26 14:38:26 +0300

    feat: add nominations object

---
+++ backend/vaa-strapi/src/api/nomination/controllers/nomination.ts
@@ -0,0 +1,7 @@
+/**
+ * nomination controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::nomination.nomination');
---
+++ backend/vaa-strapi/src/api/nomination/routes/nomination.ts
@@ -0,0 +1,7 @@
+/**
+ * nomination router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::nomination.nomination');
