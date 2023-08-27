0a39d86843eda3af0fa1c1fd0030decad3ed120d
kallelongjuhani <kalle.jarvenpaa@gmail.com>
2023-08-27 12:43:13 +0300

    feat: get candidates from the database with proper typing
    GetAllCandidates now returns all the relevant data and converts that to CandidateProps consumed by the CandidateDetailsCard component.

--- frontend/src/lib/api/getData.ts
+++ frontend/src/lib/api/getData.ts
@@ -1,9 +1,16 @@
 // Quick test function to get data from Strapi
 // To be refactored (GraphQL in the future?)
 
-import type {StrapiQuestionData, StrapiQuestionTypeData} from './getData.type';
+import type {
+  StrapiAnswerData,
+  StrapiCandidateData,
+  StrapiLanguageData,
+  StrapiQuestionData,
+  StrapiQuestionTypeData
+} from './getData.type';
 import {constants} from '$lib/utils/constants';
 import type {QuestionProps} from '$lib/components/questions';
+import type {CandidateProps} from '$lib/components/CandidateDetailsCard.type';
 
 // TODO: Define what type of data this returns instead of just any
 export const getData = async (
@@ -42,10 +49,50 @@ export const getData = async (
 // };
 
 // TODO: Define what type of data this returns instead of just any
-export const getAllCandidates = async (): Promise<any> => {
-  return await getData('api/candidates', new URLSearchParams({populate: '*'})).then((result) => {
-    if (result && result.data) return result?.data;
-    else console.error('Could not retrieve result for all candidates');
+export const getAllCandidates = async (): Promise<CandidateProps[]> => {
+  return await getData(
+    'api/candidates',
+    new URLSearchParams({
+      // We need a specific calls to populate relations, * only goes one-level deep
+      // NB. We cannot mix populate[0]=motherTongues with the deeper population def for answers!
+      'populate[motherTongues]': 'true',
+      'populate[otherLanguages]': 'true',
+      'populate[party]': 'true',
+      'populate[answers][populate][0]': 'question'
+    })
+  ).then((result) => {
+    if (result?.data) {
+      return result.data.map((d: StrapiCandidateData) => {
+        const attr = d.attributes;
+        const answers = attr.answers.data.map((a: StrapiAnswerData) => ({
+          questionId: a.attributes.question.data.id,
+          answer: a.attributes.answer.data.key
+          // openAnswer?: string;
+        }));
+        const motherTongues = attr.motherTongues.data.map(
+          (l: StrapiLanguageData) => l.attributes.name
+        );
+        const otherLanguages = attr.otherLanguages.data.map(
+          (l: StrapiLanguageData) => l.attributes.name
+        );
+        const party = {
+          name: attr.party.data.attributes.name,
+          shortName: attr.party.data.attributes.shortName
+        };
+        return {
+          answers,
+          candidateNumber: attr.candidateNumber,
+          firstName: attr.firstName,
+          lastName: attr.lastName,
+          motherTongues,
+          otherLanguages,
+          party,
+          politicalExperience: attr.politicalExperience
+        };
+      });
+    } else {
+      throw new Error('Could not retrieve result for all candidates');
+    }
   });
 };
 
@@ -58,7 +105,6 @@ export const getAllQuestions = async (): Promise<QuestionProps[]> => {
   return await getData(
     'api/question-types',
     new URLSearchParams({
-      'populate[0]': '*',
       // We need a specific call to populate the category relations, * only goes one-level deep
       'populate[questions][populate][0]': 'category'
     })
@@ -70,12 +116,12 @@ export const getAllQuestions = async (): Promise<QuestionProps[]> => {
         // TODO: Change to match new specs {type: string, values: [{key: number, label: string}]}
         const options = qType.attributes.settings.data;
         // Create the individual question objects
-        qType.attributes.questions.data.forEach((data) =>
+        qType.attributes.questions.data.forEach((d: StrapiQuestionData) =>
           questions.push({
-            id: '' + data.id,
-            text: data.attributes.text,
-            category: data.attributes.category.data.attributes.name ?? '',
-            info: data.attributes.info,
+            id: '' + d.id,
+            text: d.attributes.text,
+            category: d.attributes.category.data.attributes.name ?? '',
+            info: d.attributes.info,
             options
           })
         );
--- frontend/src/routes/results/+page.server.ts
+++ frontend/src/routes/results/+page.server.ts
@@ -1,7 +1,11 @@
-import {getAllCandidates} from '$lib/api/getData';
+import {getAllCandidates, getAllQuestions} from '$lib/api/getData';
 import type {PageServerLoad} from './$types';
 
 export const load = (async () => {
-  const results = await getAllCandidates();
-  return {results: results};
+  return {
+    candidates: await getAllCandidates(),
+    // We need to make sure we have the questions, bc if the page is reloaded
+    // we'll have lost the question data store
+    questions: await getAllQuestions()
+  };
 }) satisfies PageServerLoad;
