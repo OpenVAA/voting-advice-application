159a986104ea69b67d3c9c7e7eb172d7cf6c4c2d
kallelongjuhani <kalle.jarvenpaa@gmail.com>
2023-08-27 10:59:06 +0300

    feat: enable question skipping and add event typing

--- frontend/src/lib/components/questions/Question.type.ts
+++ frontend/src/lib/components/questions/Question.type.ts
@@ -5,3 +5,14 @@ export interface QuestionProps {
   category?: string;
   info?: string;
 }
+
+export type OnChangeEventDetail = {
+  id: string;
+  value: number;
+  originalEvent: Event;
+};
+
+export type OnSkipEventDetail = {
+  id: string;
+  originalEvent: MouseEvent;
+};
