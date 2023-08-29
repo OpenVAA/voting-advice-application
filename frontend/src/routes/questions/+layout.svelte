29b1f51eb5b2655213210a3c248a42114ee2c33e
kallelongjuhani <kalle.jarvenpaa@gmail.com>
2023-08-29 11:59:17 +0300

    feat: add unique routes to questions
    Previously the url stayed the same, meaning we couldn't use the back button. This way we no longer need the currentQuestion store either.

---
+++ frontend/src/routes/questions/+layout.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  import {_} from 'svelte-i18n';
+  import type {LayoutData} from './$types';
+  import {allQuestions} from '$lib/utils/stores';
+
+  export let data: LayoutData;
+
+  if (data?.questions) {
+    $allQuestions = data.questions;
+  } else {
+    throw new Error('Could not load data!');
+  }
+</script>
+
+<svelte:head>
+  <title>{$_('questions.questionsTitle')}</title>
+</svelte:head>
+
+<section class="flex h-screen flex-col">
+  <main class="grid flex-1 content-center px-3">
+    <slot />
+  </main>
+</section>
--- frontend/src/routes/questions/+page.server.ts
+++
@@ -1,8 +0,0 @@
-import type {PageServerLoad} from './$types';
-import {getAllQuestions} from '$lib/api/getData';
-
-export const load = (async () => {
-  return {
-    questions: await getAllQuestions() // getAllQuestions(),
-  };
-}) satisfies PageServerLoad;
