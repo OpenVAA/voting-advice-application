6def4a854553061d5716c8e55d51a01300309bea
kallelongjuhani <kalle.jarvenpaa@gmail.com>
2023-08-29 10:55:57 +0300

    feat: make href an attribute instead of hard-coded

--- frontend/src/lib/components/candidates/CandidateListing.svelte
+++ frontend/src/lib/components/candidates/CandidateListing.svelte
@@ -2,11 +2,12 @@
   import {GetFullNameInOrder} from '$lib/utils/internationalisation';
   import type {CandidateProps} from './CandidateProps.type';
   export let candidate: CandidateProps;
+  export let href: string;
 </script>
 
 <section class="card m-8 w-96 bg-base-100 shadow-xl">
   {#if candidate}
-    <a href={`/candidates/${candidate.id}`}>
+    <a {href}>
       <div class="card-body">
         <h2 class="card-title">{GetFullNameInOrder(candidate.firstName, candidate.lastName)}</h2>
         <h3>{candidate.party.name}</h3>
