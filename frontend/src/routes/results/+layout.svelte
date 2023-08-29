4b676965d9d65e4e8ef485c64008daa2c76eb3e4
kallelongjuhani <kalle.jarvenpaa@gmail.com>
2023-08-29 10:18:40 +0300

    refactor: move candidate loading to layout
    We move this to a layout so it will be available on subroutes, which we'll use the fetch individual candidates.

---
+++ frontend/src/routes/results/+layout.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  import {_} from 'svelte-i18n';
+  import type {LayoutData} from './$types';
+  import {allCandidates, allQuestions} from '$lib/utils/stores';
+
+  export let data: LayoutData;
+
+  if (data?.candidates && data?.questions) {
+    $allCandidates = data.candidates;
+    $allQuestions = data.questions;
+  } else {
+    throw new Error('Could not load candidate or question data!');
+  }
+</script>
+
+<slot />
--- frontend/src/routes/results/+page.svelte
+++ frontend/src/routes/results/+page.svelte
@@ -1,17 +1,7 @@
 <script lang="ts">
   import {_} from 'svelte-i18n';
-  import type {PageData} from './$types';
-  import {allCandidates, allQuestions, candidateRankings} from '$lib/utils/stores';
+  import {candidateRankings} from '$lib/utils/stores';
   import CandidateRankingListing from '$lib/components/CandidateRankingListing.svelte';
-
-  export let data: PageData;
-
-  if (data?.candidates && data?.questions) {
-    $allCandidates = data.candidates;
-    $allQuestions = data.questions;
-  } else {
-    throw new Error('Could not load candidate or question data!');
-  }
 </script>
 
 <h1>{$_('candidates.candidates')}</h1>
