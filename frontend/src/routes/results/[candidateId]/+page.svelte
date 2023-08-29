677205a73a15e0cfcb78a54b731e7a2c9b111bc5
kallelongjuhani <kalle.jarvenpaa@gmail.com>
2023-08-29 11:24:32 +0300

    feat: make the results route also work without rankings
    In effect, this now works the same way as the old candidates route

--- frontend/src/routes/results/[candidateId]/+page.svelte
+++ frontend/src/routes/results/[candidateId]/+page.svelte
@@ -1,23 +1,35 @@
 <script lang="ts">
   import {_} from 'svelte-i18n';
   import {page} from '$app/stores';
-  import {candidateRankings} from '$lib/utils/stores';
+  import {allCandidates, candidateRankings} from '$lib/utils/stores';
   import {
     CandidateDetailsCard,
     type CandidateProps,
     type RankingProps
   } from '$lib/components/candidates';
 
-  let ranking: {candidate: CandidateProps; match: RankingProps} | undefined;
+  let candidate: CandidateProps | undefined;
+  let ranking: RankingProps | undefined;
 
   $: if ($page.params.candidateId) {
     const id = '' + $page.params.candidateId;
-    ranking = $candidateRankings.find((r) => '' + r.candidate.id === id);
+    // First, check if we have a ranking for the candidate
+    if ($candidateRankings.length > 0) {
+      const result = $candidateRankings.find((r) => '' + r.candidate.id === id);
+      if (result) {
+        candidate = result.candidate;
+        ranking = result.match;
+      }
+    }
+    // If not, try to find the candidate
+    if (!candidate) {
+      candidate = $allCandidates.find((c) => '' + c.id === id);
+    }
   }
 </script>
 
-{#if ranking}
-  <CandidateDetailsCard candidate={ranking.candidate} ranking={ranking.match} />
+{#if candidate}
+  <CandidateDetailsCard {candidate} {ranking} />
 {:else}
   {$_('candidates.notFound')}
 {/if}
