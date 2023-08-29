3bb7f92c216dc58fe27457480eb57ce881a89c99
kallelongjuhani <kalle.jarvenpaa@gmail.com>
2023-08-29 10:55:57 +0300

    feat: make href an attribute instead of hard-coded
    fix: update CandidateDetailsCard to use RankingProps
    
    CandidateDetailsCard is the last place where we need to update the vaa-matching-compatible RankingProps format.
    
    Also made the ranking optional so that the same component can be used to show candidates without rankings, i.e., before matching has been performed.

--- frontend/src/lib/components/candidates/CandidateRankingListing.svelte
+++ frontend/src/lib/components/candidates/CandidateRankingListing.svelte
@@ -4,9 +4,10 @@
   import type {RankingProps} from './CandidateRanking.type';
   export let ranking: RankingProps;
   export let candidate: CandidateProps;
+  export let href: string;
 </script>
 
 <section class="w-256 card m-8 bg-base-100 shadow-xl">
   <p>Compatibility: {ranking.score}</p>
-  <CandidateListing {candidate} />
+  <CandidateListing {candidate} {href} />
 </section>
