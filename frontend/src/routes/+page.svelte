e9c8b5de30f4091335fdfabc66c433da12cefd1a
kallelongjuhani <kalle.jarvenpaa@gmail.com>
2023-09-05 13:18:46 +0300

    layout: make home screen layout closer to design

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,16 +1,22 @@
-<div>
-  <div class="hero">
-    <img class="w-full lg:w-10/12 xl:w-8/12 2xl:w-6/12" src="/images/hero.png" alt="" srcset="" />
+<div class="min-h-screen bg-secondary">
+  <div class="hero bg-[#d4dbef]">
+    <img
+      class="max-h-72 w-full max-w-screen-md bg-white object-cover"
+      src="/images/hero.png"
+      alt=""
+      srcset="" />
   </div>
-  <div class="flex flex-col flex-nowrap items-center gap-y-3">
-    <h1 class="text-center text-app-title">Election Compass</h1>
-    <h2 class="text-center">Colombian Parliamentary Elections 2023</h2>
-    <p class="text-center">
-      With this application you can compare candidates in the elections on 20 Dec 2023 based on
-      their opinions, parties and other data.
-    </p>
-    <a href="/navigation" class="btn">Start Finding Candidates!</a>
-    <a href="/information" class="link-hover link">Information about the elections</a>
-    <a href="/about" class="link-hover link">How does this app work</a>
+  <div class="flex flex-col items-center">
+    <div class="flex max-w-xl flex-col flex-nowrap items-center gap-y-3 p-6">
+      <h1 class="text-center text-app-title">Election Compass</h1>
+      <h2 class="text-center">Colombian Parliamentary Elections 2023</h2>
+      <p class="text-center">
+        With this application you can compare candidates in the elections on 20 Dec 2023 based on
+        their opinions, parties and other data.
+      </p>
+      <a href="/navigation" class="btn-primary btn">Start Finding Candidates!</a>
+      <a href="/information" class="btn-ghost btn">Information about the elections</a>
+      <a href="/about" class="btn-ghost btn">How does this app work</a>
+    </div>
   </div>
 </div>
