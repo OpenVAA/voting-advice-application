3c933ea1eeef8ca8241cf34ca5d35b74fc04f3db
Jhosimar Aguacia <corbin026@hotmail.com>
2023-09-06 08:53:02 +0300

    refactor: get app labels from root layout

--- frontend/src/routes/(voters)/+layout.server.ts
+++ frontend/src/routes/(voters)/+layout.server.ts
@@ -2,41 +2,9 @@ import {error} from '@sveltejs/kit';
 import type {LayoutServerLoad} from './$types';
 import {getData} from '$lib/api/getData';
 
-export const load: LayoutServerLoad = (async () => {
-  //TODO: Add filter to get the right election
-  const election = await getData(
-    'api/elections',
-    new URLSearchParams({populate: 'electionAppLabel'})
-  ).then((result) => {
-    if (result?.data[0]?.attributes) {
-      return result.data[0].attributes;
-    }
-    if (result?.error?.status === 404) {
-      throw error(404, 'election not found');
-    }
-  });
-
-  const appLabelId = election?.electionAppLabel?.data?.id;
-
-  // //TODO add filter to get the labels for the correct election
-  const appLabels = await getData(
-    'api/election-app-labels',
-    new URLSearchParams({
-      'filters[id][$eq]': appLabelId,
-      populate: '*'
-    })
-  )
-    .then((result) => {
-      if (result?.data[0]?.attributes) {
-        return result.data[0].attributes;
-      }
-      if (result?.error?.status === 404) {
-        throw error(404, 'election not found');
-      }
-    })
-    .catch((error) => {
-      console.error('Error in getting layout data from Strapi: ', ' - - - ', error);
-    });
+export const load: LayoutServerLoad = (async (loadEvent) => {
+  const {parent} = loadEvent;
+  const {appLabels} = await parent();
 
   if (appLabels?.error) {
     console.error('appLabels error', appLabels.error);
--- frontend/src/routes/+layout.ts
+++ frontend/src/routes/+layout.ts
@@ -3,7 +3,7 @@ import '$lib/i18n';
 import {locale, waitLocale} from 'svelte-i18n';
 import type {LayoutLoad} from './$types';
 
-export const load: LayoutLoad = (async ({fetch, params, route, url}) => {
+export const load: LayoutLoad = (async () => {
   if (browser) {
     locale.set(window.navigator.language);
   }
