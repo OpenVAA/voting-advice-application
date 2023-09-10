b09897b329e572177671f265a33dc4b1dcc48be6
Jhosimar Aguacia <corbin026@hotmail.com>
2023-09-10 22:57:03 +0300

    feat: add entityCard to the parties route

--- frontend/src/routes/(voters)/parties/+page.svelte
+++ frontend/src/routes/(voters)/parties/+page.svelte
@@ -1,25 +1,29 @@
 <script>
+  import {goto} from '$app/navigation';
+  import {page} from '$app/stores';
+  import {EntityCard} from '$lib/components/entityCard';
   import {_} from 'svelte-i18n';
   export let data;
-  let parties = data.parties ? Object.values(data.parties) : null;
+  let parties = data.parties ? Object.values(data.parties) : [];
 </script>
 
-<div class="flex flex-col items-center">
-  <div class="max-w-xl">
-    <h1 class="ml-2.5 mt-14 text-3xl font-medium leading-6 text-gray-500">
+<div class="bg-primary">
+  <div class="p-5">
+    <h1 class="ml-2.5 mt-14 text-center text-3xl font-medium leading-6 text-gray-500">
       {$_('parties.parties')}
     </h1>
-
-    {#if parties}
-      {#each parties as party}
-        <section class="w-256 card m-8 bg-base-100 shadow-xl">
-          <a href={`/parties/${party.id}`}>
-            <h1>{party.attributes.name}</h1>
-          </a>
-        </section>
+    <div role="feed" class="grid grid-cols-1 gap-4" aria-label={$_('candidates.candidates')}>
+      {#each parties as party, i}
+        <EntityCard
+          on:click={() => goto(`${$page.url.pathname}/${party.id}`)}
+          ariaPosinset={i + 1}
+          ariaSetsize={parties.length}
+          title={party.attributes.name}
+          id={party.id}
+          listText={party.attributes.shortName} />
+      {:else}
+        <p>{$_('parties.notFound')}</p>
       {/each}
-    {:else}
-      <p>{$_('parties.notFound')}</p>
-    {/if}
+    </div>
   </div>
 </div>
