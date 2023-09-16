5bfc223dfea185632f19b25cbddc1293b0b307cb
kallelongjuhani <kalle.jarvenpaa@gmail.com>
2023-09-16 18:40:43 +0300

    feat: convert result list to use EntityCard
    Also added a redirect to /questions if results cannot be calculated yet

--- frontend/src/routes/(voters)/results/+page.svelte
+++ frontend/src/routes/(voters)/results/+page.svelte
@@ -1,26 +1,57 @@
 <script lang="ts">
   import {_} from 'svelte-i18n';
+  import {onMount} from 'svelte';
+  import {goto} from '$app/navigation';
   import {page} from '$app/stores';
   import {candidates, candidateRankings} from '$lib/utils/stores';
   import {CandidateListing, CandidateRankingListing} from '$lib/components/candidates';
+  import {logDebugError} from '$lib/utils/logger';
+  import {GetFullNameInOrder} from '$lib/utils/internationalisation';
+  import {EntityCard} from '$lib/components/entityCard';
+  import {ScoreGauge} from '$lib/components/scoreGauge';
 
   const urlRoot = $page.url.pathname.replace(/\/$/, '');
 
-  // TODO: Check onMount if $candidateRankings.length > 0 and if not redirect to
-  // frontpage or /questions
+  onMount(() => {
+    if ($candidateRankings?.length === 0) {
+      logDebugError('No candidate rankings found. Redirecting to questions');
+      goto('/questions');
+    }
+  });
 </script>
 
-<div class="flex h-full flex-col items-center justify-center">
+<div class="flex h-full flex-col items-center justify-center bg-primary">
   <div class="max-w-xl">
     <h1>{$_('candidates.candidates')}</h1>
-    {#if $candidateRankings.length > 0}
-      {#each $candidateRankings as { match, candidate }}
-        <CandidateRankingListing ranking={match} {candidate} href={`${urlRoot}/${candidate.id}`} />
+    <div role="feed" class="grid grid-cols-1 gap-4" aria-label={$_('candidates.candidates')}>
+      {#each $candidateRankings as { match, candidate }, i}
+        <EntityCard
+          on:click={() => goto(`${urlRoot}/${candidate.id}`)}
+          ariaPosinset={i + 1}
+          ariaSetsize={$candidates.length}
+          title={GetFullNameInOrder(candidate.firstName, candidate.lastName)}
+          imgSrc="/images/candidate-photo.png"
+          id={candidate.id}
+          listText={candidate.party?.shortName ?? ''}
+          electionSymbol={candidate.electionSymbol}>
+          <div slot="card-footer">
+            {#if match.subMatches && match.subMatches.length > 0}
+              <div
+                class="mx-4 mb-6 grid grid-flow-row grid-cols-2 gap-x-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+                {#each match.subMatches as subMatch}
+                  <!-- TODO: replace the progressBarColor color with the color of the category -->
+                  <ScoreGauge
+                    score={subMatch.score}
+                    label={subMatch.questionGroup.label ?? ''}
+                    shape="linear" />
+                {/each}
+              </div>
+            {/if}
+          </div>
+        </EntityCard>
+      {:else}
+        <p>{$_('candidates.notFound')}</p>
       {/each}
-    {:else}
-      {#each $candidates as candidate}
-        <CandidateListing {candidate} href={`${urlRoot}/${candidate.id}`} />
-      {/each}
-    {/if}
+    </div>
   </div>
 </div>
