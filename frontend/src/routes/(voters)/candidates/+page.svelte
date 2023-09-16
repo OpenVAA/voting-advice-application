c45f9f24e28d2d66af1ba02296cc2615b223de6b
kallelongjuhani <kalle.jarvenpaa@gmail.com>
2023-09-16 18:24:23 +0300

    layout: fix candidates list layout

--- frontend/src/routes/(voters)/candidates/+page.svelte
+++ frontend/src/routes/(voters)/candidates/+page.svelte
@@ -9,25 +9,27 @@
   const urlRoot = $page.url.pathname.replace(/\/$/, '');
 </script>
 
-<div class="bg-primary">
-  <div class="p-5">
-    <h1 class="ml-2.5 mt-14 text-center text-3xl font-medium leading-6 text-gray-500">
-      {$_('candidates.candidates')}
-    </h1>
-  </div>
-  <div role="feed" class="grid grid-cols-1 gap-4" aria-label={$_('candidates.candidates')}>
-    {#each $candidates as { id, firstName, lastName, party, electionSymbol }, i}
-      <EntityCard
-        on:click={() => goto(`${urlRoot}/${id}`)}
-        ariaPosinset={i + 1}
-        ariaSetsize={$candidates.length}
-        title={GetFullNameInOrder(firstName, lastName)}
-        imgSrc="/images/candidate-photo.png"
-        {id}
-        listText={party?.name ?? ''}
-        {electionSymbol} />
-    {:else}
-      <p>{$_('candidates.notFound')}</p>
-    {/each}
+<div class="flex h-full flex-col items-center justify-center bg-primary">
+  <div class="max-w-xl">
+    <div class="p-5">
+      <h1 class="ml-2.5 mt-14 text-center text-3xl font-medium leading-6 text-gray-500">
+        {$_('candidates.candidates')}
+      </h1>
+    </div>
+    <div role="feed" class="grid grid-cols-1 gap-4" aria-label={$_('candidates.candidates')}>
+      {#each $candidates as { id, firstName, lastName, party, electionSymbol }, i}
+        <EntityCard
+          on:click={() => goto(`${urlRoot}/${id}`)}
+          ariaPosinset={i + 1}
+          ariaSetsize={$candidates.length}
+          title={GetFullNameInOrder(firstName, lastName)}
+          imgSrc="/images/candidate-photo.png"
+          {id}
+          listText={party?.name ?? ''}
+          {electionSymbol} />
+      {:else}
+        <p>{$_('candidates.notFound')}</p>
+      {/each}
+    </div>
   </div>
 </div>
