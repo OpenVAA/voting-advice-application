e5f209e54da22217cb7a7290c8ca19ef3b1ce6b0
kallelongjuhani <kalle.jarvenpaa@gmail.com>
2023-09-16 18:12:08 +0300

    fix: correct prop for candidate photo src

--- frontend/src/routes/(voters)/candidates/+page.svelte
+++ frontend/src/routes/(voters)/candidates/+page.svelte
@@ -2,7 +2,9 @@
   import {_} from 'svelte-i18n';
   import {page} from '$app/stores';
   import {candidates} from '$lib/utils/stores';
-  import {CandidateListing} from '$lib/components/candidates';
+  import {GetFullNameInOrder} from '$lib/utils/internationalisation';
+  import {EntityCard} from '$lib/components/entityCard';
+  import {goto} from '$app/navigation';
 
   const urlRoot = $page.url.pathname.replace(/\/$/, '');
 </script>
@@ -12,8 +14,18 @@
     <h1 class="ml-2.5 mt-14 text-center text-3xl font-medium leading-6 text-gray-500">
       {$_('candidates.candidates')}
     </h1>
-    {#each $candidates as candidate}
-      <CandidateListing {candidate} href={`${urlRoot}/${candidate.id}`} />
+  </div>
+  <div role="feed" class="grid grid-cols-1 gap-4" aria-label={$_('candidates.candidates')}>
+    {#each $candidates as { id, firstName, lastName, party, electionSymbol }, i}
+      <EntityCard
+        on:click={() => goto(`${urlRoot}/${id}`)}
+        ariaPosinset={i + 1}
+        ariaSetsize={$candidates.length}
+        title={GetFullNameInOrder(firstName, lastName)}
+        imgSrc="/images/candidate-photo.png"
+        {id}
+        listText={party?.name ?? ''}
+        {electionSymbol} />
     {:else}
       <p>{$_('candidates.notFound')}</p>
     {/each}
