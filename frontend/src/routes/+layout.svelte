3353c3f5861bb6cda6605c4539a69b27f0cec244
kallelongjuhani <kalle.jarvenpaa@gmail.com>
2023-09-17 10:39:25 +0300

    refactor: streamline data load

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
   import '../app.css';
-  import {onMount} from 'svelte';
   import type {LayoutServerData} from './$types';
   import {appLabels, election} from '$lib/utils/stores';
 
@@ -10,14 +9,12 @@
   // unless we wait for onMount. In the sublayout this seems to be fine, but would demand
   // a bit of investigation.
   // TODO: Investigate what'd be the proper way to handle data updates.
-  onMount(() => {
-    if (data.appLabels && data.election) {
-      $appLabels = data.appLabels;
-      $election = data.election;
-    } else {
-      throw new Error('appLabels not found');
-    }
-  });
+  if (data.appLabels && data.election) {
+    $appLabels = data.appLabels;
+    $election = data.election;
+  } else {
+    throw new Error('appLabels not found');
+  }
 </script>
 
 {#if $appLabels}
