0a927d00d7d0428756dbdde289e984d92e6fc432
kallelongjuhani <kalle.jarvenpaa@gmail.com>
2023-09-17 10:39:25 +0300

    refactor: streamline data load

--- frontend/src/routes/+layout.ts
+++ frontend/src/routes/+layout.ts
@@ -3,10 +3,11 @@ import '$lib/i18n';
 import {locale, waitLocale} from 'svelte-i18n';
 import type {LayoutLoad} from './$types';
 
-export const load: LayoutLoad = (async () => {
+export const load: LayoutLoad = (async (data) => {
   if (browser) {
     locale.set(window.navigator.language);
   }
 
   await waitLocale();
+  return data.data;
 }) satisfies LayoutLoad;
