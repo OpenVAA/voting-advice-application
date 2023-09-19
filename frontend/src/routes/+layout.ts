2b219ec85f9b95703cfac3cd44b156df95e394f7
kallelongjuhani <kalle.jarvenpaa@gmail.com>
2023-09-19 21:06:02 +0300

    chore: get rid of data.data

--- frontend/src/routes/+layout.ts
+++ frontend/src/routes/+layout.ts
@@ -3,11 +3,11 @@ import '$lib/i18n';
 import {locale, waitLocale} from 'svelte-i18n';
 import type {LayoutLoad} from './$types';
 
-export const load: LayoutLoad = (async (data) => {
+export const load: LayoutLoad = (async ({data}) => {
   if (browser) {
     locale.set(window.navigator.language);
   }
 
   await waitLocale();
-  return data.data;
+  return data;
 }) satisfies LayoutLoad;
