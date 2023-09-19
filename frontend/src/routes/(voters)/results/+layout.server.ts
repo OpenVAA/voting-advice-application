a1d990bc0e73fe53c80a91588746ca7d78465a08
kallelongjuhani <kalle.jarvenpaa@gmail.com>
2023-09-19 10:41:17 +0300

    fix: load answers on the /results route

--- frontend/src/routes/(voters)/results/+layout.server.ts
+++ frontend/src/routes/(voters)/results/+layout.server.ts
@@ -3,7 +3,7 @@ import type {LayoutServerLoad} from './$types';
 
 export const load = (async () => {
   return {
-    candidates: await getNominatedCandidates(),
+    candidates: await getNominatedCandidates({loadAnswers: true}),
     // We need to make sure we have the questions, bc if the page is reloaded
     // we'll have lost the question data store
     questions: await getQuestions()
