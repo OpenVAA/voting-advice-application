0447c990fda21d963f2ebb51a0f1f1e6723c21ff
kallelongjuhani <kalle.jarvenpaa@gmail.com>
2023-09-19 21:04:53 +0300

    refactor: move resetLocalStorage from function to link event

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -4,6 +4,8 @@
   import {page} from '$app/stores';
   import {resetLocalStorage} from '$lib/utils/stores';
 
+  // TODO: Maybe change the locale here to appLabels.locale so it matches the rest of the string
+  // We also need to include the locale property in the appLabels store
   const electionDateString = new Date($page.data.election.electionDate).toLocaleDateString(
     $page.data.election.locale ?? $locale ?? undefined,
     {
@@ -16,37 +18,28 @@
     '{{0}}',
     electionDateString
   );
-
-  function gotoNavigation() {
-    // We should reset the stored voter answers at some point, so we do it here
-    // TODO: Change this to work more rationally, perhaps based on a unique version id of the db
-    resetLocalStorage();
-    goto('/navigation');
-  }
 </script>
 
-{#if $page.data.appLabels && $page.data.election}
-  <div class="min-h-screen bg-primary">
-    <div class="hero bg-[#d4dbef]">
-      <img
-        class="max-h-72 w-full max-w-screen-md bg-white object-cover"
-        src="/images/hero.png"
-        alt=""
-        srcset="" />
-    </div>
-    <div class="flex flex-col items-center">
-      <div class="flex max-w-xl flex-col flex-nowrap items-center gap-y-3 p-6">
-        <h1 class="text-center text-app-title">{$page.data.appLabels.appTitle}</h1>
-        <h2 class="text-center">{$page.data.election.name}</h2>
-        <p class="text-center">
-          {appDescription}
-        </p>
-        <button on:click={gotoNavigation} class="btn-primary btn"
-          >{$page.data.appLabels.actionLabels.startButton}</button>
-        <a href="/information" class="btn-ghost btn"
-          >{$page.data.appLabels.actionLabels.electionInfo}</a>
-        <a href="/about" class="btn-ghost btn">{$page.data.appLabels.actionLabels.howItWorks}</a>
-      </div>
+<div class="min-h-screen bg-primary">
+  <div class="hero bg-[#d4dbef]">
+    <img
+      class="max-h-72 w-full max-w-screen-md bg-white object-cover"
+      src="/images/hero.png"
+      alt=""
+      srcset="" />
+  </div>
+  <div class="flex flex-col items-center">
+    <div class="flex max-w-xl flex-col flex-nowrap items-center gap-y-3 p-6">
+      <h1 class="text-center text-app-title">{$page.data.appLabels.appTitle}</h1>
+      <h2 class="text-center">{$page.data.election.name}</h2>
+      <p class="text-center">
+        {appDescription}
+      </p>
+      <a href="/navigation" on:click={resetLocalStorage} class="btn-primary btn"
+        >{$page.data.appLabels.actionLabels.startButton}</a>
+      <a href="/information" class="btn-ghost btn"
+        >{$page.data.appLabels.actionLabels.electionInfo}</a>
+      <a href="/about" class="btn-ghost btn">{$page.data.appLabels.actionLabels.howItWorks}</a>
     </div>
   </div>
-{/if}
+</div>
