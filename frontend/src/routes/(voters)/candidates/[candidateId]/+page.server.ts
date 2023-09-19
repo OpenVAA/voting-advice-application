3f584d62b49d7c0216f726dfa612d3a9117f59ac
kallelongjuhani <kalle.jarvenpaa@gmail.com>
2023-09-19 10:40:24 +0300

    fix: raise error on server if candidate not found
    To match the behaviour with /parties

--- frontend/src/routes/(voters)/candidates/[candidateId]/+page.server.ts
+++ frontend/src/routes/(voters)/candidates/[candidateId]/+page.server.ts
@@ -1,10 +1,14 @@
 import {getNominatedCandidates} from '$lib/api/getData';
+import {error} from '@sveltejs/kit';
 import type {PageServerLoad} from './$types';
 
 export const load = (async ({params}) => {
   const id = params.candidateId;
   const results = await getNominatedCandidates({id, loadAnswers: true});
+  if (results.length === 0) {
+    throw error(404, 'Candidate not found');
+  }
   return {
-    candidate: results?.length > 0 ? results[0] : undefined
+    candidate: results[0]
   };
 }) satisfies PageServerLoad;
