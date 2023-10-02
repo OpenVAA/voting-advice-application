5c3568aeffe1b3fe70aaa45a7fdcfe97d024863d
kallelongjuhani <kalle.jarvenpaa@gmail.com>
2023-10-02 20:57:18 +0300

    refactor: apply updated styles to (voter)/navigation page
    Apply updated Tailwind styles.

--- frontend/src/routes/(voters)/navigation/+page.svelte
+++ frontend/src/routes/(voters)/navigation/+page.svelte
@@ -1,32 +1,30 @@
-<div class="flex flex-col items-center gap-4 p-4">
-  <h1 class="ml-2.5 mt-14 text-3xl font-medium leading-6 text-gray-500">
-    Welcome to the Voting Advice Application!
-  </h1>
-  <div class="grid w-96 grid-flow-row gap-4 lg:w-full lg:grid-cols-3">
-    <div class="card bg-base-100 shadow-xl">
+<div class="flex flex-col items-center gap-lg p-lg pb-[3.5rem]">
+  <h1 class="my-lg">Welcome to the Voting Advice Application!</h1>
+  <div class="w-96 grid grid-flow-row gap-lg lg:w-full lg:grid-cols-3">
+    <div class="card shadow-md">
       <div class="card-body">
         <h2 class="card-title">Questions</h2>
         <p>Answer questions to find suggestions about your candidates.</p>
         <div class="card-actions justify-end">
-          <a href="/questions" class="btn-primary btn">Answer Questions</a>
+          <a href="/questions" class="btn-primary btn w-full max-w-md">Answer Questions</a>
         </div>
       </div>
     </div>
-    <div class="card bg-base-100 shadow-xl">
+    <div class="card shadow-md">
       <div class="card-body">
         <h2 class="card-title">Candidates</h2>
         <p>View the list of candidates.</p>
         <div class="card-actions justify-end">
-          <a href="/candidates" class="btn-primary btn">View Candidates</a>
+          <a href="/candidates" class="btn-primary btn w-full max-w-md">View Candidates</a>
         </div>
       </div>
     </div>
-    <div class="card bg-base-100 shadow-xl">
+    <div class="card shadow-md">
       <div class="card-body">
         <h2 class="card-title">Parties</h2>
         <p>View the list of parties.</p>
         <div class="card-actions justify-end">
-          <a href="/parties" class="btn-primary btn">View Parties</a>
+          <a href="/parties" class="btn-primary btn w-full max-w-md">View Parties</a>
         </div>
       </div>
     </div>
