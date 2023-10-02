a430b234a2436a722931601c21b695c6adb9f616
kallelongjuhani <kalle.jarvenpaa@gmail.com>
2023-10-02 20:48:09 +0300

    refactor: update frontpage styles and add footer
    Apply the updated Tailwind styles to the frontpage and move the `footer`
    from `(voter)/+layout.svelte` here, where it should be according to the
    designs.

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -20,26 +20,42 @@
   );
 </script>
 
-<div class="min-h-screen bg-primary">
-  <div class="hero bg-[#d4dbef]">
+<div class="flex w-full flex-grow flex-col items-center bg-base-300">
+  <figure class="hero bg-[#d4dbef]">
     <img
-      class="max-h-72 w-full max-w-screen-md bg-white object-cover"
+      class="max-h-[30vh] w-full max-w-lg bg-white object-cover"
       src="/images/hero.png"
       alt=""
       srcset="" />
-  </div>
-  <div class="flex flex-col items-center">
-    <div class="flex max-w-xl flex-col flex-nowrap items-center gap-y-3 p-6">
-      <h1 class="text-center text-app-title">{$page.data.appLabels.appTitle}</h1>
-      <h2 class="text-center">{$page.data.election.name}</h2>
-      <p class="text-center">
-        {appDescription}
-      </p>
-      <a href="/navigation" on:click={resetLocalStorage} class="btn-primary btn"
-        >{$page.data.appLabels.actionLabels.startButton}</a>
-      <a href="/information" class="btn-ghost btn"
-        >{$page.data.appLabels.actionLabels.electionInfo}</a>
-      <a href="/about" class="btn-ghost btn">{$page.data.appLabels.actionLabels.howItWorks}</a>
+  </figure>
+
+  <main class="flex-grow">
+    <div class="flex max-w-xl flex-col items-center p-lg pl-safelgl pr-safelgr">
+      <div class="flex flex-col flex-nowrap items-center">
+        <hgroup class="py-lg">
+          <p class="text-2xl font-bold text-primary">{$page.data.appLabels.appTitle}</p>
+          <h1 class="text-3xl font-normal">{$page.data.election.name}</h1>
+        </hgroup>
+        <p class="text-center">
+          {appDescription}
+        </p>
+        <a
+          href="/navigation"
+          on:click={resetLocalStorage}
+          class="btn-primary btn mb-md w-full max-w-md"
+          >{$page.data.appLabels.actionLabels.startButton}</a>
+        <a href="/information" class="btn-ghost btn w-full max-w-md"
+          >{$page.data.appLabels.actionLabels.electionInfo}</a>
+        <a href="/about" class="btn-ghost btn w-full max-w-md"
+          >{$page.data.appLabels.actionLabels.howItWorks}</a>
+      </div>
     </div>
-  </div>
+  </main>
+
+  <footer class="p-lg text-center text-sm text-secondary">
+    {$page.data.appLabels.viewTexts.publishedBy.replace('{{0}}', '')}
+    <img class="inline w-14" src={'/icons/publisher.svg'} alt="governmental" srcset="" />
+    Institution • {$page.data.appLabels.viewTexts.madeWith.replace('{{0}}', '')}
+    <img class="inline w-14" src="/icons/vote.svg" alt="" srcset="" /> OpenVAA
+  </footer>
 </div>
