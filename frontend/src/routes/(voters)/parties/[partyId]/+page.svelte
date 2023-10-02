d0958571a2bbbc0d9071eaf498f62e44581a9b21
kallelongjuhani <kalle.jarvenpaa@gmail.com>
2023-10-02 21:07:48 +0300

    refactor: apply updated styles to the party page
    Apply updated Tailwind styles to the placeholder page showing a single
    party.

--- frontend/src/routes/(voters)/parties/[partyId]/+page.svelte
+++ frontend/src/routes/(voters)/parties/[partyId]/+page.svelte
@@ -6,21 +6,25 @@
   export let data: PageServerData;
 </script>
 
-<section class="card m-8 w-96 bg-base-100 shadow-xl">
-  <div class="card-body">
-    <h2 class="card-title">{data.party.name}</h2>
-    <h3>{data.party.info}</h3>
-    {#if data.party.nominatedCandidates?.length}
-      <p><strong>{$_('candidates.candidates')}:</strong></p>
-      <ul>
-        {#each data.party.nominatedCandidates as candidate}
-          <li>
-            <a href={`/candidates/${candidate.id}`}>
-              {GetFullNameInOrder(candidate.firstName, candidate.lastName)}
-            </a>
-          </li>
-        {/each}
-      </ul>
-    {/if}
+<article class="flex w-full flex-grow flex-col items-center bg-base-300 lg:p-md lg:pb-0">
+  <div class="w-full max-w-xl flex-grow rounded-t-lg bg-base-100 pb-[3.5rem] lg:shadow-xl">
+    <div class="card">
+      <div class="card-body">
+        <h1 class="card-title">{data.party.name}</h1>
+        <h3>{data.party.info}</h3>
+        {#if data.party.nominatedCandidates?.length}
+          <p><strong>{$_('candidates.candidates')}:</strong></p>
+          <ul>
+            {#each data.party.nominatedCandidates as candidate}
+              <li>
+                <a href={`/candidates/${candidate.id}`}>
+                  {GetFullNameInOrder(candidate.firstName, candidate.lastName)}
+                </a>
+              </li>
+            {/each}
+          </ul>
+        {/if}
+      </div>
+    </div>
   </div>
-</section>
+</article>
