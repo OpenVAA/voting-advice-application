7c6426b7117552990c0be33960192054684d0403
Johanna Laitila <johanna.maria.laitila@gmail.com>
2023-10-09 22:50:12 +0300

    fix: Format description text numbers properly in questions intro page

--- frontend/src/routes/(voters)/questions/+page.svelte
+++ frontend/src/routes/(voters)/questions/+page.svelte
@@ -1,15 +1,21 @@
 <script lang="ts">
-  import {_} from 'svelte-i18n';
   import {page} from '$app/stores';
 
   const firstQuestionUrl = `${$page.url.pathname.replace(/\/$/, '')}/${$page.data.questions[0].id}`;
+
+  const questionCategories = new Set<string>();
+  $page.data.questions.forEach((question: any) => questionCategories.add(question?.category));
+
+  const opinionsDescriptionText = $page.data.appLabels.viewTexts.yourOpinionsDescription
+    .replace('{{0}}', $page.data.questions.length.toString())
+    .replace('{{1}}', questionCategories.size.toString());
 </script>
 
 <div class="flex max-w-xl flex-grow flex-col justify-center p-lg">
   <h1 class="my-lg">
     {$page.data.appLabels.viewTexts.yourOpinionsTitle}
   </h1>
-  <p class="text-center">{$page.data.appLabels.viewTexts.yourOpinionsDescription}</p>
+  <p class="text-center">{opinionsDescriptionText}</p>
   <a href={firstQuestionUrl} class="btn-primary btn"
     >{$page.data.appLabels.actionLabels.startQuestions}</a>
 </div>
