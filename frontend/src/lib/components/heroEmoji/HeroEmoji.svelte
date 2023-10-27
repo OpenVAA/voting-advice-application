4052c3cdbc1dc3f32c7bf737ce409d4bc30ad673
kallelongjuhani <kalle.jarvenpaa@gmail.com>
2023-10-27 14:14:37 +0900

    feat: HeroEmoji component for emoji illustrations
    The component styles its content (an emoji) in such a way that it can be
    used as an easy illustration on typical pages. The `div` produced is
    `aria-hidden` by default but this can be overriden by supplying a
    `"false"` value value for that attribute.
    
    The emoji content on each page may be hard-coded or, bettes yet,
    supplied using the standard way for text translations.
    
    Fixes #235

---
+++ frontend/src/lib/components/heroEmoji/HeroEmoji.svelte
@@ -0,0 +1,41 @@
+<script lang="ts">
+  import type {HeroEmojiProps} from './HeroEmoji.type';
+
+  type $$Props = HeroEmojiProps;
+
+  // Merge the necessary classes to possible extra classes defined in the props
+  $$restProps.class = `whitespace-nowrap truncate text-clip text-center font-emoji text-[6.5rem] leading-none ${
+    $$restProps.class ?? ''
+  }`;
+</script>
+
+<!--
+@component
+Used for large emojis acting as decorative illustrations.
+
+The content of the component is the emoji or emojis to be displayed.
+
+The content is hidden from screen readers by default, because the
+intended use is for [decorative purposes](https://www.w3.org/WAI/tutorials/images/decorative/).
+To override, add `aria-hidden="false"` to the tag and also consider
+adding an `aria-label`.
+
+To change the size of the emoji, add a `text-[size]` utility class
+using the `class` attribute, e.g. `class="text-[10rem]"`.
+
+### Properties
+
+- Any valid attributes of a `<div>` element.
+
+### Slots
+
+- default: the emoji or emojis to be displayed.
+
+### Usage
+
+```tsx
+<HeroEmoji>🚀</HeroEmoji>
+```
+-->
+
+<div role="img" aria-hidden="true" {...$$restProps}><slot /></div>
---
+++ frontend/src/lib/components/heroEmoji/HeroEmoji.type.ts
@@ -0,0 +1,2 @@
+import type {SvelteHTMLElements} from 'svelte/elements';
+export type HeroEmojiProps = SvelteHTMLElements['div'];
