74345d58ba23138b35e51011f6b6f285ae12f783
Joonatan <97550588+joonatan-aatos@users.noreply.github.com>
2023-11-03 21:26:07 +0200

    feat: Candidate basic information (#239)
    * feat: Add candidate basic info page
    
    * feat: Add portrait field
    
    * feat: Add unaffiliated toggle button
    
    * feat: Add election manifesto and fix UI issues

---
+++ frontend/src/lib/components/common/FieldGroup.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  export let fields: any[] = ['default'];
+  export let customStyle: string | undefined = undefined;
+</script>
+
+<div class="w-full">
+  <slot name="header" field={undefined} />
+  <div class="my-6 flex w-full flex-col gap-4 overflow-hidden rounded-lg">
+    {#each fields as field}
+      <div class="bg-base-300" style={customStyle}>
+        <slot {field} />
+      </div>
+    {/each}
+  </div>
+  <slot name="footer" field={undefined} />
+</div>
