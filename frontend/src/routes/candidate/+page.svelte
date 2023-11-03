f28947e5d2b38da50d9eaf5cca432cd9bf7f0861
Eemeli <eemelioksanen@protonmail.com>
2023-11-03 16:40:27 +0200

    feat: add logout button

--- frontend/src/routes/candidate/+page.svelte
+++ frontend/src/routes/candidate/+page.svelte
@@ -1,5 +1,59 @@
 <script lang="ts">
-  import {appName} from '$candidate/placeholder.json';
+  import Modal from './modal.svelte';
+  import {goto} from '$app/navigation';
+  import {_} from 'svelte-i18n';
+  import {appName, candidateAppRoute} from '$candidate/placeholder.json';
+
+  // TODO: add proper check of unfilled data
+  let unfilledData = true;
+  let isOpen = false; // variable bound to Modal
+  let isPopupVisible = false; //variable for popup
+
+  const triggerLogout = () => {
+    // TODO: check if candidate has filler all the data
+    if (unfilledData) {
+      isOpen = true;
+    } else {
+      logout();
+    }
+  };
+
+  const logout = () => {
+    // TODO check when login functionality is ready
+    localStorage.removeItem('jwt');
+    isOpen = false;
+    togglePopup();
+    // TODO: redirect to login page
+    goto(candidateAppRoute);
+  };
+
+  function togglePopup() {
+    isPopupVisible = !isPopupVisible;
+  }
 </script>
 
+{#if isPopupVisible}
+  <div
+    class="alert"
+    style="position: fixed; bottom: 0; left: 50%; transform: translateX(-50%); z-index: 9999;">
+    <span>You have logged out, congrats </span>
+  </div>
+{/if}
+
 <h1>Welcome to the {appName}</h1>
+
+<button class="btn-error btn" on:click={triggerLogout}>{$_('candidateApp.navbar.logOut')}</button>
+
+<div>
+  <Modal bind:isOpen onClick={logout} buttonText={$_('candidateApp.navbar.logOut')}>
+    <div class="notification">Unfilled data is there!!!! 😱</div>
+  </Modal>
+</div>
+
+<style>
+  .notification {
+    width: 200px;
+    height: 200px;
+    color: black;
+  }
+</style>
---
+++ frontend/src/routes/candidate/modal.svelte
@@ -0,0 +1,47 @@
+<!-- Modal.svelte -->
+<script lang="ts">
+  import {_} from 'svelte-i18n';
+  export let isOpen = true;
+  export let onClick: () => void;
+  export let buttonText: string;
+</script>
+
+<div class:none={!isOpen} class:modal2={isOpen}>
+  <div class="modal-content">
+    <slot />
+    <div class="float-left">
+      <button class="btn-success btn" on:click={() => (isOpen = false)}
+        >{$_('candidateApp.navbar.close')}</button>
+    </div>
+    <div class="float-right">
+      <button class="btn-error btn" on:click={onClick}>{buttonText}</button>
+    </div>
+  </div>
+</div>
+
+<style>
+  .none {
+    display: none;
+  }
+
+  .modal2 {
+    display: flex;
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background: rgba(0, 0, 0, 0.5);
+    backdrop-filter: blur(5px);
+    z-index: 999;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .modal-content {
+    background: white;
+    padding: 20px;
+    border-radius: 5px;
+    box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.2);
+  }
+</style>
