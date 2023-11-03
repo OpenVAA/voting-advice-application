e86029562174e3d6dfb527b3bd835ae818a219d1
Joonatan Korpela <joonatan.korpela@aalto.fi>
2023-11-03 12:50:31 +0200

    feat: Add candidate basic info page

---
+++ frontend/src/lib/components/common/FieldGroup.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  export let fields: any[] = [];
+</script>
+
+<div class="flex w-full flex-col gap-4 overflow-hidden rounded-lg">
+  {#each fields as field}
+    <div class="bg-base-300">
+      <slot {field} />
+    </div>
+  {/each}
+  <slot name="footer" field={undefined} />
+</div>
---
+++ frontend/src/routes/candidate/profile/+page.svelte
@@ -0,0 +1,64 @@
+<script lang="ts">
+  import FieldGroup from '$lib/components/common/FieldGroup.svelte';
+  import {_} from 'svelte-i18n';
+
+  const disabledFields = ['firstName', 'lastName', 'electionList', 'electionNumber'];
+  const editableFields = ['gender', 'motherTongue', 'age'];
+  const fieldOptions = new Map([
+    [
+      'gender',
+      ['male', 'female', 'nonBinary', 'other', 'preferNotToSay'].map((a) =>
+        $_(`candidateApp.genders.${a}`)
+      )
+    ],
+    ['motherTongue', ['English', 'Suomi', 'Svenska']]
+  ]);
+
+  const labelClass = 'label-sm label mx-6 my-2';
+</script>
+
+<div class="mx-40 my-20 flex flex-col items-center gap-20">
+  <h2>{$_('candidateApp.basicInfo.title')}</h2>
+
+  <p class="text-center">
+    {$_('candidateApp.basicInfo.instructions')}
+  </p>
+
+  <FieldGroup fields={disabledFields} let:field>
+    <div class="flex items-center justify-between px-4">
+      <label for={field} class={labelClass}>
+        {$_(`candidateApp.basicInfo.fields.${field}`)}
+      </label>
+      <input
+        type="text"
+        id={field}
+        placeholder="PLACEHOLDER"
+        class="input-ghost input input-sm w-6/12 text-right" />
+    </div>
+    <p class="m-0 p-0 text-sm text-secondary" slot="footer">
+      {$_('candidateApp.basicInfo.disclaimer')}
+    </p>
+  </FieldGroup>
+
+  <FieldGroup fields={editableFields} let:field>
+    <div class="flex items-center justify-between px-4">
+      <label for={field} class={labelClass}>
+        {$_(`candidateApp.basicInfo.fields.${field}`)}
+      </label>
+      {#if field === 'age'}
+        <input
+          type="number"
+          id={field}
+          placeholder="0"
+          class="input-ghost input input-sm w-6/12 text-right" />
+      {:else}
+        <select id={field} class="select-bordered select select-sm w-6/12">
+          <option value="">{$_(`candidateApp.basicInfo.fields.${field}`)}</option>
+          {#each fieldOptions.get(field) ?? [] as option}
+            <option value={option}>{option}</option>
+          {/each}
+        </select>
+      {/if}
+    </div>
+  </FieldGroup>
+</div>
