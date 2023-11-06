39d51d0691f816755d019a364914626d4c33ad5e
Stepan Fedotov <stepan.fedotov@gmail.com>
2023-11-06 14:39:19 +0200

    feat: candidate login backend

--- backend/vaa-strapi/src/extensions/users-permissions/strapi-server.js
+++ backend/vaa-strapi/src/extensions/users-permissions/strapi-server.js
@@ -1,30 +1,62 @@
+'use strict';
+
+const defaultAuthenticatedPermissions = [
+  'api::candidate.candidate.findOne',
+  'api::candidate.candidate.find',
+];
+
 module.exports = async (plugin) => {
-  // Detour bootstrap so we can overwrite default grants
-  // TODO: this gets run each boot, figure out how to make this one-off so these can be changed if needed?
   const origBootstrap = plugin.bootstrap;
   plugin.bootstrap = async (ctx) => {
     const res = origBootstrap(ctx);
 
     const pluginStore = strapi.store({ type: 'plugin', name: 'users-permissions' });
     
-    const grants = await pluginStore.get({ key: 'grant' });
-    grants.email.enabled = false;
-    await pluginStore.set({ key: 'grant', value: grants });
+    // const grants = await pluginStore.get({ key: 'grant' });
+    // grants.email.enabled = false;
+    // await pluginStore.set({ key: 'grant', value: grants });
 
+    // Disable registration by default
     const advanced = await pluginStore.get({ key: 'advanced' });
     advanced.allow_register = false;
     await pluginStore.set({ key: 'advanced', value: advanced });
 
+    // Setup default permissions for authenticated role
+    const authenticated = await strapi.query('plugin::users-permissions.role').findOne({
+      where: {
+        type: 'authenticated',
+      },
+    });
+
+    for (const permission of defaultAuthenticatedPermissions) {
+      const count = await strapi.query('plugin::users-permissions.permission').count({
+        where: {
+          action: permission,
+          role: authenticated.id,
+        },
+      });
+      if (count !== 0) continue;
+
+      await strapi.query('plugin::users-permissions.permission').create({
+        data: {
+          action: permission,
+          role: authenticated.id,
+        },
+      });
+    }
+
     return res;
   };
-  // TODO: consider adding passkey grant
 
-  // TODO: either add provider somehow or write custom controller fully
   // plugin.controllers.passkey = require('./controllers/passkey');
   // plugin.routes['content-api'].routes.push({
   //   method: 'POST',
   //   path: '/auth/passkey',
   //   handler: 'passkey.callback',
+  //   config: {
+  //     middlewares: ['plugin::users-permissions.rateLimit'],
+  //     prefix: '',
+  //   },
   // });
 
   return plugin;
