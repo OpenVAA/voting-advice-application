1648588ed9b7e3e542623466634103e02645f6bd
Joonatan Korpela <joonatan.korpela@aalto.fi>
2023-11-06 14:08:02 +0200

    feat: authenticate using Strapi

---
+++ frontend/src/lib/api/candidate.ts
@@ -0,0 +1,38 @@
+import {constants} from '$lib/utils/constants';
+import {authContext} from '$lib/components/authentication/authenticationStore';
+
+export const authenticate = async (identifier: string, password: string): Promise<any> => {
+  const url = new URL(constants.BACKEND_URL);
+  url.pathname = 'auth/local';
+
+  return await fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({identifier, password})
+  });
+};
+
+export const request = async <T>(
+  endpoint: string[],
+  params: URLSearchParams = new URLSearchParams({})
+): Promise<{data: T} | undefined> => {
+  const token = authContext.token;
+  const url = new URL(constants.BACKEND_URL);
+  url.pathname = endpoint.join('/');
+  url.search = params.toString();
+
+  return await fetch(url, {
+    headers: {
+      Authorization: `Bearer ${token}`
+    }
+  })
+    .then((response) => {
+      return response.json() as Promise<{data: T} | undefined>;
+    })
+    .catch((error) => {
+      console.error('Error in getting data from backend: ', error);
+      return undefined;
+    });
+};
