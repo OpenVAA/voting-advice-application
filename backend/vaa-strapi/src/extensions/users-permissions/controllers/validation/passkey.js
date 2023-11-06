aa34f39f39a3f0f5c156e746b96471713692af5d
Stepan Fedotov <stepan.fedotov@gmail.com>
2023-11-06 14:39:19 +0200

    feat: candidate login backend

---
+++ backend/vaa-strapi/src/extensions/users-permissions/controllers/validation/passkey.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const { yup, validateYupSchema } = require('@strapi/utils');
+
+const loginSchema = yup
+  .object({
+    passkey: yup.string().required(),
+  });
+
+module.exports = {
+  validateLoginBody: validateYupSchema(loginSchema),
+};
