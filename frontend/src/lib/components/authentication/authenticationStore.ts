176286bcca5c569de82e15da66a8527c751055ba
Joonatan Korpela <joonatan.korpela@aalto.fi>
2023-11-06 14:08:02 +0200

    feat: authenticate using Strapi

--- frontend/src/lib/components/authentication/authenticationStore.ts
+++ frontend/src/lib/components/authentication/authenticationStore.ts
@@ -1,16 +1,23 @@
+import {authenticate} from '$lib/api/candidate';
 import {writable, type Writable} from 'svelte/store';
 
-export type User = string | null;
+export type User = Record<string, string | number | boolean> | null;
+
 export interface AuthContext {
   user: Writable<User>;
+  token: Writable<string | null>;
   logIn: (password: string) => Promise<boolean>;
   logOut: () => Promise<void>;
 }
 
 const userStore = writable<User>(null);
+const tokenStore = writable<string | null>(null);
 export const logIn = async (password: string) => {
-  if (password === 'password') {
-    userStore.set('user');
+  const response = await authenticate('test', password);
+  if (response.ok) {
+    const data = await response.json();
+    userStore.set(data.user);
+    tokenStore.set(data.jwt);
     return true;
   }
   return false;
@@ -18,10 +25,12 @@ export const logIn = async (password: string) => {
 
 export const logOut = async () => {
   userStore.set(null);
+  tokenStore.set(null);
 };
 
 export const authContext: AuthContext = {
   user: userStore,
+  token: tokenStore,
   logIn,
   logOut
 };
