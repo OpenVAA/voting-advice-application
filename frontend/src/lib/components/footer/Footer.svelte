d925b7b71ac4f48435c7f96648a161915b79d24f
Stepan Fedotov <TrixterTheTux@users.noreply.github.com>
2023-11-13 12:52:50 +0200

    refactor: move footer to its own component (#245)

---
+++ frontend/src/lib/components/footer/Footer.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  import {page} from '$app/stores';
+</script>
+
+<footer class="p-lg text-center text-sm text-secondary">
+  {$page.data.appLabels.viewTexts.publishedBy.replace('{{0}}', '')}
+  <img class="inline w-14" src={'/icons/publisher.svg'} alt="governmental" srcset="" />
+  Institution • {$page.data.appLabels.viewTexts.madeWith.replace('{{0}}', '')}
+  <img class="inline w-14" src="/icons/vote.svg" alt="" srcset="" /> OpenVAA
+</footer>
