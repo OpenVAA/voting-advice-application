d473dbe67c3a65e285e1fbe5a7b4979b3ea21924
Joonatan <97550588+joonatan-aatos@users.noreply.github.com>
2023-11-13 14:07:11 +0200

    feat: Show loading screen when checking if user is logged in (#248)

--- frontend/src/lib/components/authentication/authenticationStore.ts
+++ frontend/src/lib/components/authentication/authenticationStore.ts
@@ -1,24 +1,23 @@
 import {authenticate, me} from '$lib/api/candidate';
-import {get, writable, type Writable} from 'svelte/store';
+import {writable, type Writable} from 'svelte/store';
 
 export type User = Record<string, string | number | boolean> | null;
 
 export interface AuthContext {
   user: Writable<User>;
-  token: Writable<string | null>;
+  token: Writable<string | null | undefined>;
   logIn: (email: string, password: string) => Promise<boolean>;
   loadUserData: () => Promise<void>;
   logOut: () => Promise<void>;
 }
 
 const userStore = writable<User>(null);
-const tokenStore = writable<string | null>(null);
+const tokenStore = writable<string | null | undefined>(undefined);
 export const logIn = async (email: string, password: string) => {
   const response = await authenticate(email, password);
   if (!response.ok) return false;
 
   const data = await response.json();
-  userStore.set(data.user);
   tokenStore.set(data.jwt);
   localStorage.setItem('token', data.jwt);
 
@@ -27,12 +26,16 @@ export const logIn = async (email: string, password: string) => {
   return true;
 };
 
-export const loadUserData = async () => {
-  const token = get(tokenStore);
-  if (!token) tokenStore.set(localStorage.getItem('token'));
+export const loadLocalStorage = () => {
+  tokenStore.set(localStorage.getItem('token'));
+};
 
+export const loadUserData = async () => {
   const user = await me();
-  if (user.error) return;
+  if (user.error) {
+    logOut();
+    return;
+  }
 
   userStore.set(user);
 };
