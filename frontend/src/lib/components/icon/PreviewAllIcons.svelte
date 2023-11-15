94c8bd04510d464d8842c1998f5679c51be8cd44
kallelongjuhani <kalle.jarvenpaa@gmail.com>
2023-11-15 10:13:19 +0200

    feat: PreviewAllIcons component
    The `PreviewAllIcons` can be used to easily list all available icons
    together with their names. It is intended for developers.

---
+++ frontend/src/lib/components/icon/PreviewAllIcons.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+  import Icon from './Icon.svelte';
+  import {ICONS} from './icons';
+  import type {PreviewAllIconsProps} from './PreviewAllIcons.type';
+
+  type $$Props = PreviewAllIconsProps;
+
+  // We need this clunky typing to get rid of warnings below
+  const entries: [keyof typeof ICONS, readonly [string, string]][] = Object.entries(ICONS).map(
+    ([name, path]) => [name as keyof typeof ICONS, path]
+  );
+</script>
+
+<!--
+@component
+A utility component to see all the available icons at a glance. You can supply
+any valid props to the icons.
+
+### Usage
+
+```tsx
+  <PreviewAllIcons size="lg" />
+```
+-->
+
+{#each entries as [name, path]}
+  <div>
+    <Icon {name} {...$$restProps} />
+    <span>{name} (<code>svg/{path.join('/')}.ts</code>)</span>
+  </div>
+{/each}
---
+++ frontend/src/lib/components/icon/PreviewAllIcons.type.ts
@@ -0,0 +1,3 @@
+import type {IconProps} from './Icon.type';
+
+export type PreviewAllIconsProps = Omit<IconProps, 'name'>;
