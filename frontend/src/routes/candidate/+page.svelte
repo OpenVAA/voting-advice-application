d04bc9b4c5d64807f5bfe833928285b94917b806
Joonatan <97550588+joonatan-aatos@users.noreply.github.com>
2023-11-16 13:29:28 +0200

    fix: require authentication in all routes within candidate app (#254)

--- frontend/src/routes/candidate/+page.svelte
+++ frontend/src/routes/candidate/+page.svelte
@@ -1,8 +1,5 @@
 <script lang="ts">
   import {appName} from '$candidate/placeholder.json';
-  import AuthenticationProvider from '$lib/components/authentication/AuthenticationProvider.svelte';
 </script>
 
-<AuthenticationProvider>
-  <h1>Welcome to the {appName}</h1>
-</AuthenticationProvider>
+<h1>Welcome to the {appName}</h1>
