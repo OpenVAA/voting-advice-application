360803d4b92b5a9bbee798c53911953c8c3f336c
kallelongjuhani <kalle.jarvenpaa@gmail.com>
2023-11-18 10:19:02 +0200

    feat: AppLogo template part
    The `AppLogo` template part can be used to place the application's
    logo. It currently handles switching between light and dark modes and
    offers three predefined sizes in which the logo can be used. There's
    also an `inverse` property, which will reverse the colors and can be
    used when the logo is set on a dark background.
    
    In the future, the component should also allow the use of an image file
    defined in Strapi.

---
+++ frontend/src/lib/templates/parts/appLogo/AppLogo.svelte
@@ -0,0 +1,62 @@
+<script lang="ts">
+  import normalLogo from './svg/openvaa-logo-grey.svg';
+  import inverseLogo from './svg/openvaa-logo-white.svg';
+  import type {AppLogoProps} from './AppLogo.type';
+
+  type $$Props = AppLogoProps;
+  export let alt: $$Props['alt'];
+  export let inverse: $$Props['inverse'] = false;
+  export let size: $$Props['size'] = 'md';
+
+  // Create class names
+  let classes: string;
+
+  // Predefined sizes
+  switch (size) {
+    case 'sm':
+      classes = 'h-20 pt-2';
+      break;
+    case 'lg':
+      classes = 'h-32 pt-6';
+      break;
+    default:
+      classes = 'h-24 pt-4';
+  }
+
+  // Merge classes into restProps
+  $$restProps.class = `${classes} ${$$restProps.class ?? ''}`;
+
+  // TODO: Use logo files defined in Strapi.
+  // TODO: Render only one image, see issue #279
+</script>
+
+<!--
+@component
+A template part that is used to show the application's logo. The logo 
+colour changes dynamically based on whether the light or dark mode is active.
+
+### Properties
+
+- `alt`: The `alt` text for the logo image.
+- `inverse`: If `true`, the light and dark versions of the logo will be reversed.
+  Set to `true` if using the logo on a dark background. @default `false`
+- `size`: The size of the logo as one of the predefined sizes 'sm', 'md' or 'lg'.
+  For arbitrary values, you can supply a `class` attribute, such as 
+  `class="h-[3.5rem]"`. @default `'md'`
+- Any valid attributes of the `<div>` element wrapping the light and dark
+  `<img>` elements
+
+### Usage
+
+```tsx
+<AppLogo size="lg"/>
+<div class="bg-primary">
+  <AppLogo inverse/>
+</div>
+```
+-->
+
+<div {...$$restProps}>
+  <img src={normalLogo} {alt} class="h-full {inverse ? 'hidden dark:block' : 'dark:hidden'}" />
+  <img src={inverseLogo} {alt} class="h-full {inverse ? 'dark:hidden' : 'hidden dark:block'}" />
+</div>
---
+++ frontend/src/lib/templates/parts/appLogo/AppLogo.type.ts
@@ -0,0 +1,22 @@
+import type {SvelteHTMLElements} from 'svelte/elements';
+export type AppLogoProps = SvelteHTMLElements['div'] & {
+  /**
+   * The `alt` text for the logo image.
+   */
+  alt: string;
+  /**
+   * If `true`, the light and dark versions of the logo will be reversed.
+   * Set to `true` if using the logo on a dark background.
+   *
+   * @default false
+   */
+  inverse?: boolean | null;
+  /**
+   * The size of the logo as one of the predefined sizes 'sm', 'md' or 'lg'.
+   * For arbitrary values, you can supply a `class` attribute, such as
+   * class="h-[3.5rem]"`.
+   *
+   * @default 'md'
+   */
+  size?: 'sm' | 'md' | 'lg' | null;
+};
