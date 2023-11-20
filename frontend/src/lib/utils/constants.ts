3c585613950e2c2f29f9b8ef8828f9ec6db9ee95
Johanna Laitila <johanna.maria.laitila@gmail.com>
2023-11-20 14:22:04 +0200

    fix: Make candidate app login work on production build (#267)

--- frontend/src/lib/utils/constants.ts
+++ frontend/src/lib/utils/constants.ts
@@ -1,6 +1,7 @@
+import {env} from '$env/dynamic/public';
+
 export const constants: Record<string, string> = {
   BACKEND_URL: import.meta.env.VITE_BACKEND_URL || process.env.VITE_BACKEND_URL || '',
-  PUBLIC_BACKEND_URL:
-    import.meta.env.VITE_PUBLIC_BACKEND_URL || process.env.VITE_PUBLIC_BACKEND_URL || '',
+  PUBLIC_BACKEND_URL: env.PUBLIC_BACKEND_URL || '', // Accessed by the client-side, so must be loaded dynamically
   STRAPI_TOKEN: import.meta.env.VITE_STRAPI_TOKEN || process.env.VITE_STRAPI_TOKEN || ''
 };
