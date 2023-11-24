90213c094953d916813b4cfccc9d0f4fb10ad778
kallelongjuhani <kalle.jarvenpaa@gmail.com>
2023-11-24 21:50:46 +0200

    feat: HeadingGroup component
    The `HeadingGroup` component that can be used instead of a `<hgroup>`
    to group a pre-heading and `<h1>` in a way that's as accessible as
    possible.
    
    The component will be used on many page types, such as those using
    the `BasicPage` and `FrontPage` templates.

---
+++ frontend/src/lib/components/headingGroup/HeadingGroup.type.ts
@@ -0,0 +1,15 @@
+import type {AriaRole, SvelteHTMLElements} from 'svelte/elements';
+export type HeadingGroupProps = SvelteHTMLElements['hgroup'] & {
+  /**
+   * The Aria role description of the `<hgroup>` element.
+   *
+   * @default $_('aria.headingGroup')
+   */
+  'aria-roledescription'?: string | null;
+  /**
+   * The Aria role of the `<hgroup>` element.
+   *
+   * @default 'group'
+   */
+  role?: AriaRole | null;
+};
