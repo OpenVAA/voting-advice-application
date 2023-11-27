70a5dec7fa4b85ab98d611d801e45a00cc349f32
Antti Pekkanen <antti.o.pekkanen@aalto.fi>
2023-11-27 18:33:12 +0200

    feat: registration page uses URL parameters
    If user navigates to registration page without the required URL
    parameter, a page where user can input the registration code is shown.

--- frontend/src/routes/candidate/register/+page.svelte
+++ frontend/src/routes/candidate/register/+page.svelte
@@ -1,5 +1,18 @@
-<script>
+<script lang="ts">
+  import {page} from '$app/stores';
+  import PasswordSetPage from './PasswordSetPage.svelte';
   import RegistrationCodePage from './RegistrationCodePage.svelte';
+
+  const registrationCode = $page.url.searchParams.get('registrationCode');
+
+  // TODO: replace with the real function to check the registration code
+  const checkRegistrationCode = (registrationCode: string) => {
+    return registrationCode === 'true';
+  };
 </script>
 
-<RegistrationCodePage />
+{#if registrationCode && checkRegistrationCode(registrationCode)}
+  <PasswordSetPage />
+{:else}
+  <RegistrationCodePage />
+{/if}
