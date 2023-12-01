6733d2a305b985a681e5acece10b28250c8e5983
Joonatan <97550588+joonatan-aatos@users.noreply.github.com>
2023-12-01 12:16:09 +0200

    feat: Refactor authentication provider and add documentation (#270)
    * feat: Refactor authentication provider and add documentation
    
    * chore: Localize logout modal text
    
    * feat: Add README.md to components/authentication

--- frontend/src/lib/components/authentication/AuthenticationProvider.svelte
+++ frontend/src/lib/components/authentication/AuthenticationProvider.svelte
@@ -1,10 +1,8 @@
 <script lang="ts">
-  import LoginPage from './LoginPage.svelte';
-  import {authContext, loadLocalStorage} from '$lib/components/authentication/authenticationStore';
+  import {authContext, loadLocalStorage} from '$lib/utils/authenticationStore';
   import {onMount, setContext} from 'svelte';
 
   setContext('auth', authContext);
-  const user = authContext.user;
   const token = authContext.token;
 
   onMount(() => {
@@ -13,12 +11,25 @@
   });
 </script>
 
-{#if $user}
-  <slot />
-{:else if $token === undefined || ($token && !$user)}
-  <div class="mt-100 flex h-screen flex-col items-center">
-    <span class="loading loading-spinner loading-lg" />
-  </div>
-{:else}
-  <LoginPage />
-{/if}
+<!--
+@component
+Authentication provider provides the authentication context to all its children.
+
+### Contexts
+
+- auth: The authentication context
+
+### Slots
+
+- default: The children of the component
+
+### Usage
+
+```tsx
+<AuthenticationProvider>
+  <p>Example content</p>
+</AuthenticationProvider>
+```
+-->
+
+<slot />
