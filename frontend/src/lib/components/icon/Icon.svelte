490a605b273c8b1cf2fa6bf9490ce797e369f8e9
kallelongjuhani <kalle.jarvenpaa@gmail.com>
2023-12-13 14:41:19 +0200

    fix: reactivity issues in the Icon component
    Fixes issues caused by `$$restProps` merging as well as loss of
    reactivity when changing certain properties such as `name`.

--- frontend/src/lib/components/icon/Icon.svelte
+++ frontend/src/lib/components/icon/Icon.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-  import {onMount} from 'svelte';
+  import {concatClass} from '$lib/utils/components';
   import type {IconProps} from './Icon.type';
   import {ICONS} from './icons';
 
@@ -8,44 +8,40 @@
   export let size: $$Props['size'] = 'md';
   export let color: $$Props['color'] = 'current';
 
-  // Validate name and split path
-  // We need this part-wise approach because of Vite's dynamic import limitations
-  // https://github.com/rollup/plugins/tree/master/packages/dynamic-import-vars#limitations
-  const [folder, filename] = name in ICONS ? ICONS[name] : ['custom', 'missing_icon'];
-
   // Load svg contents
   let svgElement: SVGElement;
-  // The import happens inside onMount to make sure the SVG element is available
-  onMount(() =>
-    import(`./svg/${folder}/${filename}.ts`).then((svg) => {
-      if (svgElement) {
-        svgElement.innerHTML = svg.default;
-      }
-    })
-  );
+  // Pass svgElement and name explicitly to trigger correct reactivity
+  $: loadSvg(svgElement, name);
+
+  function loadSvg(svgElement: SVGElement, name: $$Props['name']) {
+    if (!svgElement || !name) return;
+    // Validate name and split path
+    // We need this part-wise approach because of Vite's dynamic import limitations
+    // https://github.com/rollup/plugins/tree/master/packages/dynamic-import-vars#limitations
+    const [folder, filename] = name in ICONS ? ICONS[name] : ['custom', 'missing_icon'];
+    import(`./svg/${folder}/${filename}.ts`).then((svg) => (svgElement.innerHTML = svg.default));
+  }
 
   // Create class names
-  let classes = 'inline-block';
-
-  // Predefined sizes
-  switch (size) {
-    case 'sm':
-      classes += ' h-16 w-16';
-      break;
-    case 'lg':
-      classes += ' h-32 w-32';
-      break;
-    default:
-      classes += ' h-24 w-24';
+  let classes: string;
+  $: {
+    classes = 'inline-block';
+    // Predefined sizes
+    switch (size) {
+      case 'sm':
+        classes += ' h-16 w-16';
+        break;
+      case 'lg':
+        classes += ' h-32 w-32';
+        break;
+      default:
+        classes += ' h-24 w-24';
+    }
+    // Set fill color
+    if (color != null) {
+      classes += ` fill-${color}`;
+    }
   }
-
-  // Set fill color
-  if (color != null) {
-    classes += ` fill-${color}`;
-  }
-
-  // Merge classes into $$restProps
-  $$restProps.class = `${classes} ${$$restProps.class ?? ''}`;
 </script>
 
 <!--
@@ -56,9 +52,22 @@ Use the other properties to set the size and color of the icon. The icon
 is `aria-hidden` by default, but that can overriden. You can also pass 
 any valid attributes of the `<svg>` element.
 
+NB! The component does not currently support reactive updates to `name`,
+`size` or `color`. Use within a `#key` block if you need to change these.
+
 ### Properties
 
-- See `Icon.type.ts` or code completion info.
+- `name`: the name of the icon to use
+- `size`: The size of the icon as one of the predefined sizes 'sm', 'md' or 'lg'.
+   For arbitrary values, you can supply a `class` property, such as 
+   `h-[3.15rem] w-[3.15rem]`. @default 'md'
+- `color`: The color of the icon as one of the predefined colours.
+   For arbitrary values, you can supply a `class` property, such as
+   `fill-[#123456]`. @default 'current'
+- `aria-hidden`: @default `true`
+- `role`: Aria role @default `img`
+- `class`: Additional class string to append to the element's default classes.
+- Any valid attributes of a `<svg>` element
 
 ### Usage
 
@@ -71,8 +80,8 @@ any valid attributes of the `<svg>` element.
 
 <svg
   bind:this={svgElement}
-  role="img"
   aria-hidden="true"
+  role="img"
   xmlns="http://www.w3.org/2000/svg"
   viewBox="0 0 24 24"
-  {...$$restProps} />
+  {...concatClass($$restProps, classes)} />
