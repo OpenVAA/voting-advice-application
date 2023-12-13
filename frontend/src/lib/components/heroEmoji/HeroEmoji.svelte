f3c66e9b7ceeffc80c56c5389109b618fbf2c922
kallelongjuhani <kalle.jarvenpaa@gmail.com>
2023-12-13 14:43:07 +0200

    fix: reactivity issues in the HeroEmoji component
    Fixes issues caused by `$$restProps` merging.

--- frontend/src/lib/components/heroEmoji/HeroEmoji.svelte
+++ frontend/src/lib/components/heroEmoji/HeroEmoji.svelte
@@ -1,12 +1,8 @@
 <script lang="ts">
+  import {concatClass} from '$lib/utils/components';
   import type {HeroEmojiProps} from './HeroEmoji.type';
 
   type $$Props = HeroEmojiProps;
-
-  // Merge the necessary classes to possible extra classes defined in the props
-  $$restProps.class = `whitespace-nowrap truncate text-clip text-center font-emoji text-[6.5rem] leading-none ${
-    $$restProps.class ?? ''
-  }`;
 </script>
 
 <!--
@@ -25,6 +21,9 @@ using the `class` attribute, e.g. `class="text-[10rem]"`.
 
 ### Properties
 
+- `aria-hidden`: @default `true`
+- `role`: Aria role @default `img`
+- `class`: Additional class string to append to the element's default classes.
 - Any valid attributes of a `<div>` element.
 
 ### Slots
@@ -38,4 +37,12 @@ using the `class` attribute, e.g. `class="text-[10rem]"`.
 ```
 -->
 
-<div role="img" aria-hidden="true" {...$$restProps}><slot /></div>
+<div
+  aria-hidden="true"
+  role="img"
+  {...concatClass(
+    $$restProps,
+    'whitespace-nowrap truncate text-clip text-center font-emoji text-[6.5rem] leading-none'
+  )}>
+  <slot />
+</div>
