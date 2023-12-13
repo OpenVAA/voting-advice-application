b1ac8941cc17897cb4e38fbaa503ae006fdc384e
kallelongjuhani <kalle.jarvenpaa@gmail.com>
2023-12-13 14:42:12 +0200

    fix: reactivity issues in the HeadingGroup component family
    Fixes issues caused by `$$restProps` merging.

--- frontend/src/lib/components/headingGroup/PreHeading.type.ts
+++ frontend/src/lib/components/headingGroup/PreHeading.type.ts
@@ -1,10 +1,2 @@
 import type {SvelteHTMLElements} from 'svelte/elements';
-export type PreHeadingProps = SvelteHTMLElements['p'] & {
-  /**
-   * The Aria role description of the `<p>` element representing
-   * the pre-title.
-   *
-   * @default $_('aria.preHeading')
-   */
-  'aria-roledescription'?: string | null;
-};
+export type PreHeadingProps = SvelteHTMLElements['p'];
