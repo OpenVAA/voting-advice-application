cd79a95803231d333b1f3970832582162b7de397
Antti Pekkanen <74378567+anolpe@users.noreply.github.com>
2023-12-18 14:31:27 +0200

    refactor: new components to candidate app
    * refactor: Candidate navigation
    
    * fix: registration page refreshing
    
    Refreshing registration page after inputting registration code caused frontend to crash. This was fixed by disabling server-side rendering for registration page.
    
    * refactor: use page templates in candidate app
    
    * refactor: logout button component
    
    ---------
    
    Co-authored-by: Samuli Yli-Salomäki <samuliys@gmail.com>

--- frontend/src/lib/templates/page/parts/CandidateNav.svelte
+++ frontend/src/lib/templates/page/parts/CandidateNav.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
   import {type NavigationProps, Navigation, NavGroup, NavItem} from '$lib/components/navigation';
+  import {candidateAppRoute} from '$candidate/placeholder.json';
+  import {_} from 'svelte-i18n';
 
   type $$Props = NavigationProps;
 </script>
@@ -27,6 +29,39 @@ A template part that outputs the navigation menu for the Candidate App for use i
 <Navigation slot="nav" on:navFocusOut {...$$restProps}>
   <slot />
   <NavGroup>
-    <NavItem href="/" text="Candidate nav to be updated!" />
+    <NavItem href={candidateAppRoute} icon="home" text={$_('candidateApp.navbar.start')} />
+    <NavItem
+      href={`${candidateAppRoute}/profile`}
+      icon="profile"
+      text={$_('candidateApp.navbar.basicInfo')} />
+    <NavItem
+      href={`${candidateAppRoute}/opinions`}
+      icon="optinion"
+      text={$_('candidateApp.navbar.yourOpinions')} />
+    <NavItem
+      href={`${candidateAppRoute}/settings`}
+      icon="settings"
+      text={$_('candidateApp.navbar.settings')} />
+    <NavItem
+      href={`${candidateAppRoute}/preview`}
+      icon="previewProfile"
+      text={$_('candidateApp.navbar.preview')} />
+    <NavItem href={`${candidateAppRoute}/help`} icon="help" text={$_('candidateApp.navbar.help')} />
+  </NavGroup>
+  <NavGroup>
+    <NavItem
+      href={`${candidateAppRoute}/electioninfo`}
+      icon="info"
+      text={$_('candidateApp.navbar.electionInformation')} />
+    <NavItem
+      href={`${candidateAppRoute}/faq`}
+      icon="info"
+      text={$_('candidateApp.navbar.useInformation')} />
+  </NavGroup>
+  <NavGroup>
+    <NavItem
+      href={`${candidateAppRoute}/feedback`}
+      icon="feedback"
+      text={$_('candidateApp.navbar.feedback')} />
   </NavGroup>
 </Navigation>
