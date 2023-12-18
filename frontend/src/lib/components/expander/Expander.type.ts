a2543c44ed653cba07a0bdbe62eae79a11fa8212
norppaa <99613396+norppaa@users.noreply.github.com>
2023-12-18 21:52:47 +0200

    feat: create expander component (#309)
    * feat: create expander component
    
    * fix: requested changes: add comment and remove intersetion when defining variant

---
+++ frontend/src/lib/components/expander/Expander.type.ts
@@ -0,0 +1,37 @@
+import type {Color} from '$lib/components/shared/colors';
+
+export type ExpanderProps = {
+  /**
+   * Title is seen as the text in the expander's visible part, and it is mandatory.
+   * Title will also be used as a 'aria-label' for a checkbow on which the
+   * expander operates on.
+   */
+  title: string;
+  /**
+   * The color of the next-icon that is used in the expander.
+   *
+   * @default 'primary'
+   */
+  iconColor?: Color | null;
+  /**
+   * The position of the next-icon that is used in the expander.
+   *
+   * @default 'text'
+   */
+  iconPos?: string | null;
+  /**
+   * Variable with which to configure the expanders title if no variants
+   * are in use.
+   */
+  customizeTitle?: string | null;
+  /**
+   * Variable with which to configure the expanders content if no variants
+   * are in use.
+   */
+  customizeContent?: string | null;
+
+  /**
+   * Variable used to define a variant for the expander.
+   */
+  variant: 'read-more' | 'category' | 'question' | 'unansweared-question' | null;
+};
