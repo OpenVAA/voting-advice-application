5e626190d818ff234e0e68f2c0ca5dfee01d9a14
Antti Pekkanen <74378567+anolpe@users.noreply.github.com>
2023-12-18 14:31:27 +0200

    refactor: new components to candidate app
    * refactor: Candidate navigation
    
    * fix: registration page refreshing
    
    Refreshing registration page after inputting registration code caused frontend to crash. This was fixed by disabling server-side rendering for registration page.
    
    * refactor: use page templates in candidate app
    
    * refactor: logout button component
    
    ---------
    
    Co-authored-by: Samuli Yli-Salomäki <samuliys@gmail.com>

--- frontend/src/routes/candidate/(protected)/+layout.svelte
+++ frontend/src/routes/candidate/(protected)/+layout.svelte
@@ -1,10 +1,7 @@
 <script lang="ts">
-  import Navigation from '$lib/components/navigation/Navigation.svelte';
   import AuthenticationProvider from '$lib/components/authentication/AuthenticationProvider.svelte';
 </script>
 
 <AuthenticationProvider>
-  <Navigation>
-    <slot />
-  </Navigation>
+  <slot />
 </AuthenticationProvider>
--- frontend/src/routes/candidate/(protected)/+page.svelte
+++ frontend/src/routes/candidate/(protected)/+page.svelte
@@ -1,8 +1,14 @@
 <script lang="ts">
   import {appName} from '$candidate/placeholder.json';
+  import {LogoutButton} from '$lib/candidate/components/logoutButton';
   import RequireLogin from '$lib/components/authentication/RequireLogin.svelte';
+  import {BasicPage} from '$lib/templates/basicPage';
 </script>
 
 <RequireLogin showLogin>
-  <h1>Welcome to the {appName}</h1>
+  <BasicPage title="Welcome to the {appName}">
+    <svelte:fragment slot="banner">
+      <LogoutButton />
+    </svelte:fragment>
+  </BasicPage>
 </RequireLogin>
