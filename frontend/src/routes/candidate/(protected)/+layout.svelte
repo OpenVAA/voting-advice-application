6d29ff3cb29f629416894ad2c9241637350f3da9
Antti Pekkanen <74378567+anolpe@users.noreply.github.com>
2023-12-18 14:31:27 +0200

    refactor: new components to candidate app
    * refactor: Candidate navigation
    
    * fix: registration page refreshing
    
    Refreshing registration page after inputting registration code caused frontend to crash. This was fixed by disabling server-side rendering for registration page.
    
    * refactor: use page templates in candidate app
    
    * refactor: logout button component
    
    ---------
    
    Co-authored-by: Samuli Yli-Salomäki <samuliys@gmail.com>

--- frontend/src/routes/candidate/(protected)/+layout.svelte
+++ frontend/src/routes/candidate/(protected)/+layout.svelte
@@ -1,10 +1,7 @@
 <script lang="ts">
-  import Navigation from '$lib/candidate/Navigation.svelte';
   import AuthenticationProvider from '$lib/components/authentication/AuthenticationProvider.svelte';
 </script>
 
 <AuthenticationProvider>
-  <Navigation>
-    <slot />
-  </Navigation>
+  <slot />
 </AuthenticationProvider>
