4c582d9783962a966a66d7291e25dfc93071a0ce
kallelongjuhani <kalle.jarvenpaa@gmail.com>
2024-01-28 19:53:34 +0200

    feat: show answers in CandidateOpinions component
    This a quick update to the hitherto unimplemented `CandidateOpinions`
    component that uses the new Question sub-components to show both the
    Voter's and the Candidate's answers to the questions.
    
    This may be updated in the future, but it serves as an example of the
    sub-components' usage.

--- frontend/src/lib/components/candidates/CandidateOpinions.svelte
+++ frontend/src/lib/components/candidates/CandidateOpinions.svelte
@@ -1,9 +1,73 @@
 <script lang="ts">
-  // TODO: Implement this component
-  // export let candidate: CandidateProps;
+  import {_} from 'svelte-i18n';
+  import {page} from '$app/stores';
+  import {answeredQuestions} from '$lib/utils/stores';
+  import {LikertResponseButtons} from '$lib/components/questions';
+  import {HeadingGroup, PreHeading} from '$lib/components/headingGroup';
+
+  export let candidate: CandidateProps;
+
+  let questions: QuestionProps[];
+  $: questions = $page.data.questions;
+
+  const shortName = `${candidate.firstName[0].toLocaleUpperCase()}. ${candidate.lastName}`;
+
+  function getAnswer(questionId: string) {
+    const match = candidate.answers?.find((a) => a.questionId === questionId);
+    return {
+      answer: match?.answer,
+      openAnswer: match?.openAnswer
+    };
+  }
 </script>
 
-<div class="p-lg">
-  <img class="mx-auto" src="/images/error.svg" alt="Under construction" />
-  <h2 class="text-center">Under construction...</h2>
-</div>
+<section class="p-lg">
+  {#each questions as question}
+    {@const {id, text, type, options, category, info} = question}
+    {@const {answer, openAnswer} = getAnswer(id)}
+    {@const voterAnswer = $answeredQuestions[id]}
+    {@const headingId = `questionHeading-${id}`}
+
+    <div class="mb-60 mt-20">
+      <HeadingGroup id={headingId} class="mb-lg text-center">
+        <PreHeading class="text-accent">{category}</PreHeading>
+        <h3>{text}</h3>
+      </HeadingGroup>
+
+      {#if voterAnswer == null && answer == null}
+        <div class="small-label mb-16 text-center">
+          {$_('questions.bothHaventAnswered').replace('{{candidate}}', shortName)}
+        </div>
+      {:else if voterAnswer == null}
+        <div class="small-label mb-16 text-center">
+          {$_('questions.youHaventAnswered')}
+        </div>
+      {:else if answer == null}
+        <div class="small-label mb-16 text-center">
+          {$_('questions.candidateHasntAnswered').replace('{{candidate}}', shortName)}
+        </div>
+      {/if}
+
+      {#if type === 'Likert'}
+        <LikertResponseButtons
+          aria-labelledby={headingId}
+          name={id}
+          mode="display"
+          selectedKey={voterAnswer}
+          entityKey={answer}
+          entityLabel={shortName}
+          {options} />
+      {:else}
+        {$_('error.general')}
+      {/if}
+
+      {#if openAnswer}
+        <div
+          class="mt-16 rounded-md bg-base-200 p-md text-center
+          before:content-[open-quote] after:content-[close-quote]">
+          {openAnswer}
+        </div>
+      {/if}
+    </div>
+  {/each}
+</section>
