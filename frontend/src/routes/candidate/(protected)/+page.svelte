7bafa6f2a63ea664f8a3a8acf1c15e8a180244da
Noora Puhakka <noora.puhakka02@gmail.com>
2024-02-05 15:24:40 +0200

    feat: move logout button into basic page

--- frontend/src/routes/candidate/(protected)/+page.svelte
+++ frontend/src/routes/candidate/(protected)/+page.svelte
@@ -1,9 +1,6 @@
 <script lang="ts">
-  import {LogoutButton} from '$lib/candidate/components/logoutButton';
   import {BasicPage} from '$lib/templates/basicPage';
 </script>
 
 <!-- TODO: Add i18n translations -->
-<BasicPage title="Welcome to the Candidate App">
-  <LogoutButton slot="banner" />
-</BasicPage>
+<BasicPage title="Welcome to the Candidate App" />
