c8a12bc13d8eda161f5f6565960e1307a90ef3d7
puhakkn2 <noora.puhakka@aalto.fi>
2024-02-12 15:58:51 +0200

    fix: fix accesibility

--- frontend/src/routes/candidate/(protected)/questions/all/+page.svelte
+++ frontend/src/routes/candidate/(protected)/questions/all/+page.svelte
@@ -9,22 +9,19 @@
   import Icon from '$lib/components/icon/Icon.svelte';
   import {_} from 'svelte-i18n';
 
-  const questions = $page.data.questions;
-  const categories = [...new Set(questions.map((question) => question.category))];
-  // let categories = questions.map((question) => question.category);
-  // categories = [...new Set(categories)];
-
-  let nofUnasweredQuestions = 0;
-
   /**
    * A small delay before moving to the unaswered question.
    * TODO: Make this a global variable used throughout the app.
    */
   const DELAY_M_MS = 350;
 
+  const questions = $page.data.questions;
+  const categories = [...new Set(questions.map((question) => question.category))];
+
   const store = answerContext.answers;
   $: answerStore = $store;
 
+  let nofUnasweredQuestions = 0;
   $: nofUnasweredQuestions = questions.length - Object.entries(answerStore).length;
 
   function gotoToQuestion(id: string) {
@@ -49,8 +46,10 @@
     {#each categories as category}
       <div class="edgetoedge-x">
         <Expander title={category || ''} variant="category">
-          {#each questions as question}
+          {#each questions as question, i}
+            <!-- Show questions based on categories -->
             {#if category === question.category}
+              <!-- Question has been answered -->
               {#if answerStore[question.id]}
                 <div class="pb-20 pt-20">
                   <div class="text-accent">
@@ -62,25 +61,29 @@
                   </Expander>
 
                   <div class="pt-10">
+                    <!-- This gives empty form label error for every empty dot, but fix should come from LikertResponseButton -->
                     <LikertResponseButtons
-                      aria-labelledby={question.text}
                       name={question.id}
+                      mode="display"
                       options={question.options}
                       selectedKey={answerStore[question.id].key} />
 
                     {#if answerStore[question.id].openAnswer}
-                      <label for="openAnswer" class="text-m uppercase"
-                        >{$_('candidateApp.allQuestions.commentOnThisIssue')}
-                      </label>
-                      <textarea
-                        bind:value={answerStore[question.id].openAnswer}
-                        disabled={true}
-                        id="openAnswer"
-                        rows="4"
-                        class="textarea textarea-primary w-full" />
+                      <div class="pt-10">
+                        <label for="openAnswer{i}" class="text-m uppercase"
+                          >{$_('candidateApp.allQuestions.commentOnThisIssue')}
+                        </label>
+                        <textarea
+                          bind:value={answerStore[question.id].openAnswer}
+                          disabled={true}
+                          id="openAnswer{i}"
+                          rows="4"
+                          class="textarea textarea-primary w-full" />
+                      </div>
                     {/if}
                   </div>
                 </div>
+                <!-- Question not yet answered -->
               {:else}
                 <div class="pt-30 pb-20">
                   <div class="text-accent">
