75584d047e7c77ec6730c6a09e81556beefa7a87
puhakkn2 <noora.puhakka@aalto.fi>
2024-02-13 15:12:05 +0200

    fix: code quality

--- frontend/src/routes/candidate/(protected)/questions/all/+page.svelte
+++ frontend/src/routes/candidate/(protected)/questions/all/+page.svelte
@@ -5,15 +5,8 @@
   import {Expander} from '$lib/components/expander';
   import LikertResponseButtons from '$lib/components/questions/LikertResponseButtons.svelte';
   import {answerContext} from '$lib/utils/answerStore';
-  import {goto} from '$app/navigation';
   import Icon from '$lib/components/icon/Icon.svelte';
   import {_} from 'svelte-i18n';
-
-  /**
-   * A small delay before moving to the unaswered question.
-   * TODO: Make this a global variable used throughout the app.
-   */
-  const DELAY_M_MS = 350;
 
   const questions = $page.data.questions;
   const categories = [...new Set(questions.map((question) => question.category))];
@@ -23,11 +16,6 @@
 
   let nofUnasweredQuestions = 0;
   $: nofUnasweredQuestions = questions.length - Object.entries(answerStore).length;
-
-  function gotoToQuestion(id: string) {
-    const questionUrl = $page.url.pathname.replace(/\/all$/, '/' + id);
-    setTimeout(() => goto(questionUrl), DELAY_M_MS);
-  }
 </script>
 
 <BasicPage title={$_('candidateApp.allQuestions.title')}>
@@ -42,78 +30,72 @@
     {$_('candidateApp.allQuestions.info')}
   </p>
 
-  <p>
-    {#each categories as category}
-      <div class="edgetoedge-x">
-        <Expander title={category || ''} variant="category">
-          {#each questions as question, i}
-            <!-- Show questions based on categories -->
-            {#if category === question.category}
-              <!-- Question has been answered -->
-              {#if answerStore[question.id]}
-                <div class="pb-20 pt-20">
-                  <div class="text-accent">
-                    {question.category}
-                  </div>
-
-                  <Expander title={question.text || ''} variant="question">
-                    {question.info}
-                  </Expander>
-
-                  <div class="pt-10">
-                    <!-- This gives empty form label error for every empty dot, but fix should come from LikertResponseButton -->
-                    <LikertResponseButtons
-                      name={question.id}
-                      mode="display"
-                      options={question.options}
-                      selectedKey={answerStore[question.id].key} />
-
-                    {#if answerStore[question.id].openAnswer}
-                      <div class="pt-10">
-                        <label for="openAnswer{i}" class="text-m uppercase"
-                          >{$_('candidateApp.allQuestions.commentOnThisIssue')}
-                        </label>
-                        <textarea
-                          bind:value={answerStore[question.id].openAnswer}
-                          disabled={true}
-                          id="openAnswer{i}"
-                          rows="4"
-                          class="textarea textarea-primary w-full" />
-                      </div>
-                    {/if}
-                  </div>
+  {#each categories as category}
+    <div class="edgetoedge-x">
+      <Expander title={category || ''} variant="category">
+        {#each questions as question, i}
+          <!-- Show questions based on categories -->
+          {#if category === question.category}
+            <!-- Question has been answered -->
+            {#if answerStore[question.id]}
+              <div class="pb-20 pt-20">
+                <div class="text-accent">
+                  {question.category}
                 </div>
-                <!-- Question not yet answered -->
-              {:else}
-                <div class="pt-30 pb-20">
-                  <div class="text-accent">
-                    {question.category}
-                  </div>
 
-                  <Expander
-                    title={question.text || ''}
-                    variant="question"
-                    titleClass="text-warning">
-                    {question.info}
-                  </Expander>
+                <Expander title={question.text || ''} variant="question">
+                  {question.info}
+                </Expander>
 
-                  <div class="flex justify-center pt-10">
-                    <Button
-                      on:click={() => gotoToQuestion(question.id)}
-                      variant="main"
-                      text={$_('candidateApp.allQuestions.answerButton')} />
-                  </div>
+                <div class="pt-10">
+                  <!-- This gives empty form label error from Wave Extension for every empty dot, but fix should come from LikertResponseButton -->
+                  <LikertResponseButtons
+                    name={question.id}
+                    mode="display"
+                    options={question.options}
+                    selectedKey={answerStore[question.id].key} />
+
+                  {#if answerStore[question.id].openAnswer}
+                    <div class="pt-10">
+                      <label for="openAnswer{i}" class="text-m uppercase"
+                        >{$_('candidateApp.allQuestions.commentOnThisIssue')}
+                      </label>
+                      <textarea
+                        bind:value={answerStore[question.id].openAnswer}
+                        disabled
+                        id="openAnswer{i}"
+                        rows="4"
+                        class="textarea textarea-primary w-full" />
+                    </div>
+                  {/if}
                 </div>
-              {/if}
+              </div>
+              <!-- Question not yet answered -->
+            {:else}
+              <div class="pt-30 pb-20">
+                <div class="text-accent">
+                  {question.category}
+                </div>
+
+                <Expander title={question.text || ''} variant="question" titleClass="text-warning">
+                  {question.info}
+                </Expander>
+
+                <!-- Navigate to unsanswered question -->
+                <a class="flex justify-center pt-10" href="/candidate/questions/{question.id}">
+                  <Button variant="main" text={$_('candidateApp.allQuestions.answerButton')} />
+                </a>
+              </div>
             {/if}
-          {/each}
-        </Expander>
-      </div>
-    {/each}
-  </p>
+          {/if}
+        {/each}
+      </Expander>
+    </div>
+  {/each}
 </BasicPage>
 
 <style>
+  /* Hotfix for making the expander span the whole width of the page */
   .edgetoedge-x {
     padding-left: 0;
     padding-right: 0;
