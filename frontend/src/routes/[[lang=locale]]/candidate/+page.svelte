3703fabd55f7e22b4f4bec6c92963002641bcf62
kallelongjuhani <kalle.jarvenpaa@gmail.com>
2024-02-17 09:58:12 +0200

    chore: remove test route

--- frontend/src/routes/[[lang=locale]]/candidate/+page.svelte
+++
@@ -1,5 +0,0 @@
-<script lang="ts">
-  import {appName} from '$candidate/placeholder.json';
-</script>
-
-<h1>Welcome to the {appName}</h1>
--- frontend/src/routes/_test/+page.svelte
+++
@@ -1,7 +0,0 @@
-<script lang="ts">
-  const testA = [1, 2, 3];
-  // Test ternary formatting
-  const test = testA ? testA.filter((a) => a > 2).map((a) => a * 2) : [];
-</script>
-
-Foo {test}
