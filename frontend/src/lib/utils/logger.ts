2c0cdbc725835cc4ffa9a7633f6cc675cd94aaf4
kallelongjuhani <kalle.jarvenpaa@gmail.com>
2024-02-17 08:57:14 +0200

    fix: missing error logging in logDebugError

--- frontend/src/lib/utils/logger.ts
+++ frontend/src/lib/utils/logger.ts
@@ -1,10 +1,10 @@
 /**
  * Allows debug messages to be logged in development environment, but filtered out for production.
- * @param message Message to log into console
+ * @param message Message or object to log into console
  * @param error Potential error message to print out completely
  */
-export function logDebugError(message: string, error: unknown = null) {
+export function logDebugError(message: unknown, error: unknown = null) {
   if (import.meta.env.DEV) {
-    error ? console.error(message, error) : console.error(message);
+    error ? console.error(message, error) : console.info(message);
   }
 }
