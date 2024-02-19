1cb5ce982a2dca09834e87cefde3587964250c45
kallelongjuhani <kalle.jarvenpaa@gmail.com>
2024-02-19 09:38:23 +0200

    feat: updated i18n api for Candidate App
    Localized strings are now `json` fields, which is now reflected in the
    types used in the Candidate App.

--- frontend/src/lib/utils/answerStore.ts
+++ frontend/src/lib/utils/answerStore.ts
@@ -9,40 +9,15 @@ export interface AnswerContext {
 export interface Answer {
   id: string; // Id of the answer in the database
   key: AnswerOption['key']; // Selected answer option
-  openAnswer: string; // Optional free-form answer
-}
-
-// Interfaces for data returned from the API
-interface AnswerAttributes {
-  answer: {key: string};
-  question: {data: {id: string}};
-  openAnswer?: string;
-}
-
-interface AnswerData {
-  id: string;
-  attributes: AnswerAttributes;
+  openAnswer: LocalizedString | null; // Optional free-form answer
 }
 
 const answerStore = writable<Record<string, Answer> | undefined>(undefined);
 
 export const loadAnswerData = async () => {
-  const response = await getExistingAnswers();
+  const answers = await getExistingAnswers();
 
-  if (!response) return;
-
-  const answerData = await response.json();
-
-  // Parse the data into a more usable format where the question ID is the key
-  const answers: Record<string, Answer> = {};
-
-  answerData.data.forEach((answer: AnswerData) => {
-    answers[answer.attributes.question.data.id] = {
-      id: answer.id,
-      key: parseInt(answer.attributes.answer.key),
-      openAnswer: answer.attributes.openAnswer || ''
-    };
-  });
+  if (!answers) return;
 
   answerStore.set(answers);
 };
