2b8ba3db55815b1797c09f08c88387dbcb830ee5
kallelongjuhani <kalle.jarvenpaa@gmail.com>
2024-02-26 11:23:51 +0200

    refactor: capitalize first letter in NavItem

--- frontend/src/lib/components/navigation/NavItem.svelte
+++ frontend/src/lib/components/navigation/NavItem.svelte
@@ -15,8 +15,7 @@
     classes =
       'flex items-center gap-md px-10 py-md min-h-touch min-w-touch w-full !text-neutral hover:bg-base-200 active:bg-base-200';
     if (!icon) {
-      // This corresponds to the width of an icon (24/16 rem) and the gap
-      // between the icon and the text (md = 10/16 rem)
+      // This corresponds to the width of an icon (24/16 rem) and the gap between the icon and the text (md = 10/16 rem)
       classes += ' pl-[3.125rem]';
     }
   }
@@ -24,23 +23,17 @@
 
 <!--
 @component
-Outputs a navigation item for use inside a `<NavGroup>` which in turn is
-used within a `<Navigation>` component.
+Outputs a navigation item for use inside a `<NavGroup>` which in turn is used within a `<Navigation>` component.
 
-The item is rendered as an `<a>` element if `href` is supplied. Otherwise
-a `<button>` element will be used. Be sure to provde an `on:click` event 
-handler or other way of making the item interactive.
+The item is rendered as an `<a>` element if `href` is supplied. Otherwise a `<button>` element will be used. Be sure to provde an `on:click` event  handler or other way of making the item interactive.
 
 ### Properties
 
-- `href`: The URL to navigate to. If this is not supplied be sure to
-  provde an `on:click` event handler or other way of making the item
-  interactive.
+- `href`: The URL to navigate to. If this is not supplied be sure to provide an `on:click` event handler or other way of making the item interactive.
 - `icon`: An optional `IconName` of the icon to use. @default `undefined`
 - `text`: A required text to display.
 - `class`: Additional class string to append to the element's default classes.
-- Any valid attributes of either an `<a>` or `<button>` element depending
-  whether `href` was defined or not, respectively.
+- Any valid attributes of either an `<a>` or `<button>` element depending whether `href` was defined or not, respectively.
 
 ### Usage
 
@@ -50,10 +43,7 @@ handler or other way of making the item interactive.
 ```
 -->
 
-<!-- We use a div with an Aria role instead of a `<li>` because we don't
-  place the items within a valid parent for a `<li>`. We need the wrapper
-  because we can't change the role of `<ActionItem>`, which renders as
-  an `<a>` or `<button>` and thus already has a defined Aria role. -->
+<!-- We use a div with an Aria role instead of a `<li>` because we don't place the items within a valid parent for a `<li>`. We need the wrapper because we can't change the role of `<ActionItem>`, which renders as an `<a>` or `<button>` and thus already has a defined Aria role. -->
 <div role="listitem">
   <svelte:element
     this={href == null ? 'button' : 'a'}
@@ -63,6 +53,6 @@ handler or other way of making the item interactive.
     {#if icon}
       <Icon name={icon} />
     {/if}
-    {text}
+    <span class="first-letter:uppercase">{text}</span>
   </svelte:element>
 </div>
