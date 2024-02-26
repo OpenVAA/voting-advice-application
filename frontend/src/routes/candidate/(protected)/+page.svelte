08dd8b7d045a33aae29279b885d53cc06ef0338d
Henrik <henrikki.hyvarinen@gmail.com>
2024-02-26 12:51:53 +0200

    Fix: resetted branch to main and added home page

---
+++ frontend/src/routes/candidate/(protected)/+page.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  import {t} from '$lib/i18n';
+  import {LogoutButton} from '$lib/candidate/components/logoutButton';
+  import {BasicPage} from '$lib/templates/basicPage';
+  import {Button} from '$lib/components/button';
+  import {getRoute, Route} from '$lib/utils/navigation';
+  import {get} from 'svelte/store';
+  import {authContext} from '$lib/utils/authenticationStore';
+  let userName = get(authContext.user)?.candidate?.firstName;
+</script>
+
+<!--Homepage for the user-->
+
+<BasicPage title={$t('candidateApp.homePage.greeting', {userName})}>
+  <p class="max-w-md text-center">
+    {$t('candidateApp.homePage.explanation')}
+  </p>
+  <Button
+    variant="main"
+    slot="primaryActions"
+    text={$t('candidateApp.homePage.buttonText')}
+    icon="next"
+    href={getRoute(Route.Questions)} />
+</BasicPage>
