2f1e57b8322d9aa52de7ad76cdf7a5d0ae15f995
kallelongjuhani <kalle.jarvenpaa@gmail.com>
2024-02-29 11:10:29 +0200

    feat: enable custom colors for the Icon component
    Enable custom, i.e. non-named, color use with the `Icon` component.
    These are needed for colored party icons.

--- frontend/src/lib/components/icon/Icon.svelte
+++ frontend/src/lib/components/icon/Icon.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-  import {concatClass} from '$lib/utils/components';
+  import {concatProps} from '$lib/utils/components';
   import type {IconProps} from './Icon.type';
   import {ICONS} from './icons';
 
@@ -7,6 +7,8 @@
   export let name: $$Props['name'];
   export let size: $$Props['size'] = 'md';
   export let color: $$Props['color'] = 'current';
+  export let customColor: $$Props['customColor'] = undefined;
+  export let customColorDark: $$Props['customColorDark'] = undefined;
 
   // Load svg contents
   let svgElement: SVGElement;
@@ -22,10 +24,12 @@
     import(`./svg/${folder}/${filename}.ts`).then((svg) => (svgElement.innerHTML = svg.default));
   }
 
-  // Create class names
+  // Create styles
   let classes: string;
+  let styles: string | undefined;
   $: {
     classes = 'inline-block';
+    styles = '';
     // Predefined sizes
     switch (size) {
       case 'sm':
@@ -38,7 +42,17 @@
         classes += ' h-24 w-24';
     }
     // Set fill color
-    if (color != null) {
+    if (customColor) {
+      styles += ` --icon-color: ${customColor};`;
+      classes += ' fill-[var(--icon-color)]';
+      if (!customColorDark) classes += ` dark:fill-${color}`;
+    }
+    if (customColorDark) {
+      styles += ` --icon-color-dark: ${customColorDark};`;
+      classes += ' dark:fill-[var(--icon-color-dark)]';
+      if (!customColor) classes += ` fill-${color}`;
+    }
+    if (!customColor && !customColorDark) {
       classes += ` fill-${color}`;
     }
   }
@@ -52,18 +66,13 @@ Use the other properties to set the size and color of the icon. The icon
 is `aria-hidden` by default, but that can overriden. You can also pass 
 any valid attributes of the `<svg>` element.
 
-NB! The component does not currently support reactive updates to `name`,
-`size` or `color`. Use within a `#key` block if you need to change these.
-
 ### Properties
 
 - `name`: the name of the icon to use
-- `size`: The size of the icon as one of the predefined sizes 'sm', 'md' or 'lg'.
-   For arbitrary values, you can supply a `class` property, such as 
-   `h-[3.15rem] w-[3.15rem]`. @default 'md'
-- `color`: The color of the icon as one of the predefined colours.
-   For arbitrary values, you can supply a `class` property, such as
-   `fill-[#123456]`. @default 'current'
+- `size`: The size of the icon as one of the predefined sizes 'sm', 'md' or 'lg'. For arbitrary values, you can supply a `class` property, such as `h-[3.15rem] w-[3.15rem]`. @default 'md'
+- `color`: The color of the icon as one of the predefined colours. For arbitrary values, use the `customColor` and `customColorDark` properties. @default 'current'
+- `customColor`: A custom color string to use for the icon, e.g. in case of parties, which will override the `color` property. Make sure to define both `customColor` and `customColorDark` together.
+- `customColorDark`: A custom color string to use for the icon in dark mode, which will override the `color` property.
 - `aria-hidden`: @default `true`
 - `role`: Aria role @default `img`
 - `class`: Additional class string to append to the element's default classes.
@@ -84,4 +93,7 @@ NB! The component does not currently support reactive updates to `name`,
   role="img"
   xmlns="http://www.w3.org/2000/svg"
   viewBox="0 0 24 24"
-  {...concatClass($$restProps, classes)} />
+  {...concatProps($$restProps, {
+    class: classes,
+    style: styles
+  })} />
