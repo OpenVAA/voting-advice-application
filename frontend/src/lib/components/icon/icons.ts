5bad67069155bc6d4d049500f403ac3edf6ba245
kallelongjuhani <kalle.jarvenpaa@gmail.com>
2024-03-10 16:46:57 +0200

    feat: language Icon
    Add `language` as a variant for the `Icon` component.

--- frontend/src/lib/components/icon/icons.ts
+++ frontend/src/lib/components/icon/icons.ts
@@ -1,36 +1,27 @@
-// The icons in the folder `svg/material` are from Google Material
-// Design icons/symbols and fall under the Apache V 2.0 license:
-// https://github.com/google/material-design-icons/blob/master/LICENSE
-// They are sourced from https://github.com/marella/material-design-icons/
-// blob/main/svg/round/<file_name>.svg, where `file_name` is name of the
-// SVG file.
+// The icons in the folder `svg/material` are from Google Material Design icons/symbols and fall under the Apache V 2.0 license: https://github.com/google/material-design-icons/blob/master/LICENSE
 //
-// The icons in the folder `svg/custom` use the same license as the main
-// repository.
+// They are sourced from https://github.com/marella/material-design-icons/blob/main/svg/round/<file_name>.svg, where `file_name` is name of the SVG file.
 //
-// NB. Icons must be contained in a subfolder of `svg` one level deep.
-// Thus, 'icon(.svg)' and 'foo/bar/icon(.svg)' are not allowed, but
-// 'foo/icon(.svg)' is.
+// The icons in the folder `svg/custom` use the same license as the main repository.
 //
-// When adding new icons, keep in mind that they are expected to not
-// contain any strokes (the color is applied to the fill) and to be
-// monochromatic and have a 1:1 aspect ratio.
+// NB. Icons must be contained in a subfolder of `svg` one level deep. Thus, 'icon(.ts)' and 'foo/bar/icon(.ts)' are not allowed, but 'foo/icon(.ts)' is.
 //
-// An easy way to get the files is:
-// 1. Put the file urls on their own lines in a text file, e.g.:
-//    https://raw.githubusercontent.com/marella/material-design-icons/
-//    main/svg/round/playlist_add_check.svg
+// When adding new icons, keep in mind that they are expected to not contain any strokes (the color is applied to the fill) and to be monochromatic and have a 1:1 aspect ratio.
+//
+// An easy way to add icons is:
+// 1. Put the file urls on their own lines in a text file, e.g.: https://raw.githubusercontent.com/marella/material-design-icons/main/svg/round/playlist_add_check.svg
 // 2. Use `xargs -n 1 curl -O < urls.txt` to download them en masse.
 // 3. Extract the svg contents from them with search and replace:
 //    - Find:    <svg.*?>(.*)</svg>
 //    - Replace: export default '$1';
 // 4. Change the file suffices to `.ts`.
+// 5. Move the file to the `svg/material` folder.
+// 6. Add the icons' details to the `ICONS` object in `icons.ts` as, e.g. `addToList: ['material', 'playlist_add_check']`.
 
 /** Supported icon names */
 export type IconName = keyof typeof ICONS;
 
-/** All the available icons and tuples of their folders and filenames
- * without the SVG extension in the `svg` folder */
+/** All the available icons and tuples of their folders and filenames without the SVG extension in the `svg` folder */
 export const ICONS: Record<string, readonly [folder: string, filename: string]> = {
   addToList: ['material', 'playlist_add_check'],
   alliance: ['material', 'link'],
@@ -50,6 +41,7 @@ export const ICONS: Record<string, readonly [folder: string, filename: string]> 
   home: ['material', 'home'],
   important: ['material', 'priority_high'],
   info: ['material', 'info'],
+  language: ['material', 'language'],
   list: ['material', 'format_list_bulleted'],
   locked: ['material', 'lock'],
   logout: ['material', 'logout'],
