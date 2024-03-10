f58b758bc903f3199870a505fb86cf36fd9efc2d
kallelongjuhani <kalle.jarvenpaa@gmail.com>
2024-03-10 16:52:35 +0200

    feat: disabled property for NavItem
    Enables the use of `disabled` in `NavItem`s.

--- frontend/src/lib/components/navigation/NavItem.svelte
+++ frontend/src/lib/components/navigation/NavItem.svelte
@@ -5,6 +5,7 @@
 
   type $$Props = NavItemProps;
 
+  export let disabled: $$Props['disabled'] = undefined;
   export let href: $$Props['href'] = undefined;
   export let icon: $$Props['icon'] = undefined;
   export let text: $$Props['text'];
@@ -13,10 +14,10 @@
   let classes: string;
   $: {
     classes =
-      'flex items-center gap-md px-10 py-md min-h-touch min-w-touch w-full !text-neutral hover:bg-base-200 active:bg-base-200';
+      'nav-item flex items-center gap-md px-10 py-md min-h-touch min-w-touch w-full !text-neutral hover:bg-base-200 active:bg-base-200';
     if (!icon) {
       // This corresponds to the width of an icon (24/16 rem) and the gap between the icon and the text (md = 10/16 rem)
-      classes += ' pl-[3.125rem]';
+      classes += ' pl-[2.75rem]';
     }
   }
 </script>
@@ -32,13 +33,14 @@ The item is rendered as an `<a>` element if `href` is supplied. Otherwise a `<bu
 - `href`: The URL to navigate to. If this is not supplied be sure to provide an `on:click` event handler or other way of making the item interactive.
 - `icon`: An optional `IconName` of the icon to use. @default `undefined`
 - `text`: A required text to display.
+- `disabled`: Whether the button is disabled. This can also be used with items rendered as `<a>` elements.
 - `class`: Additional class string to append to the element's default classes.
 - Any valid attributes of either an `<a>` or `<button>` element depending whether `href` was defined or not, respectively.
 
 ### Usage
 
 ```tsx
-<NavItem href={getRoute(Route.Info)} icon="info" text="Show info"/>
+<NavItem href={$getRoute(Route.Info)} icon="info" text="Show info"/>
 <NavItem on:click={(e) => foo(e)} text="Do foo"/>
 ```
 -->
@@ -49,6 +51,7 @@ The item is rendered as an `<a>` element if `href` is supplied. Otherwise a `<bu
     this={href == null ? 'button' : 'a'}
     {href}
     on:click
+    disabled={disabled || undefined}
     {...concatClass($$restProps, classes)}>
     {#if icon}
       <Icon name={icon} />
@@ -57,3 +60,12 @@ The item is rendered as an `<a>` element if `href` is supplied. Otherwise a `<bu
     <slot />
   </svelte:element>
 </div>
+
+<style lang="postcss">
+  /* The class prefixes are valid even though linter flags them */
+  .nav-item[disabled],
+  .nav-item:disabled,
+  .nav-item.disabled {
+    @apply pointer-events-none !text-secondary hover:bg-transparent;
+  }
+</style>
--- frontend/src/lib/components/navigation/NavItem.type.ts
+++ frontend/src/lib/components/navigation/NavItem.type.ts
@@ -6,28 +6,27 @@ import type {IconName} from '$lib/components/icon';
  */
 export type NavItemProps = LinkOrButtonElementProps & {
   /**
-   * The optional name of the icon to use with the navigation item.
-   * See the `Icon` component for more details.
+   * The optional name of the icon to use with the navigation item. See the `Icon` component for more details.
    */
   icon?: IconName | null;
   /**
    * The text to display in the navigation item.
    */
   text: string;
+  /**
+   * Whether the button is disabled. This can also be used with items rendered as `<a>` elements.
+   */
+  disabled?: boolean | null;
 };
 
 /**
- * The base properties of a navigation item must be either those of an `<a>`
- * element with the `href` attribute, or a `<button>` element, preferably
- * with the `on:click` event handler.
+ * The base properties of a navigation item must be either those of an `<a>` element with the `href` attribute, or a `<button>` element, preferably with the `on:click` event handler.
  */
 type LinkOrButtonElementProps =
   | WithRequired<SvelteHTMLElements['a'], 'href'>
   | (SvelteHTMLElements['button'] & {href?: null});
 
 /**
- * Make specific properties of an interface required. Works the same way as
- * `Required<Type>` but only applies to keys listed.
- * Source: https://stackoverflow.com/questions/69327990/how-can-i-make-one-property-non-optional-in-a-typescript-type
+ * Make specific properties of an interface required. Works the same way as `Required<Type>` but only applies to keys listed. Source: https://stackoverflow.com/questions/69327990/how-can-i-make-one-property-non-optional-in-a-typescript-type
  */
 type WithRequired<Type, Key extends keyof Type> = Type & {[Prop in Key]-?: Type[Prop]};
