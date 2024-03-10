d49049e149df51c4a816e1b4e7adecbb8aa514bc
kallelongjuhani <kalle.jarvenpaa@gmail.com>
2024-03-10 16:48:07 +0200

    feat: convert getRoute to a store
    Convert the `getRoute` utility to a `derived` store dependent on the
    `page` store. This is needed because any links on the page created using
    `getRoute` would not update if the url or `lang` route param changes.
    
    After the update, calls in the form `getRoute(foo)` have to be converted
    to `$getRoute(foo)`.

--- frontend/src/lib/utils/tests/navigation.test.ts
+++ frontend/src/lib/utils/tests/navigation.test.ts
@@ -1,8 +1,9 @@
 import {beforeAll, expect, test} from 'vitest';
+import {get} from 'svelte/store';
 import {mockIsPageStore} from './mock-pageStore';
 import {getRoute, Route} from '../navigation';
 
-// Mock the $page store which is used by getRoute
+// Mock the $page store which is used by get(getRoute)
 beforeAll(() => {
   vi.mock('$app/stores', () => {
     const page = mockIsPageStore;
@@ -14,19 +15,23 @@ beforeAll(() => {
 test('getRoute', () => {
   const defaultLocale = 'en';
   mockIsPageStore.mockSetLocale(defaultLocale);
-  expect(getRoute(Route.Home), 'Route with default locale').toEqual(`/${defaultLocale}`);
+  expect(get(getRoute)(Route.Home), 'Route with default locale').toEqual(`/${defaultLocale}`);
+  expect(get(getRoute)({route: Route.Home, locale: 'none'}), 'Route with no locale').toEqual('');
   const id = '123';
-  expect(getRoute({route: Route.Home, id}), 'Route with id').toEqual(`/${defaultLocale}/${id}`);
+  expect(get(getRoute)({route: Route.Home, id}), 'Route with id').toEqual(
+    `/${defaultLocale}/${id}`
+  );
   const params = {value: 'value=€'};
-  expect(getRoute({route: Route.Home, params}), 'Route with params').toEqual(
+  expect(get(getRoute)({route: Route.Home, params}), 'Route with params').toEqual(
     `/${defaultLocale}?value=value%3D%E2%82%AC`
   );
   const locale = 'fi';
-  expect(getRoute({route: Route.Home, locale}), 'Route with overriden locale').toEqual(
+  expect(get(getRoute)({route: Route.Home, locale}), 'Route with overriden locale').toEqual(
     `/${locale}`
   );
   const subpath = 'help';
   const pathname = `/${defaultLocale}/${subpath}`;
   mockIsPageStore.mockSetPathname(pathname);
-  expect(getRoute({locale}), 'Switch locale without route').toEqual(`/${locale}/${subpath}`);
+  expect(get(getRoute)({locale}), 'Switch locale without route').toEqual(`/${locale}/${subpath}`);
+  expect(get(getRoute)({locale: 'none'}), 'Get route with no locale').toEqual(`/${subpath}`);
 });
