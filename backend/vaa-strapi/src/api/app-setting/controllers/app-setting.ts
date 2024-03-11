600f09774e1f422e5b7a282a1938cdef5001f241
kallelongjuhani <kalle.jarvenpaa@gmail.com>
2024-03-11 10:20:26 +0200

    feat: update Strapi content model and AC policies
    Introduces various small updates to:
    
    - Create an `App Settings` content type for future use
    - Add `color` and `colorDark` properties to `Party` and `Question
      Category`
    - Add `customData` property to `Question`, which can be used to store
      data specific to an app instance
    - Update access control policies to fix bugs with retrieving `Parties`
      and individual `Candidate`s.

---
+++ backend/vaa-strapi/src/api/app-setting/controllers/app-setting.ts
@@ -0,0 +1,7 @@
+/**
+ * app-setting controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::app-setting.app-setting');
---
+++ backend/vaa-strapi/src/api/app-setting/services/app-setting.ts
@@ -0,0 +1,7 @@
+/**
+ * app-setting service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::app-setting.app-setting');
