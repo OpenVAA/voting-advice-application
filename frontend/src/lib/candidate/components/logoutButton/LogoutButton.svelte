b809ee8c3fc627b2ffc40be85ccdf23cb6786500
Henriki2305 <122307901+Henriki2305@users.noreply.github.com>
2024-03-13 12:42:40 +0200

    Feature:Made logout modal's text dynamically tell actual amount of op… (#422)
    * Feature:Made logout modal's text dynamically tell actual amount of opinions and basic information left
    
    * fix: added localisation files
    
    * fix:removed completed TODO texts
    
    * Made logoutmodal's text not mention basic info if it is already filled

--- frontend/src/lib/candidate/components/logoutButton/LogoutButton.svelte
+++ frontend/src/lib/candidate/components/logoutButton/LogoutButton.svelte
@@ -1,10 +1,13 @@
 <script lang="ts">
+  import {get} from 'svelte/store';
   import {t} from '$lib/i18n';
   import {getRoute, Route} from '$lib/utils/navigation';
   import TimedModal from '$lib/components/modal/TimedModal.svelte';
   import {goto} from '$app/navigation';
   import {authContext} from '$lib/utils/authenticationStore';
   import {Button} from '$lib/components/button';
+  import {getContext} from 'svelte';
+  import type {AnswerContext} from '$lib/utils/answerStore';
 
   /** Defaults to true, so that button variant is "icon". Can be set to false if the button should be of variant "main" */
   export let variantIcon = true;
@@ -17,11 +20,33 @@
   let timeLeft = logoutModalTimer;
 
   // functions for logout button
-  // TODO: add proper check of unfilled data
-  let unfilledData = true;
+  const answerContext = getContext<AnswerContext | undefined>('answers');
+  const answerstoreWritable = answerContext?.answers;
+  $: answerStore = $answerstoreWritable;
+
+  const questionstoreWritable = answerContext?.questions;
+  $: questionStore = $questionstoreWritable;
+
+  const user = get(authContext.user);
+  $: remainingInfoAmount =
+    4 -
+    [
+      user?.candidate?.gender,
+      user?.candidate?.motherTongues?.length > 0,
+      user?.candidate?.birthday,
+      user?.candidate?.manifesto
+    ].filter((x) => x).length;
+
+  let remainingOpinionNumber = 0;
+  $: {
+    if (answerStore && questionStore) {
+      remainingOpinionNumber =
+        Object.entries(questionStore).length - Object.entries(answerStore).length;
+    }
+  }
+  $: unfilledData = remainingOpinionNumber > 0 || remainingInfoAmount > 0;
 
   const triggerLogout = () => {
-    // TODO: check if candidate has filled all the data
     if (unfilledData) {
       openModal();
     } else {
@@ -66,7 +91,6 @@ When set to false, the button variant is main.
     text={$t('candidateApp.allDataFilled.logOut')}
     color="warning" />
 {/if}
-
 <TimedModal
   bind:timeLeft
   bind:openModal
@@ -76,9 +100,19 @@ When set to false, the button variant is main.
   <div class="notification max-w-md text-center">
     <h2>{$t('candidateApp.logoutModal.title')}</h2>
     <br />
-    <p>
-      {$t('candidateApp.logoutModal.body')}
-    </p>
+    {#if remainingInfoAmount > 0}
+      <p>
+        {$t('candidateApp.logoutModal.body', {remainingInfoAmount, remainingOpinionNumber})}
+      </p>
+    {:else if remainingOpinionNumber > 1}
+      <p>
+        {$t('candidateApp.logoutModal.bodyBasicInfoReady', {remainingOpinionNumber})}
+      </p>
+    {:else}
+      <p>
+        {$t('candidateApp.logoutModal.bodyBasicInfoReady1OpinionLeft')}
+      </p>
+    {/if}
     <p>
       {$t('candidateApp.logoutModal.confirmation', {timeLeft})}
     </p>
