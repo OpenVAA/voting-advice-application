b8c13e5467e39f0d5d55f4400c28819cd6065d1c
kallelongjuhani <kalle.jarvenpaa@gmail.com>
2024-03-14 09:55:31 +0200

    feat: wait for Election data to be loaded before loading page contents
    Show a loading spinner while `data.election` is being loaded.

--- frontend/src/routes/[[lang=locale]]/+layout.svelte
+++ frontend/src/routes/[[lang=locale]]/+layout.svelte
@@ -1,10 +1,17 @@
 <script lang="ts">
   import {t} from '$lib/i18n';
   import '../../app.css';
+  import type {LayoutData} from './$types';
+
+  export let data: LayoutData;
 </script>
 
 <svelte:head>
   <title>{$t('viewTexts.appTitle')}</title>
 </svelte:head>
 
-<slot />
+{#if data.election}
+  <slot />
+{:else}
+  <span class="loading loading-spinner loading-lg" />
+{/if}
