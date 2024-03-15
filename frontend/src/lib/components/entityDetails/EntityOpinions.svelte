e435ad72da220ed01bddf812d5af7029f1594c86
kallelongjuhani <kalle.jarvenpaa@gmail.com>
2024-03-15 12:23:14 +0200

    refactor: convert CandidateDetailsCard to EntityDetails
    Makes the `CandidateDetailsCard`, renamed to `EntityDetails`, compatible
    with both candidates and parties.
    
    Refactors the layouts of the subcomponents to match the designs in
    Figma.

---
+++ frontend/src/lib/components/entityDetails/EntityOpinions.svelte
@@ -0,0 +1,96 @@
+<script lang="ts">
+  import {t} from '$lib/i18n';
+  import {isCandidate} from '$lib/utils/entities';
+  import {answeredQuestions, appType} from '$lib/stores';
+  import {CategoryTag} from '$lib/components/categoryTag';
+  import {LikertResponseButtons, QuestionOpenAnswer} from '$lib/components/questions';
+  import {HeadingGroup, PreHeading} from '$lib/components/headingGroup';
+  import {getLikertAnswer} from '$lib/utils/answers';
+  import type {EntityDetailsProps} from './EntityDetails.type';
+
+  export let entity: NonNullable<EntityDetailsProps['entity']>;
+  export let questions: EntityDetailsProps['opinionQuestions'];
+
+  let shortName: string;
+
+  $: shortName = isCandidate(entity)
+    ? `${entity.firstName[0].toLocaleUpperCase()}. ${entity.lastName}`
+    : entity.shortName;
+</script>
+
+<!--
+@component
+Used to show an entity's opinions in an `EntityDetails` component.
+
+### Properties
+
+- `entity`: The entity
+- `questions`: The list of Question objects to show
+
+### Usage
+
+```tsx
+<EntityOpinions entity={candidate} questions={opinionQuestions} />
+```
+-->
+
+<div class="p-lg">
+  {#each questions as question}
+    {@const {id, text, type, values, category} = question}
+    {@const {answer, openAnswer} = getLikertAnswer(entity, question)}
+    {@const voterAnswer = $answeredQuestions[id]}
+    {@const headingId = `questionHeading-${id}`}
+
+    <div class="mb-60 mt-20">
+      <HeadingGroup id={headingId} class="mb-lg text-center">
+        <PreHeading><CategoryTag {category} /></PreHeading>
+        <h3>{text}</h3>
+      </HeadingGroup>
+
+      {#if $appType === 'candidate'}
+        {#if answer == null}
+          <div class="small-label mb-16 text-center">
+            {$t('questions.entityHasntAnswered', {entity: shortName})}
+          </div>
+        {/if}
+      {:else}
+        {#if voterAnswer == null && answer == null}
+          <div class="small-label mb-16 text-center">
+            {$t('questions.bothHaventAnswered', {entity: shortName})}
+          </div>
+        {:else if voterAnswer == null && answer == null}
+          <div class="small-label mb-16 text-center">
+            {$t('questions.youHaventAnswered')}
+          </div>
+        {:else if voterAnswer == null}
+          <div class="small-label mb-16 text-center">
+            {$t('questions.youHaventAnswered')}
+          </div>
+        {:else if answer == null}
+          <div class="small-label mb-16 text-center">
+            {$t('questions.entityHasntAnswered', {entity: shortName})}
+          </div>
+        {/if}
+      {/if}
+
+      {#if type === 'singleChoiceOrdinal'}
+        <LikertResponseButtons
+          aria-labelledby={headingId}
+          name={id}
+          mode="display"
+          selectedKey={voterAnswer}
+          entityKey={answer}
+          entityLabel={shortName}
+          options={values} />
+      {:else}
+        {$t('error.general')}
+      {/if}
+
+      {#if openAnswer}
+        <QuestionOpenAnswer>
+          {openAnswer}
+        </QuestionOpenAnswer>
+      {/if}
+    </div>
+  {/each}
+</div>
