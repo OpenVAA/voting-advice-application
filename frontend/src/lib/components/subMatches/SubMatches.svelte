c9db931e21a5a803648b52a1ec31d42407a2a30e
kallelongjuhani <kalle.jarvenpaa@gmail.com>
2024-03-15 12:13:49 +0200

    feat: SubMatches component
    The `SubMatches` component streamlines the displaying of `ScoreGauge`
    components for sub-category matches.

---
+++ frontend/src/lib/components/subMatches/SubMatches.svelte
@@ -0,0 +1,43 @@
+<script lang="ts">
+  import {concatProps} from '$lib/utils/components';
+  import {ScoreGauge} from '$lib/components/scoreGauge';
+  import type {SubMatchesProps} from './SubMatches.type';
+
+  type $$Props = SubMatchesProps;
+
+  export let matches: $$Props['matches'];
+  export let variant: $$Props['variant'] = 'tight';
+  export let scoreGaugeProps: $$Props['scoreGaugeProps'] = {};
+
+  let style: string;
+  $: style = `grid-template-columns: repeat(auto-fill, minmax(${variant === 'loose' ? 9 : 6}rem, 1fr));`;
+</script>
+
+<!--
+@component
+Display an entity's sub-matches.
+
+### Properties
+
+- `matches`: The sub-matches of a `RankingProps`.
+- `variant`: Variant layout, controlling the spacing of gauges. @default `'tight'`
+- Any valid attributes of a `<div>` element
+
+### Usage
+
+```tsx
+<SubMatches matches={ranking.subMatches}/>
+```
+-->
+
+<div {...concatProps($$restProps, {class: 'grid gap-x-md gap-y-sm', style})}>
+  {#each matches as { score, questionGroup }}
+    <ScoreGauge
+      {score}
+      label={questionGroup.label ?? ''}
+      color={questionGroup.color}
+      colorDark={questionGroup.colorDark}
+      variant="radial"
+      {...scoreGaugeProps} />
+  {/each}
+</div>
---
+++ frontend/src/lib/components/subMatches/SubMatches.type.ts
@@ -0,0 +1,17 @@
+import type {SvelteHTMLElements} from 'svelte/elements';
+import type {ScoreGaugeProps} from '$lib/components/scoreGauge';
+
+export type SubMatchesProps = SvelteHTMLElements['div'] & {
+  /**
+   * The sub-matches of a `RankingProps`.
+   */
+  matches: SubMatchProps[];
+  /**
+   * Variant layout, controlling the spacing of gauges. @default 'tight'
+   */
+  variant?: 'tight' | 'loose';
+  /**
+   * Optional properties passed to `ScoreGauge`.
+   */
+  scoreGaugeProps?: Partial<Omit<ScoreGaugeProps, 'score'>>;
+};
