0680f97af0d0c2983cc769936ab62b5a12907221
kallelongjuhani <kalle.jarvenpaa@gmail.com>
2024-03-15 11:44:19 +0200

    feat: ElectionSymbol component
    The `ElectionSymbol` is used to display an entity's election symbol,
    usually a number.

---
+++ frontend/src/lib/components/electionSymbol/ElectionSymbol.type.ts
@@ -0,0 +1,12 @@
+import type {SvelteHTMLElements} from 'svelte/elements';
+
+export type ElectionSymbolProps = SvelteHTMLElements['span'] & {
+  /**
+   * The text of the symbol, e.g. '15' or 'A'. If the symbol is an image, `text` will be used as its `alt` attribute.
+   */
+  text: string;
+  /**
+   * The `src` of an image election symbol.
+   */
+  image?: string;
+};
