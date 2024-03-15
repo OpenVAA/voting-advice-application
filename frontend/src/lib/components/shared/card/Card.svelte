8b006c7e8086624839381eb9cb0c58616ea557a2
kallelongjuhani <kalle.jarvenpaa@gmail.com>
2024-03-15 18:15:51 +0200

    refactor: Card component
    Move explicit clicking handling out from the `Card` component because of
    accessibility and usability (i.e. opening card targets in new tabs)
    issues.
    
    Rename slots with more generic terms.
    
    Refactor drop shadow styling so that it's applied when the `Card` is
    inside an interactive element or has an explicit `interactive` property
    set to `true`.

--- frontend/src/lib/components/shared/card/Card.svelte
+++ frontend/src/lib/components/shared/card/Card.svelte
@@ -1,51 +1,83 @@
 <script lang="ts">
+  import {concatClass} from '$lib/utils/components';
   import type {CardProps} from './Card.type';
 
   type $$Props = CardProps;
 
-  export let keyboardClick: $$Props['keyboardClick'] = 'Enter';
-
-  if (keyboardClick && typeof keyboardClick === 'string') keyboardClick = [keyboardClick];
-
-  let cardElement: HTMLElement;
-
-  /**
-   * Trigger the on:click handler when any of the listed keyboard keys
-   * is pressed.
-   *
-   * TODO: Wrap the item in an `<a>` so that tabbed browsing works and then
-   * we won't need this keyboard handler anymore.
-   */
-  function onKeyPress(event: KeyboardEvent) {
-    if (cardElement && keyboardClick && keyboardClick.includes(event.key)) {
-      cardElement.click();
-    }
-  }
+  export let interactive: $$Props['interactive'] = false;
 </script>
 
-<!-- svelte-ignore a11y-no-noninteractive-tabindex -->
+<!--@component
+A generic card component. For making `Card`s interactive, it is preferred to wrap it in an `<a>` or a `<button>` element, instead of providing an `on:click` handler.
+
+### Properties
+
+- `interactive`: Set to `true` if you want to explicitly use the styling for an interactive card, i.e. shadow on hover and a pointer cursor. The styles are automatically applied if the component is wrapped in an `<a>` or a `<button>`. @default `false`
+- Any valid attributes of an `<article>` element
+
+### Slots
+
+- `image`: The image to display in the card, e.g. a portrait or a logo.
+- `title`: The card title.
+- `subtitle`: The details below the card title, e.g. a candidate's party and election number.
+- `callout`: The callout in the corner of the card, e.g. a match percentage.
+- default: The content of the card below the other slots, e.g. a candidate's subcategory matches.
+
+### Usage
+
+```tsx
+<a href="/candidate/{id}">
+  <Card>
+    <h3 slot="title">{name}</h3>
+    <PartyTag slot="subtitle" {party}/>
+    <svelte:fragment slot="callout">
+      {match}%
+    </svelte:fragment>
+    {#each themes as theme}
+      <div>{theme}</div>
+    {/each}
+  </Card>
+</a>
+```
+-->
+
 <article
-  class="duration-300 relative flex cursor-pointer flex-col rounded-md bg-base-100 p-md transition-shadow ease-in-out hover:shadow-xl"
-  {...$$props}
-  bind:this={cardElement}
-  on:click
-  on:keypress={onKeyPress}
-  on:keypress>
+  {...concatClass(
+    $$restProps,
+    `vaa-card ${interactive ? 'interactive' : ''} relative flex flex-col rounded-md bg-base-100 px-md py-16 gap-md`
+  )}>
   <div class="flex justify-stretch gap-md">
-    <figure>
-      <slot name="card-media" />
-    </figure>
+    {#if $$slots.image}
+      <div>
+        <slot name="image" />
+      </div>
+    {/if}
     <div class="flex w-full flex-row items-center justify-between gap-md">
-      <div class="flex flex-col items-start gap-6">
-        <slot name="body-title" />
-        <slot name="body-content" />
+      <div class="flex flex-col items-start gap-4">
+        {#if $$slots.image}
+          <div><slot name="title" /></div>
+        {/if}
+        {#if $$slots.subtitle}
+          <div><slot name="subtitle" /></div>
+        {/if}
       </div>
-      <div class="flex flex-row">
-        <slot name="body-match" />
-      </div>
+      {#if $$slots.callout}
+        <div class="flex flex-row">
+          <slot name="callout" />
+        </div>
+      {/if}
     </div>
   </div>
-  <div>
-    <slot name="card-footer" />
-  </div>
+  {#if $$slots.default}
+    <slot />
+  {/if}
 </article>
+
+<style lang="postcss">
+  /* Nb. hover: is a valid class prefix even though the linter flags it */
+  :global(a > .vaa-card),
+  :global(button > .vaa-card),
+  .vaa-card.interactive {
+    @apply cursor-pointer text-neutral transition-shadow ease-in-out hover:shadow-xl;
+  }
+</style>
