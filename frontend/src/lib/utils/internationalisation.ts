5417c77058eea4321cc061125e24ec9c931b2d1f
kallelongjuhani <kalle.jarvenpaa@gmail.com>
2024-03-15 11:52:13 +0200

    refactor: GetFullNameInOrder and rename to formatName
    The `formatName` utility accepts an object with `firstName` and
    `lastName` instead of individual arguments so that it's compatible with
    `CandidateProps` objects.

--- frontend/src/lib/utils/internationalisation.ts
+++ frontend/src/lib/utils/internationalisation.ts
@@ -1,5 +1,15 @@
-export function GetFullNameInOrder(firstName: string, lastName: string) {
-  // TODO: Get this value from settings
-  const firstNameLastNameOrder = true;
-  return firstNameLastNameOrder ? `${firstName} ${lastName}` : `${lastName} ${firstName}`;
+/**
+ * Return the name of the person in a locale-based order.
+ * TODO: Use locale to define the order.
+ * @param firstName
+ * @param lastName
+ * @returns
+ */
+export function formatName({firstName, lastName}: NameValues) {
+  return `${firstName} ${lastName}`;
+}
+
+interface NameValues {
+  firstName: string;
+  lastName: string;
 }
