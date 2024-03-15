5881d1a2452d393d4a62ee77818fc1ab837cf9e2
kallelongjuhani <kalle.jarvenpaa@gmail.com>
2024-03-15 18:22:45 +0200

    refactor: apply updated entity components on /(voter) routes
    Applies the updated `EntityCard`, `EntityDetails` and `EntityList`
    components to pages in the Voter App.
    
    Divides the subroutes in `/(voter)/results/` between candidates and
    parties.

---
+++ frontend/src/routes/[[lang=locale]]/(voters)/results/party/[partyId]/+page.svelte
@@ -0,0 +1,41 @@
+<script lang="ts">
+  import {t} from '$lib/i18n';
+  import {Route, getRoute} from '$lib/utils/navigation';
+  import {Button} from '$lib/components/button';
+  // import {EntityDetails} from '$lib/components/entityDetails';
+  import {SingleCardPage} from '$lib/templates/singleCardPage';
+  import type {PageData} from './$types';
+
+  export let data: PageData;
+
+  let partyId: string;
+  let title: string;
+  $: {
+    partyId = data.partyId;
+    title = `Not implemented yet • PartyId: ${partyId}`;
+  }
+
+  // let ranking: RankingProps<PartyId> | undefined;
+
+  // onMount(() => {
+  //   // First, check if we have a ranking for the candidate,
+  //   // which contains the Candidate object
+  //   if ($candidateRankings.length > 0)
+  //     ranking = $candidateRankings.find(r => r.entity.id == candidateId);
+  //   // If not, switch to the candidates route
+  //   if (!ranking)
+  //     goto(getRoute({route: Route.Candidate, id: candidateId}));
+  // });
+</script>
+
+<SingleCardPage {title}>
+  <Button
+    slot="banner"
+    class="!text-neutral"
+    variant="icon"
+    icon="close"
+    href={$getRoute(Route.Results)}
+    text={$t('header.back')} />
+  <h1>{title}</h1>
+  <!-- <EntityDetails {ranking} opinionQuestions={questions} {infoQuestions} /> -->
+</SingleCardPage>
