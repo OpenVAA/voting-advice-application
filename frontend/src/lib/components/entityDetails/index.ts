f52d6358343e6c26d81f439c966d5240c6588b8f
kallelongjuhani <kalle.jarvenpaa@gmail.com>
2024-03-15 12:23:14 +0200

    refactor: convert CandidateDetailsCard to EntityDetails
    Makes the `CandidateDetailsCard`, renamed to `EntityDetails`, compatible
    with both candidates and parties.
    
    Refactors the layouts of the subcomponents to match the designs in
    Figma.

---
+++ frontend/src/lib/components/entityDetails/index.ts
@@ -0,0 +1,4 @@
+export {default as EntityInfo} from './EntityInfo.svelte';
+export {default as EntityDetails} from './EntityDetails.svelte';
+export {default as EntityOpinions} from './EntityOpinions.svelte';
+export * from './EntityDetails.type';
