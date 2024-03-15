3f5a91d2bf907554dab57a6e2a146e78ac2e11d8
kallelongjuhani <kalle.jarvenpaa@gmail.com>
2024-03-15 17:35:37 +0200

    fix: localisation issues in the Candidate App
    Make server-based data fetching dependent on `parent`'s
    `i18n.currentLocale`.
    
    Make `export let data` assignments reactive so that localised texts
    contained in them are updated.

--- frontend/src/routes/[[lang=locale]]/candidate/(protected)/preview/+page.svelte
+++ frontend/src/routes/[[lang=locale]]/candidate/(protected)/preview/+page.svelte
@@ -5,18 +5,23 @@
   import {getContext} from 'svelte';
   import type {AuthContext} from '$lib/utils/authenticationStore';
   import LogoutButton from '$lib/candidate/components/logoutButton/LogoutButton.svelte';
+  import type {PageData} from './$types';
 
-  export let data: {
-    infoQuestions: QuestionProps[];
-    opinionQuestions: QuestionProps[];
-    candidates: CandidateProps[];
-  };
+  export let data: PageData;
 
-  const {opinionQuestions, infoQuestions, candidates} = data;
   const {user} = getContext<AuthContext>('auth');
-  const candidate = candidates.find(
-    (c: CandidateProps) => c.id === $user?.candidate?.id.toString()
-  );
+
+  let infoQuestions: QuestionProps[];
+  let opinionQuestions: QuestionProps[];
+  let candidates: CandidateProps[];
+  let candidate: CandidateProps | undefined;
+
+  $: {
+    infoQuestions = data.infoQuestions;
+    opinionQuestions = data.opinionQuestions;
+    candidates = data.candidates;
+    candidate = candidates.find((c) => c.id === `${$user?.candidate?.id}`);
+  }
 </script>
 
 {#if !candidate}
