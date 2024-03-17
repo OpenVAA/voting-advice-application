e8145bceffe010d386cbfe46e7dc5b3dbfb14467
kallelongjuhani <kalle.jarvenpaa@gmail.com>
2024-03-17 11:24:39 +0200

    feat: add note slot to SingleCardPage
    Add a similar `note` slot to `SingleCardPage` as on `BasicPage` for
    displaying notifications.

--- frontend/src/lib/templates/singleCardPage/SingleCardPage.type.ts
+++ frontend/src/lib/templates/singleCardPage/SingleCardPage.type.ts
@@ -1,9 +1,17 @@
+import type {AriaRole} from 'svelte/elements';
 import type {PageProps} from '../page';
 
 export interface SingleCardPageProps extends PageProps {
   /**
-   * Optional class string to add to the `<div>` tag that defines the
-   * card wrapping the `default` slot.
+   * Optional class string to add to the `<div>` tag that defines the card wrapping the `default` slot.
    */
   cardClass?: string;
+  /**
+   * Optional class string to add to the `<div>` tag wrapping the `note` slot.
+   */
+  noteClass?: string;
+  /**
+   * Aria role for the `note` slot. @default 'note'
+   */
+  noteRole?: AriaRole;
 }
