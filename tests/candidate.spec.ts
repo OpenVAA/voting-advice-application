ed9935fe7568a3cc2af145bcd47f0d269efb5693
Stepan Fedotov <stepan.fedotov@gmail.com>
2024-03-18 15:10:05 +0200

    feat: run e2e in CI

--- tests/candidate.spec.ts
+++ tests/candidate.spec.ts
@@ -1,31 +1,31 @@
 import { test, expect } from '@playwright/test';
 
 test.beforeEach(async ({ page, baseURL }) => {
-  await page.goto(`${baseURL}/candidate`);
+  await page.goto(`${baseURL}/en/candidate`);
 });
 
 test('should log out', async ({ page }) => {
-  await expect(page).toHaveURL(/(http[s]?:\/\/)?(.*)\/candidate/);
+  await expect(page).toHaveURL(/(http[s]?:\/\/)?(.*)\/en\/candidate/);
 
   await page.getByTitle('Logout').click();
   await expect(page.getByText('Some of Your Data Is Still Missing')).toBeVisible();
   await expect(page.getByText('Continue Entering Data')).toBeVisible();
 
-  const logoutButton = page.getByRole('dialog').getByRole('button', { name: 'Logout' })
+  const logoutButton = page.getByRole('dialog').getByRole('button', { name: 'Logout' });
   await expect(logoutButton).toBeVisible();
   await logoutButton.click();
 
-  await expect(page).toHaveURL(/(http[s]?:\/\/)?(.*)\/candidate/);
+  await expect(page).toHaveURL(/(http[s]?:\/\/)?(.*)\/en\/candidate/);
   await expect(page.getByText('Sign in')).toBeVisible();
 });
 
 test('should navigate', async ({ page }) => {
-  await expect(page).toHaveURL(/(http[s]?:\/\/)?(.*)\/candidate/);
+  await expect(page).toHaveURL(/(http[s]?:\/\/)?(.*)\/en\/candidate/);
   await expect(page.getByRole('button', { name: 'Close menu' })).not.toBeVisible();
   await page.getByLabel('Open menu').click();
   await expect(page.getByRole('button', { name: 'Close menu' })).toBeVisible();
   await page.getByRole('link', { name: 'Basic Info' }).click();
-  await expect(page).toHaveURL(/(http[s]?:\/\/)?(.*)\/profile/);
+  await expect(page).toHaveURL(/(http[s]?:\/\/)?(.*)\/en\/candidate\/profile/);
   await expect(page.getByRole('button', { name: 'Close menu' })).not.toBeVisible();
   await expect(page.getByRole('heading', { name: 'Basic Information' })).toBeVisible();
 });
