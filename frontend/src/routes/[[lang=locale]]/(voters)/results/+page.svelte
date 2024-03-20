5cf595f1a5d2b82ceb2fda0bb860989fc8eacc2a
kallelongjuhani <kalle.jarvenpaa@gmail.com>
2024-03-20 10:53:27 +0200

    feat: add candidates and parties tabs in results
    Add a tabs to change between parties and candidates in the results view.
    
    Note that both party rankings and party pages remain for later
    implementation.

--- frontend/src/routes/[[lang=locale]]/(voters)/results/+page.svelte
+++ frontend/src/routes/[[lang=locale]]/(voters)/results/+page.svelte
@@ -1,14 +1,24 @@
 <script lang="ts">
+  import {error} from '@sveltejs/kit';
+  import {page} from '$app/stores';
   import {t} from '$lib/i18n';
-  import {candidateRankings} from '$lib/utils/stores';
+  import {candidateRankings, settings} from '$lib/utils/stores';
   import {getRoute, Route} from '$lib/utils/navigation';
   import {Button} from '$lib/components/button';
   import {EntityList} from '$lib/components/entityList';
   import {HeroEmoji} from '$lib/components/heroEmoji';
   import {StretchBackground} from '$lib/components/stretchBackground';
   import {BasicPage} from '$lib/templates/basicPage';
+  import {Tabs} from '$lib/components/tabs';
 
+  let activeIndex = 0;
   let itemsShown = 0;
+
+  // Which entity sections to show
+  const sections = $settings.results.sections as EntityType[];
+  if (!sections?.length) error(500, 'No sections to show');
+
+  // TODO: Enable party rankings
 </script>
 
 <BasicPage title={$t('viewTexts.yourCandidatesTitle')}>
@@ -42,15 +52,34 @@
   </p>
 
   <StretchBackground padding="medium" bgColor="base-300" toBottom>
-    <h2 class="mb-lg mt-xl">
-      {$t('results.candidatesShown', {numShown: itemsShown})}
-      <span class="font-normal text-secondary"
-        >{$t('results.candidatesTotal', {numTotal: $candidateRankings.length})}</span>
-    </h2>
-    <EntityList
-      bind:itemsShown
-      rankings={$candidateRankings}
-      actionCallBack={({id}) => $getRoute({route: Route.ResultCandidate, id})}
-      class="mb-lg" />
+    {#if sections.length > 1}
+      <Tabs
+        tabs={sections.map((entityType) => $t(`common.${entityType}.plural`))}
+        bind:activeIndex />
+    {/if}
+
+    {#if sections[activeIndex] === 'candidate'}
+      <h2 class="mb-lg mt-xl">
+        {$t('results.candidatesShown', {numShown: itemsShown})}
+        <span class="font-normal text-secondary"
+          >{$t('results.candidatesTotal', {numTotal: $candidateRankings.length})}</span>
+      </h2>
+      <EntityList
+        bind:itemsShown
+        rankings={$candidateRankings}
+        actionCallBack={({id}) => $getRoute({route: Route.ResultCandidate, id})}
+        class="mb-lg" />
+    {:else if sections[activeIndex] === 'party'}
+      <h2 class="mb-lg mt-xl">
+        {$t('results.partiesShown', {numShown: itemsShown})}
+        <span class="font-normal text-secondary"
+          >{$t('results.partiesTotal', {numTotal: $page.data.parties.length})}</span>
+      </h2>
+      <EntityList
+        bind:itemsShown
+        entities={$page.data.parties}
+        actionCallBack={({id}) => $getRoute({route: Route.ResultParty, id})}
+        class="mb-lg" />
+    {/if}
   </StretchBackground>
 </BasicPage>
