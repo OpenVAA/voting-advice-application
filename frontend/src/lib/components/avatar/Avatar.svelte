81bcb04ea2f2456ca91a1f40a0ba6632ef71cb09
kallelongjuhani <kalle.jarvenpaa@gmail.com>
2024-03-21 11:30:59 +0200

    feat: link to full image from Avatar component thumbnail
    Add a link to the full image from the `<Avatar>` component's thumbnail
    if it's used in the `details` context. The link is not used on lists,
    i.e. the default context.

--- frontend/src/lib/components/avatar/Avatar.svelte
+++ frontend/src/lib/components/avatar/Avatar.svelte
@@ -1,19 +1,21 @@
 <script lang="ts">
+  import {t} from '$lib/i18n';
   import {concatClass} from '$lib/utils/components';
   import {abbreviate} from '$lib/utils/text/abbreviate';
   import type {AvatarProps} from './Avatar.type';
 
   type $$Props = AvatarProps;
   export let name: $$Props['name'];
-  export let src: $$Props['src'] = undefined;
+  export let image: $$Props['image'] = undefined;
   export let initials: $$Props['initials'] = undefined;
   export let color: $$Props['color'] = 'base-300';
+  export let linkFullImage: $$Props['linkFullImage'] = false;
 
   // Create class names
   let classes: string;
   $: {
     classes = `w-[3.125rem] h-[3.125rem] overflow-hidden flex justify-center items-center bg-${color}`;
-    classes += src ? ' rounded-md' : ' rounded-full';
+    classes += image ? ' rounded-md' : ' rounded-full';
   }
 </script>
 
@@ -24,9 +26,10 @@ Display either a photo or a initials-based avatar for an entity.
 ### Properties
 
 - `name`: The name of the entity. This is used either for the `alt` text of a possible image or the construction of a initials for a placeholder.
-- `src`: The `src` of the avatar image.
+- `image`: The possible avatar image.
 - `initials`: These can be provided to override the automatic initials construction.
 - `color`: The background color of the initials placeholder. @default `'base-300'`
+- `linkFullImage`: Whether to link the thumbnail to the full image. @default false
 - Any valid attributes of a `<figure>` element
 
 ### Usage
@@ -37,8 +40,24 @@ Display either a photo or a initials-based avatar for an entity.
 -->
 
 <figure {...concatClass($$restProps, classes)}>
-  {#if src}
-    <img class="h-full w-full object-contain" {src} alt={name} />
+  {#if image}
+    {#if linkFullImage}
+      <a
+        href={image.url}
+        target="_blank"
+        title={$t('aria.showFullImage')}
+        aria-label={$t('aria.showFullImage')}>
+        <img
+          class="border-bg-300 h-full w-full border-md object-cover"
+          src={image.thumbnail.url}
+          alt={name} />
+      </a>
+    {:else}
+      <img
+        class="border-bg-300 h-full w-full border-md object-cover"
+        src={image.thumbnail.url}
+        alt={name} />
+    {/if}
   {:else}
     <div class="avatar placeholder text-2xl">
       {initials ?? abbreviate(name)}
