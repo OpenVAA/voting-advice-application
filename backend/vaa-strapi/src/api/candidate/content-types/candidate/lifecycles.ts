1e01bcc569b340c1d72688a6db802542574ddb7e
Antti Pekkanen <antti.o.pekkanen@aalto.fi>
2024-03-22 15:51:18 +0200

    fix: password resetting bug
    Password resetting didn't work if the email of the user wasn't in lowercase. This caused
    issues when creating users for candidates that had email that wasn't in lowercase.

--- backend/vaa-strapi/src/api/candidate/content-types/candidate/lifecycles.ts
+++ backend/vaa-strapi/src/api/candidate/content-types/candidate/lifecycles.ts
@@ -3,5 +3,9 @@ import crypto from 'crypto';
 export default {
   beforeCreate(event) {
     event.params.data.registrationKey = event.params.data.registrationKey ?? crypto.randomUUID();
+    event.params.data.email = event.params.data.email?.toLowerCase();
+  },
+  beforeUpdate(event) {
+    event.params.data.email = event.params.data.email?.toLowerCase();
   }
 };
