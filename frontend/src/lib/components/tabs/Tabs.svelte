78d83825289d8d15986af96111c4fcee7069df31
kallelongjuhani <kalle.jarvenpaa@gmail.com>
2024-03-26 11:20:19 +0200

    refactor: Tabs component
    Update the `<Tabs>` component to match other compontents and move it
    away from the `shared` folder. Add property binding to `activeIndex` for
    easier use.

--- frontend/src/lib/components/tabs/Tabs.svelte
+++ frontend/src/lib/components/tabs/Tabs.svelte
@@ -1,7 +1,12 @@
 <script lang="ts">
   import {createEventDispatcher} from 'svelte';
-  export let tabs: string[] = [];
-  export let activeIndex = 0;
+  import {concatClass} from '$lib/utils/components';
+  import type {TabsProps} from './Tabs.type';
+
+  type $$Props = TabsProps;
+
+  export let tabs: $$Props['tabs'] = [];
+  export let activeIndex: $$Props['activeIndex'] = 0;
 
   const dispatch = createEventDispatcher();
 
@@ -11,7 +16,32 @@
   }
 </script>
 
-<ul class="flex items-center justify-center bg-base-300 px-lg py-8">
+<!--@component
+Show a tab title bar that can be used to switch between different tabs.
+
+### Properties
+
+- `tabs`: The titles of the tabs.
+- `activeIndex`: The index of the active tab. Bind to this to change or read the active tab. @default 0
+- Any valid attributes of a `<ul>` element
+
+### Events
+
+- `changeTab`: Emitted when the active tab changes. The event `details` contain the active tab index. Note, it's preferable to just bind to the `activeIndex` property instead.
+
+### Accessibility
+
+- The tab can be activated by pressing `Enter` or `Space`.
+- TODO: Add support for keyboard navigation with the arrow keys.
+
+### Usage
+
+```tsx
+<Tabs bind:activeIndex tabs={['Basic Info', 'Opinions']}/>
+```
+-->
+
+<ul {...concatClass($$restProps, 'flex items-center justify-center bg-base-300 px-lg py-8')}>
   {#each tabs as tab, i}
     <li
       class="btn btn-outline m-0 h-[2.2rem] min-h-[2.2rem] w-auto flex-grow truncate rounded-sm px-12 text-md text-secondary hover:bg-base-100 hover:text-primary focus:bg-base-100 focus:text-primary"
@@ -21,7 +51,7 @@
       role="tab"
       on:click={() => activate(i)}
       on:keyup={(e) => {
-        if (e.key === 'Enter') activate(i);
+        if (e.key === 'Enter' || e.key === ' ' || e.key === 'Spacebar') activate(i);
       }}>
       {tab}
     </li>
