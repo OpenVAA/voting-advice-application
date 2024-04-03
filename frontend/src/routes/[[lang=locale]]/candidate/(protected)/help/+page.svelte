6bfb3edd7ba57dc17c8a7e99257819ae9eea0a46
suviz <suvi.zitting@gmail.com>
2024-04-03 18:28:34 +0300

    fix: add variant and edit expander component

--- frontend/src/routes/[[lang=locale]]/candidate/(protected)/help/+page.svelte
+++ frontend/src/routes/[[lang=locale]]/candidate/(protected)/help/+page.svelte
@@ -3,7 +3,7 @@
   import {getRoute, Route} from '$lib/utils/navigation';
   import BasicPage from '$lib/templates/basicPage/BasicPage.svelte';
   import Button from '$lib/components/button/Button.svelte';
-  import QAExpander from './QAExpander.svelte';
+  import Expander from '$lib/components/expander/Expander.svelte';
 </script>
 
 <BasicPage title={$t('candidateApp.help.title')}>
@@ -11,25 +11,25 @@
     <p class="pb-lg">{$t('candidateApp.help.description')}</p>
   </div>
 
-  <QAExpander
-    question={$t('candidateApp.help.questions.question1.question')}
-    answer={$t('candidateApp.help.questions.question1.answer')} />
+  <Expander title={$t('candidateApp.help.questions.question1.question')} variant="question-help">
+    {$t('candidateApp.help.questions.question1.answer')}
+  </Expander>
 
-  <QAExpander
-    question={$t('candidateApp.help.questions.question2.question')}
-    answer={$t('candidateApp.help.questions.question2.answer')} />
+  <Expander title={$t('candidateApp.help.questions.question2.question')} variant="question-help">
+    {$t('candidateApp.help.questions.question2.answer')}
+  </Expander>
 
-  <QAExpander
-    question={$t('candidateApp.help.questions.question3.question')}
-    answer={$t('candidateApp.help.questions.question3.answer')} />
+  <Expander title={$t('candidateApp.help.questions.question3.question')} variant="question-help">
+    {$t('candidateApp.help.questions.question3.answer')}
+  </Expander>
 
-  <QAExpander
-    question={$t('candidateApp.help.questions.question4.question')}
-    answer={$t('candidateApp.help.questions.question4.answer')} />
+  <Expander title={$t('candidateApp.help.questions.question4.question')} variant="question-help">
+    {$t('candidateApp.help.questions.question4.answer')}
+  </Expander>
 
-  <QAExpander
-    question={$t('candidateApp.help.questions.question5.question')}
-    answer={$t('candidateApp.help.questions.question5.answer')} />
+  <Expander title={$t('candidateApp.help.questions.question5.question')} variant="question-help">
+    {$t('candidateApp.help.questions.question5.answer')}
+  </Expander>
 
   <svelte:fragment slot="primaryActions">
     <Button
