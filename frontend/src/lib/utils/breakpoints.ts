9ada5944d490f0bc1e57fb6a8ad7ba7662ccf363
kallelongjuhani <kalle.jarvenpaa@gmail.com>
2024-04-21 10:29:49 +0300

    feat: breakpoints utility enum
    Adds a `breakpoints` enumeration that contains the pixel widths for the
    tailwind screen size breakpoints. These can be used in components and on
    pages trigger layout changes not acchievable with tailwind class
    prefixes, such as `md:`.

---
+++ frontend/src/lib/utils/breakpoints.ts
@@ -0,0 +1,11 @@
+/**
+ * The breakpoint sizes for the different screen sizes.
+ * NB. Currently, the values used here are hardcoded and should be manually kept in line width the Tailwind definitions.
+ */
+export enum Breakpoints {
+  sm = 640,
+  md = 768,
+  lg = 1024,
+  xl = 1280,
+  '2xl' = 1536
+}
