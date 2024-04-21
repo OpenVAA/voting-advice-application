250a40b046c57f59f6804e9a5b205966682c0111
kallelongjuhani <kalle.jarvenpaa@gmail.com>
2024-04-21 10:25:11 +0300

    feat: layout variants for QuestionActions
    Adds `tight` and `icon` layout `variant`s to the `<QuestionActions>`
    component. These make the buttons progressively more compact and can be
    used in circumastances where space is scarce, such as video pages.

--- frontend/src/lib/components/questions/QuestionActions.svelte
+++ frontend/src/lib/components/questions/QuestionActions.svelte
@@ -8,13 +8,13 @@
   type $$Props = QuestionActionsProps;
 
   export let answered: $$Props['answered'] = false;
+  export let variant: $$Props['variant'] = 'default';
   export let separateSkip: $$Props['separateSkip'] = false;
 
   const dispatch = createEventDispatcher();
 
   function onDelete() {
-    // We need to check `answered` here, because the button might be shown even when
-    // no answer was given due to the delay in hiding and showing actions
+    // We need to check `answered` here, because the button might be shown even when no answer was given due to the delay in hiding and showing actions
     if (answered) dispatch('delete');
   }
 
@@ -33,25 +33,19 @@ Display the question's secondary actions, such as skip.
 
 ### Properties
 
-- `answered`: Set to `true` if the question has already been answered.
-  This controls which actions are shown. @default `false`
-- `separateSkip`: Whether to separate `skip` and `next` actions both as 
-  events and button symbols. @default `false`
+- `answered`: Set to `true` if the question has already been answered. This controls which actions are shown. @default `false`
+- `variant`: Use to switch between looser and tighter layouts. @default `'default'`
+- `separateSkip`: Whether to separate `skip` and `next` actions both as events and button symbols. @default `false`
 - Any valid properties of a `<div>` element
 
 ### Events
 
-If `separateSkip` is set to `true`, the `next` event is switched to a 
-`skip` event if the user has not yet answered the question. Otherwise,
-only `next` events will be fired.
+If `separateSkip` is set to `true`, the `next` event is switched to a `skip` event if the user has not yet answered the question. Otherwise, only `next` events will be fired.
 
 - `previous`: The user has clicked on the previous button.
-- `next`: The user has clicked on the next button. This is only available
-  if `answered` is `true` or `separateSkip` is `true`.
-- `skip`: The user has clicked on the skip button. This is only available
-  if `answered` is `false` and `separateSkip` is `true`.
-- `delete`: The user has clicked on the delete answer button. This is only 
-  available if `answered` is `true`.
+- `next`: The user has clicked on the next button. This is only available if `answered` is `true` or `separateSkip` is `true`.
+- `skip`: The user has clicked on the skip button. This is only available if `answered` is `false` and `separateSkip` is `true`.
+- `delete`: The user has clicked on the delete answer button. This is only available if `answered` is `true`.
 
 ### Usage
 
@@ -59,6 +53,7 @@ only `next` events will be fired.
 <QuestionActions 
   answered={voterAnswer != null}
   separateSkip={true}
+  variant="tight"
   on:previous={gotoPreviousQuestion}
   on:delete={deleteAnswer}
   on:next={gotoNextQuestion}
@@ -74,7 +69,9 @@ only `next` events will be fired.
     on:click={onNext}
     style="grid-row: 1; grid-column: 3"
     color="secondary"
-    iconPos="top"
+    variant={variant === 'icon' ? 'icon' : 'secondary'}
+    iconPos={variant === 'tight' ? 'right' : 'top'}
+    class={variant === 'icon' || variant === 'tight' ? 'content-end' : ''}
     icon={answered || !separateSkip ? 'next' : 'skip'}
     text={answered || !separateSkip ? $t('questions.next') : $t('questions.skip')} />
   <Button
@@ -83,14 +80,17 @@ only `next` events will be fired.
     class="transition-opacity delay-500 disabled:opacity-0"
     style="grid-row: 1; grid-column: 2"
     color="secondary"
-    iconPos="top"
+    variant={variant === 'icon' ? 'icon' : 'secondary'}
+    iconPos={variant === 'tight' ? 'left' : 'top'}
     icon="close"
     text={$t('questions.remove')} />
   <Button
     on:click={onPrevious}
     style="grid-row: 1; grid-column: 1"
     color="secondary"
-    iconPos="top"
+    variant={variant === 'icon' ? 'icon' : 'secondary'}
+    iconPos={variant === 'tight' ? 'left' : 'top'}
+    class={variant === 'icon' || variant === 'tight' ? 'content-start' : ''}
     icon="previous"
     text={$t('questions.previous')} />
 </div>
