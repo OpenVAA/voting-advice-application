385286860fcb6197731e817ea3efcbd0f7371541
kallelongjuhani <kalle.jarvenpaa@gmail.com>
2024-04-21 10:25:11 +0300

    feat: layout variants for QuestionActions
    Adds `tight` and `icon` layout `variant`s to the `<QuestionActions>`
    component. These make the buttons progressively more compact and can be
    used in circumastances where space is scarce, such as video pages.

--- frontend/src/lib/components/questions/QuestionActions.type.ts
+++ frontend/src/lib/components/questions/QuestionActions.type.ts
@@ -2,13 +2,15 @@ import type {SvelteHTMLElements} from 'svelte/elements';
 
 export type QuestionActionsProps = SvelteHTMLElements['div'] & {
   /**
-   * Set to `true` if the question has already been answered. This controls which actions are shown.
-   * @default false
+   * Set to `true` if the question has already been answered. This controls which actions are shown. @default false
    */
   answered?: boolean | null;
   /**
-   * Whether to separate `skip` and `next` actions both as events and button symbols.
-   * @default false
+   * Use to switch between looser and tighter layouts. @default 'default'
+   */
+  variant?: 'default' | 'tight' | 'icon';
+  /**
+   * Whether to separate `skip` and `next` actions both as events and button symbols. @default false
    */
   separateSkip?: boolean | null;
 };
