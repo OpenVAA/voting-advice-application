80fc581fd77f3b24b27abff86364876db21f3434
kallelongjuhani <kalle.jarvenpaa@gmail.com>
2024-04-21 10:13:06 +0300

    feat: sanitizeHTML utility
    The `sanitizeHTML` should be used whenever rendering user-supplied
    content with the Svelte `@html` directive.

---
+++ frontend/src/lib/utils/sanitize.ts
@@ -0,0 +1,11 @@
+import DOMPurify from 'isomorphic-dompurify';
+
+/**
+ * Sanitize HTML received from the server for displaying with Svelte `@html`
+ * @param html Dirty HTML
+ * @returns Sanitized HTML
+ */
+export function sanitizeHtml(html?: string): string {
+  if (!html) return '';
+  return DOMPurify.sanitize(html, {USE_PROFILES: {html: true}});
+}
