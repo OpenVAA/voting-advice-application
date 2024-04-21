d2f99ee2c2254234e1e204c61ef2ef1113242cdc
kallelongjuhani <kalle.jarvenpaa@gmail.com>
2024-04-21 11:07:37 +0300

    feat: Video component
    Adds a `<Video>` component that can be used to display video content and
    associated HTML transcripts.

---
+++ frontend/src/lib/components/video/component-stores.ts
@@ -0,0 +1,10 @@
+import {writable} from 'svelte/store';
+
+/**
+ * Store for persistent video selections.
+ */
+export const videoPreferences = writable({
+  muted: false,
+  textTracksHidden: false,
+  transcriptVisible: false
+});
