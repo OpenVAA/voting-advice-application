b877efcbcfed6a1b85ee8fa85196632e17186ffe
kallelongjuhani <kalle.jarvenpaa@gmail.com>
2024-04-23 11:36:29 +0300

    refactor/i18n: match appLabels with frontend translations
    Synchronises Strapi `App Labels`' terms, including mock data generation,
    with the pruned translations used in the frontend.
    
    NB. In the future, the `App Labels`' components containing the
    translations should be automatically generated from the local
    translation files. To that effect, however, the hierarchy or naming need
    be changed, because keys containing periods, e.g. "foo.bar.foo" cannot
    be used as field names in Strapi.

--- backend/vaa-strapi/types/generated/components.d.ts
+++ backend/vaa-strapi/types/generated/components.d.ts
@@ -10,36 +10,17 @@ export interface LabelsActionLabels extends Schema.Component {
     startButton: Attribute.String &
       Attribute.Required &
       Attribute.DefaultTo<'Start Finding Candidates!'>;
-    electionInfo: Attribute.String &
+    electionInfo: Attribute.Text &
       Attribute.Required &
-      Attribute.DefaultTo<'Information about the elections'>;
-    howItWorks: Attribute.String &
+      Attribute.DefaultTo<'Information About the Elections'>;
+    howItWorks: Attribute.Text &
       Attribute.Required &
-      Attribute.DefaultTo<'How does this app work?'>;
+      Attribute.DefaultTo<'How Does This App Work?'>;
     help: Attribute.String & Attribute.Required & Attribute.DefaultTo<'Help'>;
-    searchMunicipality: Attribute.String &
-      Attribute.Required &
-      Attribute.DefaultTo<'Your Municipality or Town'>;
     startQuestions: Attribute.String &
       Attribute.Required &
       Attribute.DefaultTo<'Start the Questionnaire'>;
-    selectCategories: Attribute.String &
-      Attribute.Required &
-      Attribute.DefaultTo<'Select Categories'>;
-    previous: Attribute.String & Attribute.Required & Attribute.DefaultTo<'Previous'>;
-    answerCategoryQuestions: Attribute.String &
-      Attribute.Required &
-      Attribute.DefaultTo<'Answer {numQuestions} Questions'>;
-    readMore: Attribute.String & Attribute.Required & Attribute.DefaultTo<'Read More'>;
-    skip: Attribute.String & Attribute.Required & Attribute.DefaultTo<'Skip'>;
-    filter: Attribute.String & Attribute.Required & Attribute.DefaultTo<'Filter Results'>;
-    alphaOrder: Attribute.String & Attribute.Required & Attribute.DefaultTo<'A-Z'>;
-    bestMatchOrder: Attribute.String & Attribute.Required & Attribute.DefaultTo<'Best Match'>;
-    addToList: Attribute.String & Attribute.Required & Attribute.DefaultTo<'Add to List'>;
-    candidateBasicInfo: Attribute.String & Attribute.Required & Attribute.DefaultTo<'Basic Info'>;
-    candidateOpinions: Attribute.String & Attribute.Required & Attribute.DefaultTo<'Opinions'>;
     home: Attribute.String & Attribute.Required & Attribute.DefaultTo<'home'>;
-    constituency: Attribute.String & Attribute.Required & Attribute.DefaultTo<'Constituency'>;
     opinions: Attribute.String & Attribute.Required & Attribute.DefaultTo<'Opinions'>;
     results: Attribute.String & Attribute.Required & Attribute.DefaultTo<'Results'>;
     yourList: Attribute.String & Attribute.Required & Attribute.DefaultTo<'Your List'>;
@@ -53,42 +34,20 @@ export interface LabelsViewTexts extends Schema.Component {
     description: '';
   };
   attributes: {
-    toolTitle: Attribute.String & Attribute.Required & Attribute.DefaultTo<'Election Compass'>;
-    toolDescription: Attribute.Text &
-      Attribute.Required &
-      Attribute.DefaultTo<'With this application you can compare candidates in the elections on {electionDate, date, ::yyyyMMdd} based on their opinions, parties and other data.'>;
     publishedBy: Attribute.String &
       Attribute.Required &
       Attribute.DefaultTo<'Published by {publisher}'>;
     madeWith: Attribute.String & Attribute.Required & Attribute.DefaultTo<'Made with '>;
-    selectMunicipalityTitle: Attribute.String &
-      Attribute.Required &
-      Attribute.DefaultTo<'Select Your Municipality'>;
-    selectMunicipalityDescription: Attribute.Text &
-      Attribute.Required &
-      Attribute.DefaultTo<'In these elections, you can only vote for candidates in your own constituency. Select your municipality and the app will find it for you.'>;
-    yourConstituency: Attribute.String &
-      Attribute.Required &
-      Attribute.DefaultTo<'Your constituency is {constituency}'>;
     yourOpinionsTitle: Attribute.String &
       Attribute.Required &
       Attribute.DefaultTo<'Tell Your Opinions'>;
-    yourOpinionsDescription: Attribute.Text &
-      Attribute.Required &
+    questionsTip: Attribute.Text &
+      Attribute.DefaultTo<'Tip: If you don\u2019t care about an issue, you can skip it.'>;
+    appTitle: Attribute.String & Attribute.Required & Attribute.DefaultTo<'Election Compass'>;
+    frontpageIngress: Attribute.Text &
+      Attribute.DefaultTo<'With this application you can compare candidates in the elections on {electionDate, date, ::yyyyMMdd} based on their opinions, parties and other data.'>;
+    yourOpinionsIngress: Attribute.Text &
       Attribute.DefaultTo<'Next, the app will ask your opinions on {numStatements} statements about political issues and values, which the candidates have also answered. After you\u2019ve answered them, the app will find the candidates that best agree with your opinions. The statements are grouped into {numCategories} categories. You can answer all of them or only select those you find important.'>;
-    questionsTip: Attribute.String &
-      Attribute.DefaultTo<'Tip: If you don\u2019t care about a single issue or a category of them, you can skip it later.'>;
-    yourCandidatesTitle: Attribute.String &
-      Attribute.Required &
-      Attribute.DefaultTo<'Your Candidates'>;
-    yourCandidatesDescription: Attribute.Text &
-      Attribute.Required &
-      Attribute.DefaultTo<'These are the candidates in your constituency. The best matches are first on the list. You can also see which {numCandidates} best match your opinions. To narrow down the results, you can also use {filters}.'>;
-    yourPartiesTitle: Attribute.String & Attribute.Required & Attribute.DefaultTo<'Your Parties'>;
-    yourPartiesDescription: Attribute.Text &
-      Attribute.Required &
-      Attribute.DefaultTo<'These are the parties in your constituency. The best matches are first on the list. You can also see which individual {partiesTerm} best match your opinions. To narrow down the results, you can also use {filters}.'>;
-    appTitle: Attribute.String & Attribute.Required & Attribute.DefaultTo<'Election App'>;
   };
 }
 
