59c472f94c5263e45e12bde9babf8f764535b25c
kallelongjuhani <kalle.jarvenpaa@gmail.com>
2024-04-24 12:11:14 +0300

    feat: load data to Strapi on initialise
    Adds a function for importing full election data to Strapi in
    initialise, including images.

---
+++ backend/vaa-strapi/src/functions/utils/api.ts
@@ -0,0 +1,16 @@
+export enum API {
+  Answer = 'api::answer.answer',
+  AppLabel = 'api::election-app-label.election-app-label',
+  AppSettings = 'api::app-setting.app-setting',
+  Candidate = 'api::candidate.candidate',
+  Constituency = 'api::constituency.constituency',
+  Election = 'api::election.election',
+  Gender = 'api::gender.gender',
+  Language = 'api::language.language',
+  Nomination = 'api::nomination.nomination',
+  Party = 'api::party.party',
+  Question = 'api::question.question',
+  QuestionCategory = 'api::question-category.question-category',
+  QuestionType = 'api::question-type.question-type',
+  User = 'plugin::users-permissions.user'
+}
