5f086709524ab5446c6168619736e34c90ceb7da
kallelongjuhani <kalle.jarvenpaa@gmail.com>
2024-04-25 10:58:11 +0300

    feat: election store
    Adds a utility store for the `ElectionProps` object that's part of
    `PageData`.

--- frontend/src/routes/[[lang=locale]]/(voters)/+page.svelte
+++ frontend/src/routes/[[lang=locale]]/(voters)/+page.svelte
@@ -1,24 +1,23 @@
 <script lang="ts">
-  import {page} from '$app/stores';
   import {t} from '$lib/i18n';
   import {getRoute, Route} from '$lib/utils/navigation';
-  import {resetLocalStorage, settings} from '$lib/utils/stores';
+  import {election, resetLocalStorage, settings} from '$lib/utils/stores';
   import {Button} from '$lib/components/button';
   import {HeadingGroup, PreHeading} from '$lib/components/headingGroup';
   import {FrontPage} from '$lib/templates/frontPage';
 </script>
 
-<FrontPage title={$page.data.election.name}>
+<FrontPage title={$election?.name ?? ''}>
   <HeadingGroup slot="heading">
     <PreHeading class="text-2xl font-bold text-primary">{$t('viewTexts.appTitle')}</PreHeading>
-    <h1 class="text-3xl font-normal">{$page.data.election.name}</h1>
+    <h1 class="text-3xl font-normal">{$election?.name ?? ''}</h1>
   </HeadingGroup>
 
   <img slot="hero" class="bg-white" src={$settings.poster?.url ?? '/images/hero.png'} alt="" />
 
   <p class="text-center">
     {$t('viewTexts.frontpageIngress', {
-      electionDate: new Date($page.data.election.electionDate)
+      electionDate: new Date($election?.electionDate ?? '')
     })}
   </p>
 
