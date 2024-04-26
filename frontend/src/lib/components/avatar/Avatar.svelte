83d91299c99449ee3c43dfde5093ae255efc029b
kallelongjuhani <kalle.jarvenpaa@gmail.com>
2024-04-26 09:53:43 +0300

    feat: custom colors for text Avatar and dynamic text sizing
    Adds properties for defining a custom bg color for the `<Avatar>`
    component when it's used to show initials, as well as one for defining
    the text color.
    
    Also changes the text size dynamically based on the length of the
    `initials` text so that it fits better on the background.
    
    NB. For an advanced solution, this might be changed to an `<svg>`
    element in the future, which can automatically adjust text size to fit
    the allotted space.
    
    The first use case for this is for displaying parties.

--- frontend/src/lib/components/avatar/Avatar.svelte
+++ frontend/src/lib/components/avatar/Avatar.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
   import {t} from '$lib/i18n';
-  import {concatClass} from '$lib/utils/components';
+  import {concatProps} from '$lib/utils/components';
   import {abbreviate} from '$lib/utils/text/abbreviate';
   import type {AvatarProps} from './Avatar.type';
 
@@ -9,13 +9,55 @@
   export let image: $$Props['image'] = undefined;
   export let initials: $$Props['initials'] = undefined;
   export let color: $$Props['color'] = 'base-300';
+  export let textColor: $$Props['textColor'] = 'neutral';
+  export let customColor: $$Props['customColor'] = undefined;
+  export let customColorDark: $$Props['customColorDark'] = undefined;
   export let linkFullImage: $$Props['linkFullImage'] = false;
 
   // Create class names
   let classes: string;
+  let styles: string;
+  let initialsClasses: string;
   $: {
-    classes = `w-[3.125rem] h-[3.125rem] overflow-hidden flex justify-center items-center bg-${color}`;
+    classes = `w-[3.125rem] h-[3.125rem] overflow-hidden flex justify-center items-center text-${textColor}`;
     classes += image ? ' rounded-md' : ' rounded-full';
+    styles = '';
+    initialsClasses = 'avatar placeholder';
+
+    // Set custom color (if we have an image, these are not needed)
+    if (!image) {
+      if (customColor) {
+        styles += ` --bg-color: ${customColor};`;
+        classes += ' bg-[var(--bg-color)]';
+        if (!customColorDark) classes += ` dark:bg-${color}`;
+      }
+      if (customColorDark) {
+        styles += ` --bg-color-dark: ${customColorDark};`;
+        classes += ' dark:bg-[var(--bg-color-dark)]';
+        if (!customColor) classes += ` bg-${color}`;
+      }
+      if (!customColor && !customColorDark) {
+        classes += ` bg-${color}`;
+      }
+      // Set initials size
+      if (!initials) initials = abbreviate(name);
+      switch (initials.length) {
+        case 1:
+        case 2:
+          initialsClasses += ' text-2xl';
+          break;
+        case 3:
+        case 4:
+          initialsClasses += ' text-xl';
+          break;
+        case 5:
+        case 6:
+          initialsClasses += ' text-lg';
+          break;
+        default:
+          initialsClasses += ' text-md';
+      }
+    }
   }
 </script>
 
@@ -29,6 +71,9 @@ Display either a photo or a initials-based avatar for an entity.
 - `image`: The possible avatar image.
 - `initials`: These can be provided to override the automatic initials construction.
 - `color`: The background color of the initials placeholder. @default `'base-300'`
+- `textColor`: The color of the initials text placeholder. @default `'neutral'`
+- `customColor`: A custom color string to use for the background color of the initials placeholder, e.g. in case of parties, which will override the `color` property. Make sure to define both `customColor` and `customColorDark` together.
+- `customColorDark`: A custom color string to use for the background color of the initials placeholder in dark mode, which will override the `color` property.
 - `linkFullImage`: Whether to link the thumbnail to the full image. @default false
 - Any valid attributes of a `<figure>` element
 
@@ -39,7 +84,7 @@ Display either a photo or a initials-based avatar for an entity.
 ```
 -->
 
-<figure {...concatClass($$restProps, classes)}>
+<figure {...concatProps($$restProps, {class: classes, style: styles})}>
   {#if image}
     {#if linkFullImage}
       <a
@@ -59,8 +104,6 @@ Display either a photo or a initials-based avatar for an entity.
         alt={name} />
     {/if}
   {:else}
-    <div class="avatar placeholder text-2xl">
-      {initials ?? abbreviate(name)}
-    </div>
+    <div class={initialsClasses}>{initials}</div>
   {/if}
 </figure>
