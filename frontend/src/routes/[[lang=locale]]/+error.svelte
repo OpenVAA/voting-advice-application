8197b43dc5c8baf979ab7db700d53718ffef3e42
kallelongjuhani <kalle.jarvenpaa@gmail.com>
2024-04-27 10:47:52 +0300

    refactor: nicer and more informative error page

--- frontend/src/routes/[[lang=locale]]/+error.svelte
+++ frontend/src/routes/[[lang=locale]]/+error.svelte
@@ -1,10 +1,20 @@
 <script lang="ts">
   import {page} from '$app/stores';
   import {t} from '$lib/i18n';
+  import {sanitizeHtml} from '$lib/utils/sanitize';
+  import {HeroEmoji} from '$lib/components/heroEmoji';
+  import {BasicPage} from '$lib/templates/basicPage/';
+
+  let title: string;
+  $: {
+    const key = `error.${$page.status}`;
+    title = $t(key);
+    // This means, there was no error message defined for this status code.
+    if (title === key) title = $t('error.default');
+  }
 </script>
 
-<div class="flex h-full flex-col justify-center">
-  <h1 class="text-center text-xl text-error">
-    {$t(`error.${$page.status}`, {default: $t('error.default')})}
-  </h1>
-</div>
+<BasicPage {title} titleClass="text-warning">
+  <HeroEmoji slot="hero" emoji={$t('error.heroEmoji')} />
+  <div class="text-center">{@html sanitizeHtml($t('error.content'))}</div>
+</BasicPage>
