33c5bc72dff74089b35887e648e7ab39e8cec341
kallelongjuhani <kalle.jarvenpaa@gmail.com>
2024-05-02 20:58:27 +0300

    feat: show subentities in EntityDetails
    Adds a `<EntitySubentities>` component that can be used to show an
    entity's subentities, such as a party's candidates, in
    `<EntityDetails>`.

--- frontend/src/lib/components/entityDetails/index.ts
+++ frontend/src/lib/components/entityDetails/index.ts
@@ -1,4 +1,5 @@
 export {default as EntityInfo} from './EntityInfo.svelte';
 export {default as EntityDetails} from './EntityDetails.svelte';
 export {default as EntityOpinions} from './EntityOpinions.svelte';
+export {default as EntitySubentities} from './EntitySubentities.svelte';
 export * from './EntityDetails.type';
