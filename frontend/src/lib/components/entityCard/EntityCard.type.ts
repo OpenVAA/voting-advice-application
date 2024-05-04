25150d3673148fe7cc8175856ad86f8f984ebfb4
kallelongjuhani <kalle.jarvenpaa@gmail.com>
2024-05-04 09:43:49 +0300

    feat: add settings for specifying question format in EntityCards
    Extends the format for setting questions in
    `results.cardContents[entityType]` so that their question label can be
    hidden by setting `hideLabel: true` and a `format` option passed to
    the `<InfoAnswer>` component.

--- frontend/src/lib/components/entityCard/EntityCard.type.ts
+++ frontend/src/lib/components/entityCard/EntityCard.type.ts
@@ -1,4 +1,5 @@
 import type {SvelteHTMLElements} from 'svelte/elements';
+import type {InfoAnswerProps} from '../infoAnswer';
 export type EntityCardProps = SvelteHTMLElements['article'] & {
   /**
    * The action to take when the card is clicked. If the card has subentites, the action will only be triggered by clicking the header of the card.
@@ -13,9 +14,22 @@ export type EntityCardProps = SvelteHTMLElements['article'] & {
    */
   context?: 'list' | 'details' | 'subcard';
   /**
-   * Possible question whose answers should be shown in the card.
+   * Possible question whose answers should be shown in the card, along with possibly options for their display.
    */
-  questions?: QuestionProps[];
+  questions?: {
+    /**
+     * The question whose answer is shown.
+     */
+    question: QuestionProps;
+    /**
+     * Whether to hide the question label in the card.
+     */
+    hideLabel?: boolean;
+    /**
+     * How to format the answer. See `<InfoAnswer>` for details. @default 'default'
+     */
+    format?: InfoAnswerProps['format'];
+  }[];
   /**
    * Possible sub-entities of the entity to show in the card, e.g. candidates for a party.
    */
