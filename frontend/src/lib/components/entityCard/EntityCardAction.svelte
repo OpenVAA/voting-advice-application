4e1210a7947d8d22c9754b9973941b51e61992ef
kallelongjuhani <kalle.jarvenpaa@gmail.com>
2024-05-04 14:46:55 +0300

    refactor: extend interactive area in EntityCard
    Extends the interactive area in `<EntityCard>`s with subentities to
    cover possible submatches and questions.

--- frontend/src/lib/components/entityCard/EntityCardAction.svelte
+++ frontend/src/lib/components/entityCard/EntityCardAction.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import {error} from '@sveltejs/kit';
+  import {concatClass} from '$lib/utils/components';
   import type {EntityCardActionProps} from './EntityCardAction.type';
 
   type $$Props = EntityCardActionProps;
@@ -15,6 +16,7 @@ A simple utility component for possibly wrapping content in an action handler.
 
 - `action`: The action to take when the part or card is clicked.
 - `shadeOnHover`: Whether to shade the element on hover. Use when applying to subcards or their parent card's header. @default `false`
+- Any valid attributes common to HTML elements. Note that these will only be applied if `<EntityCardAction>` is rendered.
 
 ### Slots
 
@@ -32,11 +34,14 @@ A simple utility component for possibly wrapping content in an action handler.
 {#if action == null}
   <slot />
 {:else if typeof action === 'function'}
-  <button on:click={action} class="!text-neutral" class:hover-shaded={shadeOnHover}>
+  <button
+    on:click={action}
+    class:hover-shaded={shadeOnHover}
+    {...concatClass($$restProps, '!text-neutral')}>
     <slot />
   </button>
 {:else if typeof action === 'string'}
-  <a href={action} class="!text-neutral" class:hover-shaded={shadeOnHover}>
+  <a href={action} class:hover-shaded={shadeOnHover} {...concatClass($$restProps, '!text-neutral')}>
     <slot />
   </a>
 {:else}
@@ -45,7 +50,7 @@ A simple utility component for possibly wrapping content in an action handler.
 
 <style lang="postcss">
   .hover-shaded {
-    /** Hover: is a valid prefix */
+    /* hover: is a valid prefix */
     @apply rounded-md transition-all hover:bg-base-content/20 hover:ring-4 hover:ring-base-content/20;
   }
 </style>
