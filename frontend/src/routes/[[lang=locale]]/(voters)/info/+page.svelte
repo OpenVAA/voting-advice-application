2a65093f0b3b69eb425246c58b6af0f9d8d626c7
kallelongjuhani <kalle.jarvenpaa@gmail.com>
2024-05-06 14:30:22 +0300

    custom: /info route

--- frontend/src/routes/[[lang=locale]]/(voters)/info/+page.svelte
+++ frontend/src/routes/[[lang=locale]]/(voters)/info/+page.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-  import {t} from '$lib/i18n';
+  import {locale, t} from '$lib/i18n';
   import {getRoute, Route} from '$lib/utils/navigation';
   import {sanitizeHtml} from '$lib/utils/sanitize';
   import {election, openFeedbackModal, settings} from '$lib/stores';
@@ -7,6 +7,14 @@
   import {HeadingGroup, PreHeading} from '$lib/components/headingGroup';
   import {HeroEmoji} from '$lib/components/heroEmoji';
   import {BasicPage} from '$lib/templates/basicPage';
+  import introVideos from '../intro/introVideos.json';
+
+  // Use the transcripts from the intro videos for text content here
+  let videoProps: CustomVideoProps[];
+
+  $: videoProps = introVideos
+    .slice(1)
+    .map((d) => d[$locale as keyof (typeof introVideos)[0]]?.video);
 </script>
 
 <BasicPage title={$t('info.title')}>
@@ -36,15 +44,31 @@
       text={$t('info.returnButton')} />
   </svelte:fragment>
 
-  <div>
-    {@html sanitizeHtml(
-      $t('info.content', {
-        electionDate: new Date($election?.electionDate ?? '')
-      })
-    )}
+  <div class="text-content">
+    {#each videoProps as { title, transcript }}
+      <h2 class="mb-md mt-lg">{title}</h2>
+      <div>
+        {@html sanitizeHtml(transcript)}
+      </div>
+    {/each}
   </div>
 
   <svelte:fragment slot="primaryActions">
     <Button variant="main" href={$getRoute(Route.Home)} text={$t('info.returnButton')} />
   </svelte:fragment>
 </BasicPage>
+
+<style lang="postcss">
+  :global(.text-content img) {
+    @apply mx-auto my-lg max-h-[100vw] rounded-sm;
+  }
+  :global(.text-content figure) {
+    @apply mb-lg text-center text-sm;
+  }
+  :global(.text-content figure img) {
+    @apply mb-sm;
+  }
+  :global(.text-content figure figcaption) {
+    @apply mb-lg text-center text-sm;
+  }
+</style>
