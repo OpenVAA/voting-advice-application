0e46f25a5b7a47854dce088f8cfdf8babfd63ab2
kallelongjuhani <kalle.jarvenpaa@gmail.com>
2024-05-06 15:24:41 +0300

    fix: InfoItem layout on Safari

--- frontend/src/lib/components/entityDetails/InfoItem.svelte
+++ frontend/src/lib/components/entityDetails/InfoItem.svelte
@@ -24,7 +24,7 @@ Used to show a label-content pair in a Candidate's basic information.
 ```
 -->
 
-<div class="infoItem grid grid-flow-col justify-start gap-md">
+<div class="infoItem grid grid-flow-col grid-cols-[min-content_auto] justify-start gap-md">
   <!-- pt-[0.3rem] matches the baselines of the small label and the content text -->
   <div class="small-label w-min min-w-[7rem] pt-[0.3rem] text-left align-top">{label}</div>
   <div class="overflow-hidden align-top">
