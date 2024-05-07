48712bbba6fcba2463da64026a68e2e0ccea2fdc
kallelongjuhani <kalle.jarvenpaa@gmail.com>
2024-05-07 11:42:16 +0300

    feat: add admin email to /about page

--- frontend/src/routes/[[lang=locale]]/(voters)/about/+page.svelte
+++ frontend/src/routes/[[lang=locale]]/(voters)/about/+page.svelte
@@ -36,6 +36,17 @@
     )}
   {/if}
 
+  {#if $settings.admin?.email}
+    <h2 class="mb-md mt-lg">{$t('about.feedbackTitle')}</h2>
+    <p>
+      {$t('about.feedbackContent')}
+      <a
+        href="mailto:{$settings.admin.email}"
+        class="small-label me-md inline-block rounded-[1rem] bg-base-300 px-md py-sm"
+        >{$settings.admin.email}</a>
+    </p>
+  {/if}
+
   {#if $settings.appVersion.source}
     <h2 class="mb-md mt-lg">{$t('about.sourceTitle')}</h2>
     <p>
