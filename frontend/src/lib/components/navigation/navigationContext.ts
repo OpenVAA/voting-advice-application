f9dcdd166b65ab3c7ff2bfd8f002e2de0ddb4040
kallelongjuhani <kalle.jarvenpaa@gmail.com>
2024-05-08 12:39:52 +0300

    feat: close navigation when clicked
    Defines a `NavigationContext` which individual `<NavItems>` can use to
    close the navigation menu when clicked.

---
+++ frontend/src/lib/components/navigation/navigationContext.ts
@@ -0,0 +1,30 @@
+import {getContext, setContext} from 'svelte';
+
+/**
+ * Get the context object that should contain a callback for closing the navigation menu.
+ */
+export function getNavigationContext() {
+  return getContext<NavigationContext>(NAVIGATION_CONTEXT_NAME) ?? {};
+}
+
+/**
+ * Create context object that should contain a callback for closing the navigation menu.
+ */
+export function setNavigationContext(context: NavigationContext) {
+  setContext<NavigationContext>(NAVIGATION_CONTEXT_NAME, context);
+}
+
+/**
+ * The name of the navigation context.
+ */
+const NAVIGATION_CONTEXT_NAME = 'vaa-navigation';
+
+/**
+ * A context object that should contain a callback for closing the navigation menu.
+ */
+interface NavigationContext {
+  /**
+   * A function that closes the navigation drawer.
+   */
+  close?: () => void;
+}
