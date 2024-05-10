64fbcb5c8d3e3ababdf4a94c64541b07af8ac938
kallelongjuhani <kalle.jarvenpaa@gmail.com>
2024-05-10 14:39:20 +0300

    feat: Alert component
    Adds an `<Alert>` component that can be used to show a non-model
    alert or dialog that appears at the bottom of the screen.

---
+++ frontend/src/lib/components/alert/Alert.svelte
@@ -0,0 +1,118 @@
+<script lang="ts">
+  import {createEventDispatcher, onMount} from 'svelte';
+  import {concatClass, getUUID} from '$lib/utils/components';
+  import {Button} from '$lib/components/button';
+  import {Icon} from '$lib/components/icon';
+  import type {AlertProps} from './Alert.type';
+
+  type $$Props = AlertProps;
+
+  export let title: $$Props['title'];
+  export let icon: $$Props['icon'] = undefined;
+  export let autoOpen: $$Props['autoOpen'] = true;
+
+  // Bindable
+  export let isOpen: $$Props['isOpen'] = false;
+
+  // For aria references
+  const contentId = getUUID();
+
+  const dispatchEvent = createEventDispatcher();
+
+  onMount(() => {
+    if (autoOpen) openAlert();
+  });
+
+  export function openAlert() {
+    isOpen = true;
+    dispatchEvent('open');
+  }
+
+  export function closeAlert() {
+    isOpen = false;
+    dispatchEvent('close');
+  }
+</script>
+
+<!--
+@component
+Show a non-model alert or dialog that appears at the bottom of the screen.
+
+### Slots
+
+- `actions`: The action buttons to display.
+- default: The content of the alert.
+
+### Properties
+
+- `title`: The title of the modal 
+- `icon`: The icon of the alert. @default `info`
+- `autoOpen`: Whether to open the alert automatically. @default `true`
+- Any valid properties of a `<div>` element.
+
+### Bindable functions
+
+- `openAlert`: Opens the alert
+- `closeAlert`: Closes the alert
+
+### Events
+
+- `open`: Fired after the alert is opened.
+- `close`: Fired when the alert is closed by any means.
+- Neither event has any details.
+
+### Usage
+
+```tsx
+<script lang="ts">
+  let closeAlert: () => void;
+</script>
+<Alert 
+  bind:closeAlert
+  title="Can we help you?"
+  icon="warning"
+  on:close={() => console.info('Alert closed')}>
+  Please tell us whether we can help you?
+  <div slot="actions" class="flex flex-col w-full max-w-md mx-auto">
+    <Button on:click={() => {console.info('Yes'); closeAlert();}} text="Yes" variant="main"/>
+    <Button on:click={closeAlert} text="No"/>
+  </div>
+</Alert>
+```
+-->
+
+<div
+  role={$$slots.actions ? 'dialog' : 'alert'}
+  aria-modal="false"
+  aria-label={title}
+  aria-describedby={contentId}
+  {...concatClass(
+    $$restProps,
+    'alert fixed z-30 w-auto justify-items-stretch shadow-xl transition-all sm:!pr-[2rem] ' +
+      (icon ? '' : 'sm:grid-cols-[minmax(auto,1fr)_auto] ') +
+      'bottom-0 left-0 right-0 pb-safelgb pl-safelgl rounded-b-none ' +
+      'sm:bottom-safelgb sm:left-safelgl sm:right-safelgr sm:p-lg sm:rounded-b-[var(--rounded-box,1rem)]'
+  )}
+  class:vaa-alert-hidden={!isOpen}>
+  {#if icon}
+    <Icon name={icon} class="justify-self-center" />
+  {/if}
+  <div id={contentId} class="w-full">
+    <slot />
+  </div>
+  <div>
+    {#if $$slots.actions}
+      <slot name="actions" />
+    {:else}
+      <Button on:click={closeAlert} text="Dismiss" class="-mt-[1rem] sm:mt-0" />
+    {/if}
+  </div>
+  <button on:click={closeAlert} class="btn btn-circle btn-ghost btn-sm absolute right-2 top-2"
+    >✕</button>
+</div>
+
+<style lang="postcss">
+  .vaa-alert-hidden {
+    @apply translate-y-[100%] opacity-0;
+  }
+</style>
---
+++ frontend/src/lib/components/alert/Alert.type.ts
@@ -0,0 +1,21 @@
+import type {SvelteHTMLElements} from 'svelte/elements';
+import type {IconName} from '../icon';
+
+export type AlertProps = SvelteHTMLElements['dialog'] & {
+  /**
+   * The title of the alert
+   */
+  title: string;
+  /**
+   * Possible icon of the alert. @default undefined
+   */
+  icon?: IconName;
+  /**
+   * Whether to open the alert automatically. @default true
+   */
+  autoOpen?: boolean;
+  /**
+   * Bind to this to get the alert's open state.
+   */
+  readonly isOpen?: boolean;
+};
