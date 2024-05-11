172cef98fc4cacaafca30660cf21ca4f5021a4af
kallelongjuhani <kalle.jarvenpaa@gmail.com>
2024-05-11 19:54:10 +0300

    feat: UmamiAnalytics component
    Adds the `<UmamiAnalytics>` component which can be used to implement
    client-side analytics and extended event tracking.

---
+++ frontend/src/lib/components/analytics/umami/UmamiAnalytics.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+  import {browser} from '$app/environment';
+  import {sendTrackingEvent, type TrackingEvent} from '$lib/utils/analytics/track';
+  import {settings} from '$lib/utils/stores';
+
+  if ($settings.analytics?.trackEvents) {
+    $sendTrackingEvent = trackEvent;
+  }
+
+  function trackEvent({name, data}: TrackingEvent<Record<string, JSONData>>) {
+    if (!browser || !('umami' in window)) return;
+    (window.umami as {track: UmamiTrack}).track(name, data);
+  }
+
+  /**
+   * The basic tracking properties, such as url, referrer, etc. are automatically added.
+   */
+  type UmamiTrack = (event_name: string, event_data?: {[key: string]: JSONData}) => void;
+</script>
+
+<svelte:head>
+  <script
+    defer
+    src="https://analytics.eu.umami.is/script.js"
+    data-website-id={$settings.analytics?.platform?.code}
+    data-auto-track="true"></script>
+</svelte:head>
