dc971573ec8c3056d6f1df159c40697566b6f989
kallelongjuhani <kalle.jarvenpaa@gmail.com>
2024-05-12 12:55:06 +0300

    fixup! feat: UmamiAnalytics component

--- frontend/src/lib/components/analytics/umami/UmamiAnalytics.svelte
+++ frontend/src/lib/components/analytics/umami/UmamiAnalytics.svelte
@@ -1,13 +1,13 @@
 <script lang="ts">
   import {browser} from '$app/environment';
-  import {sendTrackingEvent, type TrimmedTrackingEvent} from '$lib/utils/analytics/track';
+  import {sendTrackingEvent, type TrackingEvent} from '$lib/utils/analytics/track';
   import {settings} from '$lib/utils/stores';
 
   if ($settings.analytics?.trackEvents) {
     $sendTrackingEvent = trackEvent;
   }
 
-  function trackEvent({name, data}: TrimmedTrackingEvent) {
+  function trackEvent({name, data}: TrackingEvent<Record<string, JSONData>>) {
     if (!browser || !('umami' in window)) return;
     (window.umami as {track: UmamiTrack}).track(name, data);
   }
@@ -15,7 +15,7 @@
   /**
    * The basic tracking properties, such as url, referrer, etc. are automatically added.
    */
-  type UmamiTrack = (event_name: string, event_data?: {[key: string]: string | number}) => void;
+  type UmamiTrack = (event_name: string, event_data?: {[key: string]: JSONData}) => void;
 </script>
 
 <svelte:head>
