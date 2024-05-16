1d49c2d9a6159f3d47be71fe920f676b5787f0ef
kallelongjuhani <kalle.jarvenpaa@gmail.com>
2024-05-16 15:11:12 +0300

    feat: SurveyBanner component
    Adds a `<SurveyBanner>` component for displaying a prompt for filling
    out a user survey if the setting is enabled and the user has not
    answered the survey yet. Otherwise, nothing will be rendered.

---
+++ frontend/src/lib/utils/analytics/survey.ts
@@ -0,0 +1,16 @@
+import {derived} from 'svelte/store';
+import {parse} from '$lib/i18n';
+import {settings} from '$lib/utils/stores';
+import {sessionId} from './track';
+
+/**
+ * A link to the user survey, including the session ID, or `undefined` if the survey is not configured.
+ */
+export const surveyLink = derived(
+  [settings, sessionId],
+  ([$settings, $sessionId]) => {
+    const linkTemplate = $settings.analytics.survey?.linkTemplate;
+    return linkTemplate ? parse(linkTemplate, {sessionId: $sessionId ?? ''}) : undefined;
+  },
+  ''
+);
