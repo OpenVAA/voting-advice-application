00e6468001a806ba49cf29fdc34d76deba3ded7c
kallelongjuhani <kalle.jarvenpaa@gmail.com>
2024-05-18 12:42:06 +0300

    fixup! feat: add start search param to set a question as first

--- frontend/src/app.d.ts
+++ frontend/src/app.d.ts
@@ -18,6 +18,7 @@ declare namespace App {
     entityType?: EntityType;
     parties?: Promise<PartyProps[]>;
     questionId?: string;
+    // `setQuestionAsFirst` will be set to `true` on the `/(voter)/questions/[questionId]` route if a `start` search param is present (regardless of its value)
     setQuestionAsFirst?: boolean;
     opinionQuestions?: Promise<QuestionProps[]>;
     // TODO: This is a temporary fix for the candidate app and will be corrected when this branc is merged into main
