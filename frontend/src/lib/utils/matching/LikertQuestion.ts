1899c53b2d824baa40daf4a8c45082bd8e074d9b
kallelongjuhani <kalle.jarvenpaa@gmail.com>
2024-05-19 11:34:25 +0300

    refactor: matching utilities
    Reorder `matching` utilities to separate files so that we can later use
    `imputePartyAnswers` outside of `matchParties` as well, specifically to
    show parties' answers when displaying questions.

---
+++ frontend/src/lib/utils/matching/LikertQuestion.ts
@@ -0,0 +1,20 @@
+import {MultipleChoiceQuestion} from '$voter/vaa-matching';
+
+/**
+ * A dummy question object for matching.
+ */
+export class LikertQuestion extends MultipleChoiceQuestion {
+  public readonly category: QuestionCategoryProps | undefined;
+  constructor({id, values, category}: LikertQuestionOptions) {
+    super(id, values);
+    this.category = category;
+  }
+}
+/**
+ * Options for a dummy question object for matching.
+ */
+interface LikertQuestionOptions {
+  id: ConstructorParameters<typeof MultipleChoiceQuestion>[0];
+  values: ConstructorParameters<typeof MultipleChoiceQuestion>[1];
+  category?: QuestionCategoryProps;
+}
