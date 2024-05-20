97e798f245b7efa723d3914976d5c5c67224a834
kallelongjuhani <kalle.jarvenpaa@gmail.com>
2024-05-20 14:23:31 +0300

    feat: SurveyPopup component
    Add a `<SurveyPopup>` component that can be used to show a popup asking
    the user to fill a user survey.
    
    Also, extract a `<SurveyButton>` component from the `<SurveyBanner>`
    component for less redundancy.

--- frontend/src/lib/components/survey/banner/SurveyBanner.svelte
+++ frontend/src/lib/components/survey/banner/SurveyBanner.svelte
@@ -1,23 +1,15 @@
 <script lang="ts">
   import {t} from '$lib/i18n';
-  import {surveyLink} from '$lib/utils/analytics/survey';
-  import {startEvent} from '$lib/utils/analytics/track';
   import {concatClass} from '$lib/utils/components';
-  import {setSurveyStatus, settings, userPreferences} from '$lib/stores';
-  import {Button} from '$lib/components/button';
+  import {settings, userPreferences} from '$lib/stores';
+  import {SurveyButton} from '..';
   import type {SurveyBannerProps} from './SurveyBanner.type';
 
   type $$Props = SurveyBannerProps;
 
   export let variant: $$Props['variant'] = 'default';
 
-  let showThanks = false;
-
-  function onClick() {
-    showThanks = true;
-    setSurveyStatus('received');
-    startEvent('survey_opened');
-  }
+  let clicked: boolean;
 </script>
 
 <!--
@@ -37,24 +29,14 @@ Display a prompt for filling out a user survey if the setting is enabled and the
 ```
 -->
 
-{#if showThanks || ($settings.analytics?.survey && $userPreferences.survey?.status !== 'received')}
+{#if clicked || ($settings.analytics?.survey && $userPreferences.survey?.status !== 'received')}
   <div
     {...concatClass(
       $$restProps,
       'grid justify-items-center w-full ' +
         (variant === 'compact' ? '' : 'rounded-lg bg-base-200 p-lg pt-md')
     )}>
-    <Button
-      href={$surveyLink}
-      target="_blank"
-      on:click={onClick}
-      variant="normal"
-      color="accent"
-      class="justify-center"
-      disabled={showThanks}
-      text={showThanks ? $t('survey.thanks') : $t('survey.button')}
-      icon="research"
-      iconPos="left" />
+    <SurveyButton bind:clicked />
     {#if variant !== 'compact'}
       <div class="text-center text-sm text-secondary">{$t('survey.info')}</div>
     {/if}
---
+++ frontend/src/lib/components/survey/popup/SurveyPopup.svelte
@@ -0,0 +1,60 @@
+<script lang="ts">
+  import {onDestroy} from 'svelte';
+  import {t} from '$lib/i18n';
+  import {Alert} from '$lib/components/alert';
+  import {Button} from '$lib/components/button';
+  import {SurveyButton} from '..';
+  import type {SurveyPopupProps} from './SurveyPopup.type';
+
+  /* eslint-disable @typescript-eslint/no-unused-vars */
+  type $$Props = SurveyPopupProps;
+
+  /**
+   * The delay for autoclosing the modal after it's been submitted.
+   */
+  const CLOSE_DELAY = 1500;
+
+  let closeTimeout: NodeJS.Timeout | undefined;
+
+  onDestroy(() => {
+    if (closeTimeout) clearTimeout(closeTimeout);
+  });
+
+  let closeAlert: () => void;
+
+  function onClick() {
+    closeTimeout = setTimeout(() => {
+      closeAlert();
+    }, CLOSE_DELAY);
+  }
+</script>
+
+<!--
+@component
+Show a popup asking for user feedback.
+
+### Properties
+
+- Any valid properties of an `<Alert>` component.
+
+### Usage
+
+```tsx
+<FeedbackPopup/>
+```
+-->
+
+<Alert bind:closeAlert title={$t('survey.title')} {...$$restProps}>
+  <div class="justify-self-stretch">
+    <h3 class="mb-sm mt-0 text-center">
+      {$t('survey.popupTitle')}
+    </h3>
+  </div>
+  <div slot="actions">
+    <SurveyButton on:click={onClick} variant="main" />
+    <p class="my-sm text-sm">
+      {$t('survey.popupInfo')}
+    </p>
+    <Button on:click={closeAlert} text={$t('common.thanksNo')} color="warning" />
+  </div>
+</Alert>
