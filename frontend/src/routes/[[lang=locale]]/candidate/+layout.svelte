651e1bf8c70f2c257f11764a091c71f701b5ce89
kallelongjuhani <kalle.jarvenpaa@gmail.com>
2024-05-24 11:31:39 +0300

    feat: show error if Candidate App is not available
    Shows an error page if the `dataProvider.supportsCandidateApp` setting
    is not `true`.

--- frontend/src/routes/[[lang=locale]]/candidate/+layout.svelte
+++ frontend/src/routes/[[lang=locale]]/candidate/+layout.svelte
@@ -1,6 +1,17 @@
 <script lang="ts">
+  import {t} from '$lib/i18n';
+  import localSettings from '$lib/config/settings.json';
   import {appType} from '$lib/stores';
+  import {ErrorPage} from '$lib/templates/error';
+
   $appType = 'candidate';
 </script>
 
-<slot />
+{#if localSettings.dataProvider.supportsCandidateApp}
+  <slot />
+{:else}
+  <ErrorPage
+    title={$t('candidateApp.notSupported.title')}
+    content={$t('candidateApp.notSupported.content')}
+    emoji={$t('candidateApp.notSupported.emoji')} />
+{/if}
