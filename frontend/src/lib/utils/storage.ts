bcdf91989df55c55a3670164b45ead90deeadedd
kallelongjuhani <kalle.jarvenpaa@gmail.com>
2024-06-04 09:46:10 +0300

    chore: relax storage utilities' typing
    The enforcement of `JSONData` on the contents prevents subsequent
    narrower typing of store contents, so it is relaxed to `any`.

--- frontend/src/lib/utils/storage.ts
+++ frontend/src/lib/utils/storage.ts
@@ -5,7 +5,7 @@ import {browser} from '$app/environment';
 import {settings} from '../stores/stores';
 import {logDebugError} from './logger';
 
-type StorageType = 'localStorage' | 'sessionStorage';
+export type StorageType = 'localStorage' | 'sessionStorage';
 
 /**
  * Data saved in `localStorage` is accompanied by a version number, which can be checked agains to expire the data.
@@ -17,33 +17,32 @@ type LocallyStoredValue<T> = {
 
 /**
  * Create a store that is persisted in `localStorage` and initiate it unless a saved value is found. The version number saved with the item is checked and the data is expired if it does match the required version defined in `settings`.
+ * NB. The type of `defaultValue` should be one that can be serialized to JSON. We cannot define typing for that in a satisfactory way due to TS limitations. See: https://github.com/microsoft/TypeScript/issues/1897#issuecomment-1415776159
  * @param key The key to store the value under.
  * @param defaultValue The default value for the store.
  */
-export function localStorageWritable<T = JSONData>(key: string, defaultValue: T) {
+export function localStorageWritable<T>(key: string, defaultValue: T) {
   return storageWritable('localStorage', key, defaultValue);
 }
 
 /**
  * Create a store that is persisted in `sessionStorage` and initiate it unless a saved value is found.
+ * NB. The type of `defaultValue` should be one that can be serialized to JSON. We cannot define typing for that in a satisfactory way due to TS limitations. See: https://github.com/microsoft/TypeScript/issues/1897#issuecomment-1415776159
  * @param key The key to store the value under.
  * @param defaultValue The default value for the store.
  */
-export function sessionStorageWritable<T = JSONData>(key: string, defaultValue: T) {
+export function sessionStorageWritable<T>(key: string, defaultValue: T) {
   return storageWritable('sessionStorage', key, defaultValue);
 }
 
 /**
  * Create a store that is persisted in `localStorage` or `sessionStorage` and initiate it unless a saved value is found.
+ * NB. The type of `defaultValue` should be one that can be serialized to JSON. We cannot define typing for that in a satisfactory way due to TS limitations. See: https://github.com/microsoft/TypeScript/issues/1897#issuecomment-1415776159
  * @param type The type of storage to use.
  * @param key The key to store the value under.
  * @param defaultValue The default value for the store.
  */
-function storageWritable<T = JSONData>(
-  type: StorageType,
-  key: string,
-  defaultValue: T
-): Writable<T> {
+export function storageWritable<T>(type: StorageType, key: string, defaultValue: T): Writable<T> {
   const value = getItemFromStorage<T>(type, key);
   // TODO: Check that the value matches the defaultValue's type
   const store = writable(value == null ? defaultValue : value) as Writable<T>;
@@ -54,7 +53,7 @@ function storageWritable<T = JSONData>(
 /**
  * Get an item from storage. If the `type` is `localStorage`, the version number saved with the item is checked and the data is expired if it does match the required version defined in `settings`.
  */
-function getItemFromStorage<T = JSONData>(type: StorageType, key: string): T | null {
+function getItemFromStorage<T>(type: StorageType, key: string): T | null {
   let item: T | null = null;
   const storage = getStorage(type);
   if (!storage) return null;
@@ -91,7 +90,7 @@ function getItemFromStorage<T = JSONData>(type: StorageType, key: string): T | n
 /**
  * Save an item to storage. If the `type` is `localStorage`, the data is versioned.
  */
-function saveItemToStorage<T = JSONData>(type: StorageType, key: string, value: T): void {
+function saveItemToStorage<T>(type: StorageType, key: string, value: T): void {
   const storage = getStorage(type);
   if (!storage) return;
   const toStore =
