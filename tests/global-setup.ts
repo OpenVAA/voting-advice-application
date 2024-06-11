3f93fd9d519303b2f24357dbf060ba71971a37b5
Antti Pekkanen <antti.o.pekkanen@aalto.fi>
2024-06-11 15:57:13 +0300

    refactor: Playwright tests
    Refactors navbar.spec.ts and settings.spec.ts to use Route enum
    and translation files instead of hardcoded strings.

--- tests/global-setup.ts
+++ tests/global-setup.ts
@@ -2,6 +2,7 @@ import { expect, chromium, FullConfig } from '@playwright/test';
 // Import Route directly so that other imports are not bundled in
 import { Route } from '../frontend/src/lib/utils/navigation/route';
 import candidateAppTranslations from '../frontend/src/lib/i18n/translations/en/candidateApp.json';
+import mockUser from '../backend/vaa-strapi/src/functions/mockData/mockUser.json';
 
 import { STORAGE_STATE } from '../playwright.config';
 
@@ -11,8 +12,8 @@ async function globalSetup(config: FullConfig) {
   const page = await browser.newPage();
 
   await page.goto(`${baseURL!}/${Route.CandAppHome}`);
-  await page.getByLabel(candidateAppTranslations.common.email, {exact: true}).fill('first.last@example.com');
-  await page.getByLabel(candidateAppTranslations.common.password, {exact: true}).fill('password');
+  await page.getByLabel(candidateAppTranslations.common.email, {exact: true}).fill(mockUser.email);
+  await page.getByLabel(candidateAppTranslations.common.password, {exact: true}).fill(mockUser.password);
   await page.getByText(candidateAppTranslations.common.logIn, {exact: true}).click();
 
   // Wait until the page actually signs in.
--- tests/settings.spec.ts
+++ tests/settings.spec.ts
@@ -1,53 +1,66 @@
 import { test, expect } from '@playwright/test';
+// Import Route directly so that other imports are not bundled in
+import { Route } from '../frontend/src/lib/utils/navigation/route';
+import candidateAppTranslationsEn from '../frontend/src/lib/i18n/translations/en/candidateApp.json';
+import candidateAppTranslationsFi from '../frontend/src/lib/i18n/translations/fi/candidateApp.json';
+import headerTranslations from '../frontend/src/lib/i18n/translations/en/header.json';
+import mockUser from '../backend/vaa-strapi/src/functions/mockData/mockUser.json';
+
+const LOCALE_EN = 'en';
+const LOCALE_FI = 'fi';
 
 test.describe.serial('settings page should work expectedly', () => {
-  test('should change app language', async ({ page }) => {
+  test('should change app language', async ({ page, baseURL }) => {
     
     //Go to setting and change language to en
-    await page.goto('http://localhost:5173/fi/candidate/settings');
-    await page.getByLabel('Sovelluksen kieli').selectOption('en');
-    await expect(page).toHaveURL(/(http[s]?:\/\/)?(.*)\/en\/candidate\/settings/);
+    await page.goto(`${baseURL}/${LOCALE_FI}/${Route.CandAppSettings}`);
+    await page.getByLabel(candidateAppTranslationsFi.settings.fields.language, {exact: true}).selectOption(LOCALE_EN);
+    const settingsUrlRe = new RegExp(`(http[s]?:\/\/)?(.*)\/${LOCALE_EN}\/${Route.CandAppSettings}`);
+    await expect(page).toHaveURL(settingsUrlRe);
 
     //Log out
-    await page.getByLabel('Logout').click();
+    await page.getByLabel(candidateAppTranslationsEn.common.logOut, {exact: true}).click();
 
     //Change page language to fi
-    await page.getByLabel('Open menu').click();
-    await page.getByRole('link', { name: 'Suomi' }).click();
-    await expect(page).toHaveURL(/(http[s]?:\/\/)?(.*)\/fi\/candidate/);
-    await page.locator('.drawer-overlay').click();
+    await page.getByLabel(headerTranslations.openMenu, {exact: true}).click();
+    await page.getByRole('link', { name: candidateAppTranslationsFi.languages.Finnish, exact: true }).click();
+
+    const candidateUrlReFi = new RegExp(`(http[s]?:\/\/)?(.*)\/${LOCALE_FI}\/${Route.CandAppHome}`)
+    await expect(page).toHaveURL(candidateUrlReFi);
 
     //Sign in and expect language to automatically change to en
-    await page.getByPlaceholder('example@email.com').fill('first.last@example.com');
-    await page.getByPlaceholder('Esim. CP23-174a-f4%&-aHAB').fill('password');
-    await page.getByRole('button', { name: 'FI - Sign in' }).click();
-    await expect(page).toHaveURL(/(http[s]?:\/\/)?(.*)\/en\/candidate/);
+    await page.getByPlaceholder(candidateAppTranslationsFi.common.emailPlaceholder, {exact: true}).fill(mockUser.email);
+    await page.getByPlaceholder(candidateAppTranslationsFi.common.passwordPlaceholder, {exact: true}).fill(mockUser.password);
+    await page.getByRole('button', { name: candidateAppTranslationsFi.common.logIn, exact: true }).click();
+    const candidateUrlReEn = new RegExp(`(http[s]?:\/\/)?(.*)\/${LOCALE_EN}\/${Route.CandAppHome}`)
+    await expect(page).toHaveURL(candidateUrlReEn);
 
   });
 
-  test('should update password', async ({ page }) => {
+  test('should update password', async ({ page, baseURL }) => {
     
-    //Go to settings and change password 
-    await page.goto('http://localhost:5173/en/candidate/settings');    
-    await page.getByLabel('Current Password').fill('password');
-    await page.getByLabel('New Password', { exact: true }).fill('Password1!');
-    await page.getByLabel('New Password Confirmation').fill('Password1!');
-    await page.getByRole('button', { name: 'Update Password' }).click();
-    await expect(page.getByText('The password has been updated!')).toBeVisible();
+    //Go to settings and change password     
+    await page.goto(`${baseURL}/${LOCALE_EN}/${Route.CandAppSettings}`);    
+    await page.getByLabel(candidateAppTranslationsEn.settings.currentPassword, {exact: true}).fill(mockUser.password);
+    const newPassword = 'Password1!';
+    await page.getByLabel(candidateAppTranslationsEn.settings.newPassword, { exact: true }).fill(newPassword);
+    await page.getByLabel(candidateAppTranslationsEn.settings.newPasswordConfirmation, {exact: true}).fill(newPassword);
+    await page.getByRole('button', { name: candidateAppTranslationsEn.settings.updatePassword, exact: true }).click();
+    await expect(page.getByText(candidateAppTranslationsEn.settings.passwordUpdated, {exact: true})).toBeVisible();
 
     //Logout
-    await page.getByLabel('Logout').click();
+    await page.getByLabel(candidateAppTranslationsEn.common.logOut, {exact: true}).click();
 
     //Should not login with old password
-    await page.getByPlaceholder('example@email.com').fill('first.last@example.com');
-    await page.getByPlaceholder('E.g. CP23-174a-f4%&-aHAB').fill('password');
-    await page.getByRole('button', { name: 'Sign in' }).click();
-    await expect(page.getByText('Wrong email or password')).toBeVisible();
+    await page.getByPlaceholder(candidateAppTranslationsEn.common.emailPlaceholder, {exact: true}).fill(mockUser.email);
+    await page.getByPlaceholder(candidateAppTranslationsEn.common.passwordPlaceholder, {exact: true}).fill(mockUser.password);
+    await page.getByRole('button', { name: candidateAppTranslationsEn.common.logIn, exact: true }).click();
+    await expect(page.getByText(candidateAppTranslationsEn.common.wrongEmailOrPassword, {exact: true})).toBeVisible();
 
     //Should login with new password
-    await page.getByPlaceholder('E.g. CP23-174a-f4%&-aHAB').fill('Password1!');
-    await page.getByRole('button', { name: 'Sign in' }).click();
-    await expect(page.getByRole('heading', { name: 'You\'re Ready to Roll!' })).toBeVisible();
-
+    await page.getByPlaceholder(candidateAppTranslationsEn.common.passwordPlaceholder, {exact: true}).fill(newPassword);
+    await page.getByRole('button', { name: candidateAppTranslationsEn.common.logIn, exact: true }).click();
+    
+    await expect(page.getByRole('heading', { name: candidateAppTranslationsEn.homePage.ready, exact: true })).toBeVisible();
   });
 });
