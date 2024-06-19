a9b3de42f78f2dc515578759e289b28fc0660ec2
Antti Pekkanen <antti.o.pekkanen@aalto.fi>
2024-06-19 16:04:08 +0300

    deps: bump frontend deps
    vitest from  0.30.1 to 1.6.0
    @vitest/coverage-c8 removed
    @vitest/coverage-v8 added
    intl-messageformat from 10.5.11 to 10.5.14
    isomorphic-dompurify from 2.6.0 to 2.12.0
    @sveltejs/adapter-auto from 3.1.1 to 3.2.2
    @sveltejs/adapter-node from 5.0.1 to 5.2.0
    @sveltejs/kit from 2.5.3 to 2.5.17
    @sveltejs/vite-plugin-svelte from 3.0.2 to 3.1.1
    @testing-library/jest-dom from 6.4.2 to 6.4.6
    @types/node from 20.11.25 to 20.14.5
    @typescript-eslint/eslint-plugin from 7.1.1 to 7.13.1
    @typescript-eslint/parser from 7.1.1 to 7.13.1
    autoprefixer from 10.4.18 to 10.4.19
    daisyui from 4.7.2 to 4.12.2
    eslint-plugin-svelte from 2.35.1 to 2.40.0
    jsdom from 24.0.0 to 24.1.0
    postcss from 8.4.31 to 8.4.38
    prettier from 3.2.5 to 3.3.2
    prettier-plugin-svelte from 3.2.2 to 3.2.4
    prettier-plugin-tailwindcss from 0.5.12 to 0.6.5
    svelte from 4.2.12 to 4.2.18
    svelte-check from 3.6.6 to 3.8.1
    svelte-preprocess from 5.1.3 to 6.0.1
    tailwindcss from 3.3.5 to 3.4.4
    tslib from 2.6.2 to 2.6.3
    typescript from 5.4.2 to 5.4.5
    vite from 5.1.5 to 5.3.1
    vite-tsconfig-paths from 4.3.1 to 4.3.2

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -1,5 +1,5 @@
 import adapter from '@sveltejs/adapter-node';
-import preprocess from 'svelte-preprocess';
+import {sveltePreprocess} from 'svelte-preprocess';
 import path from 'path';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -7,7 +7,7 @@ const config = {
   // Consult https://github.com/sveltejs/svelte-preprocess
   // for more information about preprocessors
   preprocess: [
-    preprocess({
+    sveltePreprocess({
       postcss: true
     })
   ],
