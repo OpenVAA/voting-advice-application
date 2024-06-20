7ab360ea4c6682e91eca9ec334d9df6bba17e077
kallelongjuhani <kalle.jarvenpaa@gmail.com>
2024-06-20 14:17:48 +0300

    feat: /api/data/[collection] API route
    Adds a generic API route for `GET` requests to load election data using
    a using `ServerDataProvider`, which must run on the server.
    
    This allows access to the data from the client even when the server is
    needed, e.g., when reading data stored in local json files.

---
+++ frontend/src/routes/[[lang=locale]]/api/data/[collection]/+server.ts
@@ -0,0 +1,34 @@
+/**
+ * An generic API route for getting data from server-dependent DataProviders.
+ */
+
+import {error} from '@sveltejs/kit';
+import {DATA_COLLECTIONS} from '$lib/_api/dataCollections';
+import type {GetDataOptionsBase} from '$lib/_api/dataProvider.type';
+import {serverDataProvider} from '$lib/server/_api/serverDataProvider';
+import type {RequestHandler} from './$types';
+
+export const GET: RequestHandler = async ({fetch, params, url}) => {
+  console.info(`[debug] /api/data/${params.collection}/+server.ts: GET`);
+
+  const provider = await serverDataProvider;
+  if (!provider) error(500, 'No server data provider available');
+
+  provider.init({fetch});
+
+  const {collection} = params;
+  if (!collection || !(collection in DATA_COLLECTIONS)) error(500, 'No valid collection provided');
+
+  const getDataMethod = DATA_COLLECTIONS[collection];
+  const options = Object.fromEntries(url.searchParams.entries());
+  const data = await provider[getDataMethod](options as GetDataOptionsBase);
+
+  if (data instanceof Error) {
+    console.error(`[debug] /api/data/${params.collection}/+server.ts: GET Error: ${data}`);
+    error(500, data.message);
+  }
+
+  return new Response(data, {
+    headers: {'Content-Type': 'application/json'}
+  });
+};
