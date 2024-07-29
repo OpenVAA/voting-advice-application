446cb9be3cb733baa539c0e3582253d6db2feabf
Antti Pekkanen <antti.o.pekkanen@aalto.fi>
2024-07-29 14:38:34 +0300

    refactor: separate settings into dynamic and static settings

---
+++ shared/settings/dynamicSettings.ts
@@ -0,0 +1,6 @@
+import type {DynamicSettings} from './settings.type';
+
+/**
+ * Default dynamic settings can be overridden in this file.
+ */
+export const dynamicSettings: Partial<DynamicSettings> = {};
---
+++ shared/settings/utils.ts
@@ -0,0 +1,22 @@
+import {
+  defaultDynamicSettings,
+  defaultStaticSettings,
+  dynamicSettings,
+  staticSettings,
+  type DynamicSettings
+} from './';
+
+export const mergedStaticSettings = {...defaultStaticSettings, ...staticSettings};
+
+export function mergedDynamicSettings(
+  dataProviderDynamicSettings?: Partial<DynamicSettings>
+): DynamicSettings {
+  if (dataProviderDynamicSettings) {
+    // Null values are filtered to avoid overriding values of defaultDynamiSettings or dynamicSettings with null.
+    const filtered = Object.fromEntries(
+      Object.entries(dataProviderDynamicSettings).filter(([key, value]) => value)
+    );
+    return {...defaultDynamicSettings, ...dynamicSettings, ...filtered};
+  }
+  return {...defaultDynamicSettings, ...dynamicSettings};
+}
