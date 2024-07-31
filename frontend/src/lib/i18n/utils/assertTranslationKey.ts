80bd286e97b70c6d68ff51ac40c4b1b426857448
kallelongjuhani <kalle.jarvenpaa@gmail.com>
2024-07-31 15:42:55 +0300

    feat: assertTranslationKey utility
    Add `assertTranslationKey` utility for blindly asserting a dynamically
    generated key is a valid `TranslationKey`.
    
    TODO: In the future, check that the key is a valid translation key.

---
+++ frontend/src/lib/i18n/utils/assertTranslationKey.ts
@@ -0,0 +1,9 @@
+import type {TranslationKey} from '$types';
+
+/**
+ * Asserts blindly that key is `TranslationKey` for use with translation keys that are formed dynamically.
+ * TODO: In the future, check that the key is a valid translation key.
+ */
+export function assertTranslationKey(key: string): TranslationKey {
+  return key as TranslationKey;
+}
