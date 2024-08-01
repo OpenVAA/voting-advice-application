461297d0745b81d6bd01e98d6546e7cd9f4c1418
puhakkn2 <noora.puhakka@aalto.fi>
2024-08-01 09:34:49 +0300

    feat: remove unused content types from backend
    Remove content types that are deprecated by the switch to the
    `Question`-`Answer` model for candidates’ basic info.

--- backend/vaa-strapi/src/api/candidate-attribute/controllers/candidate-attribute.ts
+++
@@ -1,7 +0,0 @@
-/**
- * candidate-attribute controller
- */
-
-import { factories } from '@strapi/strapi'
-
-export default factories.createCoreController('api::candidate-attribute.candidate-attribute');
