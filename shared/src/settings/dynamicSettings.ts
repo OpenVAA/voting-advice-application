1caac21c5354151135dd0684feca82105f0d707a
Antti Pekkanen <antti.o.pekkanen@aalto.fi>
2024-08-12 15:44:27 +0300

    refactor: separate settings into dynamic and static settings

---
+++ shared/src/settings/dynamicSettings.ts
@@ -0,0 +1,48 @@
+import type {DynamicSettings} from '../';
+
+export const dynamicSettings: DynamicSettings = {
+  entityDetails: {
+    contents: {
+      candidate: ['info', 'opinions'],
+      party: ['info', 'candidates', 'opinions']
+    },
+    showMissingElectionSymbol: {
+      candidate: true,
+      party: false
+    },
+    showMissingAnswers: {
+      candidate: true,
+      party: true
+    }
+  },
+  header: {
+    showFeedback: true,
+    showHelp: true
+  },
+  matching: {
+    minimumAnswers: 5,
+    partyMatching: 'median'
+  },
+  questions: {
+    categoryIntros: {
+      allowSkip: true,
+      show: true
+    },
+    questionsIntro: {
+      allowCategorySelection: true,
+      show: true
+    },
+    showCategoryTags: true,
+    showResultsLink: true
+  },
+  results: {
+    cardContents: {
+      candidate: ['submatches'],
+      party: ['candidates']
+    },
+    showFeedbackPopup: 180,
+    showSurveyPopup: 500,
+    sections: ['candidate', 'party']
+  },
+  underMaintenance: false
+};
