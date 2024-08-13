d341c0ff95cf8168f4868145a68a2bab6e6c92e4
Antti Pekkanen <antti.o.pekkanen@aalto.fi>
2024-08-13 15:09:04 +0300

    refactor: remove json fields from app settings in Strapi

--- backend/vaa-strapi/src/api/app-setting/controllers/app-setting.ts
+++ backend/vaa-strapi/src/api/app-setting/controllers/app-setting.ts
@@ -2,6 +2,30 @@
  * app-setting controller
  */
 
-import { factories } from '@strapi/strapi'
+import {factories} from '@strapi/strapi';
+import {API} from '../../../functions/utils/api';
+import {getCardContentsFromStrapi} from '../../../functions/utils/appSettings';
 
-export default factories.createCoreController('api::app-setting.app-setting');
+export default factories.createCoreController(API.AppSettings, () => ({
+  async findOne(ctx) {
+    const result = await super.find(ctx);
+
+    for (const [i, val] of result.data.entries()) {
+      const cardContents = await getCardContentsFromStrapi(val.id);
+      result.data[i].attributes.results.cardContents = cardContents;
+    }
+
+    return result;
+  },
+
+  async find(ctx) {
+    const {data, meta} = await super.find(ctx);
+
+    for (const [i, val] of data.entries()) {
+      const cardContents = await getCardContentsFromStrapi(val.id);
+      data[i].attributes.results.cardContents = cardContents;
+    }
+
+    return {data, meta};
+  }
+}));
