eab90ba4b8bd61fba24e54c7f324f2271a9d40e3
kallelongjuhani <kalle.jarvenpaa@gmail.com>
2024-08-24 12:44:14 +0300

    refactor: move globally needed id and matching types to vaa-shared
    Move those types and some utilities to `vaa-shared` that need to match
    between all of the modules. These include:
    
    - The `Id` type used for objects with unique ids
    - Types related to data passed to the matching algorithm in
      `vaa-matching`, such as distances, coordinates, missing values and
      requirements for questions used for matching
    - Some utilities related to these types

---
+++ shared/src/matching/serializable.type.ts
@@ -0,0 +1,9 @@
+/**
+ * Any value allowed in JSON. Used to restrict dynamically fetched data.
+ */
+export type Serializable = string |
+  number |
+  boolean |
+  null |
+  Array<Serializable> |
+{ [key: string]: Serializable; };
