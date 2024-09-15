6fd25494ed52c8a945f3a79c5b7cd817021f51df
kallelongjuhani <kalle.jarvenpaa@gmail.com>
2024-09-15 12:27:55 +0300

    fix: remove deprecated `manifesto` property
    The `manifesto` property is replaced by the `Question`–`Answer` model
    for info questions.

--- backend/vaa-strapi/src/api/candidate/content-types/candidate/lifecycles.ts
+++ backend/vaa-strapi/src/api/candidate/content-types/candidate/lifecycles.ts
@@ -3,12 +3,9 @@ import crypto from 'crypto';
 export default {
   beforeCreate(event) {
     event.params.data.registrationKey = event.params.data.registrationKey ?? crypto.randomUUID();
-    event.params.data.manifesto = event.params.data.manifesto ?? {};
     event.params.data.email = event.params.data.email?.toLowerCase();
   },
   beforeUpdate(event) {
     event.params.data.email = event.params.data.email?.toLowerCase();
-    event.params.data.manifesto =
-      event.params.data.manifesto === null ? {} : event.params.data.manifesto;
   }
 };
