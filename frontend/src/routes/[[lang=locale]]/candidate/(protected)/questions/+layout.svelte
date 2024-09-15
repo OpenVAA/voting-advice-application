1a2715469c02a9ef3626f43061ad327176e9b4d9
kallelongjuhani <kalle.jarvenpaa@gmail.com>
2024-09-15 14:31:18 +0300

    feat: nice error message when there are no questions
    Show a nice error message when there are no questions both in the
    Candidate and Voter Apps with useful actions to take.

--- frontend/src/routes/[[lang=locale]]/candidate/(protected)/questions/+layout.svelte
+++ frontend/src/routes/[[lang=locale]]/candidate/(protected)/questions/+layout.svelte
@@ -5,6 +5,9 @@
   import {t} from '$lib/i18n';
   import {getRoute, Route} from '$lib/utils/navigation';
   import {LoadingSpinner} from '$candidate/components/loadingSpinner';
+  import {Button} from '$lib/components/button';
+  import {BasicPage} from '$lib/templates/basicPage';
+  import {HeroEmoji} from '$lib/components/heroEmoji';
 
   const {opinionQuestions, unansweredRequiredInfoQuestions} =
     getContext<CandidateContext>('candidate');
@@ -21,7 +24,16 @@
 {#if $unansweredRequiredInfoQuestions?.length}
   <LoadingSpinner />
 {:else if !$opinionQuestions || !Object.values($opinionQuestions).length}
-  <p>{$t('error.noQuestions')}</p>
+  <BasicPage title={$t('error.noQuestions')}>
+    <HeroEmoji emoji={$t('dynamic.error.heroEmoji')} slot="hero" />
+    <Button
+      href={$getRoute(Route.CandAppHome)}
+      text={$t('common.return')}
+      variant="main"
+      icon="previous"
+      iconPos="left"
+      slot="primaryActions" />
+  </BasicPage>
 {:else}
   <slot />
 {/if}
