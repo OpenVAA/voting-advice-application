55d53f67e58da328510b3d80a97b427aba9c80cb
kallelongjuhani <kalle.jarvenpaa@gmail.com>
2024-09-20 09:23:13 +0200

    feat: setting for hiding candidates with missing answers
    Add a dynamic setting for hiding candidates with missing answers to
    opinion questions. The filtering takes place in the `candidates` store
    so the data for all the candidates is still retrieved from the backend.

---
+++ frontend/src/lib/utils/hasAllAnswers.ts
@@ -0,0 +1,16 @@
+import {isCandidate, isParty} from './entities';
+
+/**
+ * Check whether the entity has answers to all of the given questions.
+ * @param entity A Candidate or a Party
+ * @param questions A list of questions
+ */
+export function hasAllAnswers(entity: EntityProps, questions: Array<QuestionProps>): boolean {
+  let type: EntityType;
+  if (isCandidate(entity)) type = 'candidate';
+  else if (isParty(entity)) type = 'party';
+  else throw new Error('Invalid entity type');
+  return questions.every(
+    (q) => (q.entityType !== 'all' && q.entityType !== type) || entity.answers[q.id] != null
+  );
+}
