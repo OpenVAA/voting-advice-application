9585bf6b686f74b9ba11c51e707ed89b4a1e398c
Egor Iakovlev <egor.iakovlev@rizolvit.com>
2024-10-01 18:15:37 +0300

    feat: add AWS SES support for nodemailer (#613)

--- backend/vaa-strapi/config/plugins.ts
+++ backend/vaa-strapi/config/plugins.ts
@@ -1,41 +1,59 @@
-export default ({env}) => ({
-  'users-permissions': {
-    config: {
-      jwt: {
-        expiresIn: '4h'
-      },
-      register: {
-        allowedFields: ['candidate']
-      }
-    }
-  },
-  email: {
-    config: {
-      provider: 'nodemailer',
-      providerOptions: {
-        host: env('SMTP_HOST', 'smtp.example.com'),
-        port: env('SMTP_PORT', 587),
-        auth: {
-          user: env('SMTP_USERNAME'),
-          pass: env('SMTP_PASSWORD')
+const aws = require('@aws-sdk/client-ses');
+
+export default ({env}) => {
+  /**
+   * Strapi initilises nodemailer transporter only once using main plugin config.
+   * It allows to override only transporter specific (SMPT or SES) settings using [env]/plugins.ts, not the transporter type itself.
+   * So we have to make sure that for development we initilise the transporter as SMTP to be able to use maildev.
+   */
+  const emailProviderOptions =
+    env('NODE_ENV') === 'development'
+      ? undefined
+      : {
+          SES: {
+            ses: new aws.SES({
+              apiVersion: '2010-12-01',
+              region: env('AWS_REGION'),
+              credentials: {
+                accessKeyId: env('AWS_ACCESS_KEY_ID'),
+                secretAccessKey: env('AWS_SECRET_ACCESS_KEY')
+              }
+            }),
+            aws
+          },
+          // max 14 messages per second to comply with AWS SES
+          sendingRate: 14
+        };
+  return {
+    'users-permissions': {
+      config: {
+        jwt: {
+          expiresIn: '4h'
+        },
+        register: {
+          allowedFields: ['candidate']
         }
-      },
-      settings: {
-        defaultFrom: env('MAIL_FROM', 'candidate-app@example.com'),
-        defaultReplyTo: env('MAIL_REPLY_TO', 'candidate-app@example.com')
-      },
-      ratelimit: env.NODE_ENV === 'development' && {
-        max: 10000,
-        interval: 60000
       }
+    },
+    email: {
+      config: {
+        provider: 'nodemailer',
+        providerOptions: {
+          ...emailProviderOptions
+        },
+        settings: {
+          defaultFrom: env('MAIL_FROM'),
+          defaultReplyTo: env('MAIL_REPLY_TO')
+        }
+      }
+    },
+    'import-export-entries': {
+      enabled: true,
+      resolve: './strapi-plugin-import-export-entries'
+    },
+    'candidate-admin': {
+      enabled: true,
+      resolve: './src/plugins/candidate-admin'
     }
-  },
-  'import-export-entries': {
-    enabled: true,
-    resolve: './strapi-plugin-import-export-entries'
-  },
-  'candidate-admin': {
-    enabled: true,
-    resolve: './src/plugins/candidate-admin'
-  }
-});
+  };
+};
