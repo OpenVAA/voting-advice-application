eedd23d7e43d9a039619e011d016b89fdc8bc95c
kallelongjuhani <kalle.jarvenpaa@gmail.com>
2024-11-07 21:37:56 +0200

    feat: `shared-config` module
    Creates an `@openvaa/shared-config` module that contains common
    settings for `prettier`, `eslint` and `tsconfig`. These can be imported
    into other modules, although in practice only the `tsconfig`s are used,
    because linting is now performed on the root level.

---
+++ packages/shared-config/eslint.config.mjs
@@ -0,0 +1,136 @@
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { FlatCompat } from '@eslint/eslintrc';
+import js from '@eslint/js';
+import typescriptEslint from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+import simpleImportSort from 'eslint-plugin-simple-import-sort';
+import globals from 'globals';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all
+});
+
+export default [
+  {
+    ignores: [
+      '**/*.cjs',
+      '**/.DS_Store',
+      '**/node_modules',
+      'build',
+      'package',
+      '**/.env',
+      '**/.env.*',
+      '!**/.env.example',
+      '**/pnpm-lock.yaml',
+      '**/package-lock.json',
+      '**/yarn.lock',
+      '**/$types.d.ts',
+    ]
+  },
+  ...compat.extends(
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ),
+  {
+    plugins: {
+      '@typescript-eslint': typescriptEslint,
+      'simple-import-sort': simpleImportSort //https://github.com/lydell/eslint-plugin-simple-import-sort?tab=readme-ov-file
+    },
+
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+        ...globals.jest,
+      },
+
+      parser: tsParser,
+      ecmaVersion: 2020,
+      sourceType: 'module',
+    },
+
+    rules: {
+      'no-console': [
+        'error',
+        {
+          allow: ['warn', 'error', 'info']
+        }
+      ],
+
+      'no-undef': 'off',
+
+      quotes: [
+        'error',
+        'single',
+        {
+          avoidEscape: true,
+          allowTemplateLiterals: false
+        }
+      ],
+
+      'no-restricted-syntax': [
+        'error',
+        {
+          selector: 'TSEnumDeclaration',
+          message: 'Use const assertion or a string union type instead.'
+        }
+      ],
+
+      'func-style': ['error', 'declaration', {allowArrowFunctions: false}],
+
+      '@typescript-eslint/no-duplicate-enum-values': 'off',
+
+      '@typescript-eslint/array-type': [
+        'error',
+        {
+          default: 'generic'
+        }
+      ],
+
+      '@typescript-eslint/naming-convention': [
+        'error',
+        {
+          selector: 'typeParameter',
+          format: ['PascalCase'],
+          custom: {
+            regex: '^T[A-Z]',
+            match: true
+          }
+        },
+        {
+          selector: 'typeAlias',
+          format: ['PascalCase']
+        }
+      ],
+
+      'simple-import-sort/exports': 'error',
+
+      'simple-import-sort/imports': [
+        'error',
+        {
+          groups: [
+            [
+              '^\\u0000',
+              '^node:',
+              '^@?\\w',
+              '^',
+              '^\\./',
+              '^\\.\\./',
+              '^\\.',
+              '^node:.*\\u0000$',
+              '^@?\\w.*\\u0000$',
+              '^[^.].*\\u0000$',
+              '^\\..*\\u0000$'
+            ]
+          ]
+        }
+      ]
+    }
+  }
+];
---
+++ packages/shared-config/prettier.config.mjs
@@ -0,0 +1,13 @@
+/**
+ * @see https://prettier.io/docs/en/configuration.html
+ * @type {import(prettier).Config}
+ */
+const config = {
+  bracketSpacing: true,
+  singleQuote: true,
+  trailingComma: 'none',
+  printWidth: 100,
+  bracketSameLine: true
+};
+
+export default config;
