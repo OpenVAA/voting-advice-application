c5c3e0eefa7c4e0e93595c7d5e8517278a95f4c4
Egor Iakovlev <egor.iakovlev@rizolvit.com>
2024-11-08 14:35:03 +0200

    Use LocalStack for local testing. (#618)

--- backend/vaa-strapi/config/middlewares.ts
+++ backend/vaa-strapi/config/middlewares.ts
@@ -1,7 +1,36 @@
-module.exports = [
+module.exports = ({env}) => [
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
+  /*
+   * To enable AWS S3 support:
+   * https://github.com/strapi/strapi/tree/main/packages/providers/upload-aws-s3
+   **/
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", env('NODE_ENV') === 'development' ? 'http:' : 'https:'],
+          'img-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'market-assets.strapi.io',
+            `${env('STATIC_CONTENT_BASE_URL')}`
+          ],
+          'media-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'market-assets.strapi.io',
+            `${env('STATIC_CONTENT_BASE_URL')}`
+          ],
+          upgradeInsecureRequests: null
+        }
+      }
+    }
+  },
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
