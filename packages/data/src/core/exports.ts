41e46b4c9d95c8449a86a41a41806601e13f2f94
kallelongjuhani <kalle.jarvenpaa@gmail.com>
2024-11-16 10:08:30 +0200

    feat: additional exports from `@openvaa/data`
    Add more exports, which are needed in the frontend.

--- packages/data/src/core/exports.ts
+++ packages/data/src/core/exports.ts
@@ -1,4 +1,6 @@
 export * from './colors.type';
+export * from './dataObject';
+export * from './dataObject.type';
 export * from './error';
 export * from './filter.type';
 export * from './image.type';
