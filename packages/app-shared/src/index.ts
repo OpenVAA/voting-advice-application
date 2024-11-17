3bac3eb9644f79cbdabae9fb7f205815d2ddab8e
kallelongjuhani <kalle.jarvenpaa@gmail.com>
2024-11-17 10:30:26 +0200

    chore: split `settings.type.ts`
    Split the typing into two files for readability.

--- packages/app-shared/src/index.ts
+++ packages/app-shared/src/index.ts
@@ -1,4 +1,5 @@
 export * from './settings/dynamicSettings.js';
-export * from './settings/settings.type.js';
+export * from './settings/dynamicSettings.type.js';
 export * from './settings/staticSettings.js';
+export * from './settings/staticSettings.type.js';
 export * from './utils/passwordValidation.js';
