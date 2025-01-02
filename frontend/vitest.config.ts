c6f1199c23c4530e92eecdb6a0032834b3f8809e
kallelongjuhani <kalle.jarvenpaa@gmail.com>
2025-01-02 09:04:32 +0200

    fix: frontend unit tests
    Run frontend unit tests using a a specified script in the frontend
    package, because otherwise it will:
    - try to run all tests in the repo
    - break `$lib` and other Sveltekit aliases.
    
    This may be due to a misconfiguration of Sveltekit that does not
    properly specify the project root to be `/frontend`.

--- frontend/vitest.config.ts
+++ frontend/vitest.config.ts
@@ -1,18 +1,9 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
-import path from 'path';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   plugins: [svelte({ hot: !process.env.VITEST }), sveltekit()],
-  resolve: {
-    alias: {
-      $lib: path.join(__dirname, './src/lib'),
-      $types: path.join(__dirname, './src/lib/types'),
-      $voter: path.resolve(__dirname, './src/lib/voter'),
-      $candidate: path.resolve(__dirname, './src/lib/candidate')
-    }
-  },
   test: {
     globals: true,
     environment: 'jsdom'
