ad98b2c1d13166c86dd053a4d314017b5d9ced0b
Aleksi Lassila <aleksi.emil.lassila@gmail.com>
2025-01-12 18:11:54 +0200

    feat: add drawer component for question info

---
+++ frontend/src/lib/components/drawer/Drawer.type.ts
@@ -0,0 +1,40 @@
+import type { SvelteHTMLElements } from 'svelte/elements';
+
+export type ExpanderProps = SvelteHTMLElements['div'] & {
+  /**
+   * Title is seen as the text in the expander's visible part, and it is mandatory.
+   * Title will also be used as a 'aria-label' for a checkbow on which the
+   * expander operates on.
+   */
+  title: string;
+  /**
+   * The color of the next-icon that is used in the expander.
+   *
+   * @default 'primary'
+   */
+  iconColor?: Color;
+  /**
+   * The position of the next-icon that is used in the expander.
+   *
+   * @default 'text'
+   */
+  iconPos?: string;
+  /**
+   * Variable with which to configure the expanders title if no variants
+   * are in use.
+   */
+  titleClass?: string;
+  /**
+   * Variable with which to configure the expanders content if no variants
+   * are in use.
+   */
+  contentClass?: string;
+  /**
+   * Variable used to define if the expander is expanded or not by default.
+   */
+  defaultExpanded?: boolean;
+  /**
+   * Variable used to define a variant for the expander.
+   */
+  variant?: 'read-more' | 'category' | 'question' | 'question-help';
+};
