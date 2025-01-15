2768d157e3b7544b06ce6ec1dcd95cd45b57bf38
Aleksi Lassila <aleksi.emil.lassila@gmail.com>
2025-01-15 16:29:51 +0200

    feat: initialize llm backend package

---
+++ packages/llm/src/index.ts
@@ -0,0 +1,8 @@
+/*
+ * Utility exports for this module. We are using blob exports here because
+ * we're using named exports in the folder index files.
+ */
+
+export function testFunction() {
+  return 'hello world';
+}
