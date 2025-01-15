de49b9e1205f2764de644d11def249d7a9f1ab1a
Aleksi Lassila <aleksi.emil.lassila@gmail.com>
2025-01-15 17:44:02 +0200

    feat: example code for using llm backend

--- packages/llm/src/index.ts
+++ packages/llm/src/index.ts
@@ -1,8 +1,2 @@
-/*
- * Utility exports for this module. We are using blob exports here because
- * we're using named exports in the folder index files.
- */
-
-export function testFunction() {
-  return 'hello world';
-}
+export * from './llm-providers/openai-provider';
+export * from './llm-providers/llm-provider';
