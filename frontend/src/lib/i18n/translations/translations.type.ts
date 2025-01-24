e75d7295f194e53e0a8642a67c0d184432aebc18
kallelongjuhani <kalle.jarvenpaa@gmail.com>
2025-01-24 10:06:47 +0200

    chore: remove translations deprecated by the `Input` component

--- frontend/src/lib/i18n/translations/translations.type.ts
+++ frontend/src/lib/i18n/translations/translations.type.ts
@@ -23,6 +23,7 @@ export type TranslationsPayload = Partial<{
   numTotal: number;
   numUnansweredQuestions: number;
   openVAA: string;
+  option: string;
   organization: string;
   partiesTerm: string;
   partyMatchingMethod: AppSettingsGroupMatchingType;
