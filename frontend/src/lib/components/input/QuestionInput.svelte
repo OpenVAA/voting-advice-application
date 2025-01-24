f0dfcb4f64c78415802bab12190b1029325b65c4
kallelongjuhani <kalle.jarvenpaa@gmail.com>
2025-01-24 10:37:01 +0200

    chore: run lint:fix

--- frontend/src/lib/components/input/QuestionInput.svelte
+++ frontend/src/lib/components/input/QuestionInput.svelte
@@ -22,34 +22,42 @@ NB. To show opinion `Question`s, use the components in `$lib/components/question
 -->
 
 <script lang="ts">
-  import type { QuestionInputProps } from './QuestionInput.type';
-  import { Input, type InputProps } from '.';
-  import { ChoiceQuestion, DateQuestion, MultipleChoiceQuestion, QUESTION_TYPE, type QuestionType } from '@openvaa/data';
+  import {
+    ChoiceQuestion,
+    DateQuestion,
+    MultipleChoiceQuestion,
+    QUESTION_TYPE,
+    type QuestionType
+  } from '@openvaa/data';
   import { logDebugError } from '$lib/utils/logger';
+  import { Input, type InputProps } from '.';
+  import type { QuestionInputProps } from './QuestionInput.type';
 
   type $$Props = QuestionInputProps;
 
   export let question: $$Props['question'];
   export let answer: $$Props['answer'] = undefined;
   export let onChange: $$Props['onChange'] = undefined;
-  
+
   // TODO: Implement
-  if (question.type === QUESTION_TYPE.MultipleText) throw new Error(`MultipleTextQuestions are not yet supported by QuestionInput. Question id: ${question.id}.`);
-  if (question instanceof DateQuestion && question.format) logDebugError(`Date formats are not supported yet by QuestionInput. Question id: ${question.id}.`);
+  if (question.type === QUESTION_TYPE.MultipleText)
+    throw new Error(`MultipleTextQuestions are not yet supported by QuestionInput. Question id: ${question.id}.`);
+  if (question instanceof DateQuestion && question.format)
+    logDebugError(`Date formats are not supported yet by QuestionInput. Question id: ${question.id}.`);
 
   ////////////////////////////////////////////////////////////////////
   // Define Input properties
   ////////////////////////////////////////////////////////////////////
 
   const INPUT_TYPES: Record<Exclude<QuestionType, typeof QUESTION_TYPE.MultipleText>, InputProps['type']> = {
-   [QUESTION_TYPE.Text]: 'text',
-   [QUESTION_TYPE.Number]: 'number',
-   [QUESTION_TYPE.Boolean]: 'boolean',
-   [QUESTION_TYPE.Image]: 'image',
-   [QUESTION_TYPE.Date]: 'date',
-   [QUESTION_TYPE.SingleChoiceOrdinal]: 'select',
-   [QUESTION_TYPE.SingleChoiceCategorical]: 'select',
-   [QUESTION_TYPE.MultipleChoiceCategorical]: 'select-multiple',
+    [QUESTION_TYPE.Text]: 'text',
+    [QUESTION_TYPE.Number]: 'number',
+    [QUESTION_TYPE.Boolean]: 'boolean',
+    [QUESTION_TYPE.Image]: 'image',
+    [QUESTION_TYPE.Date]: 'date',
+    [QUESTION_TYPE.SingleChoiceOrdinal]: 'select',
+    [QUESTION_TYPE.SingleChoiceCategorical]: 'select',
+    [QUESTION_TYPE.MultipleChoiceCategorical]: 'select-multiple'
   } as const;
 
   let inputProps: InputProps;
@@ -59,7 +67,14 @@ NB. To show opinion `Question`s, use the components in `$lib/components/question
 
   if (question instanceof ChoiceQuestion) {
     const options = question.choices;
-    inputProps = { type, id, label, info, options, ordered: question instanceof MultipleChoiceQuestion ? true : undefined } as InputProps;
+    inputProps = {
+      type,
+      id,
+      label,
+      info,
+      options,
+      ordered: question instanceof MultipleChoiceQuestion ? true : undefined
+    } as InputProps;
   } else {
     inputProps = { type, id, label, info };
   }
