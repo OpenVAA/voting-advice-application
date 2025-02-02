8baa62e75536bd5b62e99030b437eedda9902e6e
kallelongjuhani <kalle.jarvenpaa@gmail.com>
2025-02-02 10:55:06 +0200

    fix: `Expander` aria label

--- frontend/src/lib/components/expander/Expander.svelte
+++ frontend/src/lib/components/expander/Expander.svelte
@@ -49,6 +49,7 @@ You should not try to use a variant and customize at the same time.
   import { Icon } from '$lib/components/icon';
   import { concatClass } from '$lib/utils/components';
   import type { ExpanderProps } from './Expander.type';
+  import { getComponentContext } from '$lib/contexts/component';
 
   type $$Props = ExpanderProps;
 
@@ -60,6 +61,16 @@ You should not try to use a variant and customize at the same time.
   export let contentClass: $$Props['contentClass'] = '';
   export let defaultExpanded: $$Props['defaultExpanded'] = false;
 
+  ////////////////////////////////////////////////////////////////////
+  // Get contexts
+  ////////////////////////////////////////////////////////////////////
+
+  const { t } = getComponentContext();
+
+  ////////////////////////////////////////////////////////////////////
+  // Handle expansion/collapse
+  ////////////////////////////////////////////////////////////////////
+
   const dispatch = createEventDispatcher<{ expand: null; collapse: null }>();
 
   let expanded = defaultExpanded;
@@ -68,6 +79,10 @@ You should not try to use a variant and customize at the same time.
     expanded = !expanded;
     dispatch(expanded ? 'expand' : 'collapse');
   }
+
+  ////////////////////////////////////////////////////////////////////
+  // Styling
+  ////////////////////////////////////////////////////////////////////
 
   // Build classes
   // 1. Base classes for all collapse components
@@ -116,7 +131,7 @@ You should not try to use a variant and customize at the same time.
 </script>
 
 <div {...concatClass($$restProps, collapseClasses)}>
-  <input type="checkbox" aria-label="open ${title}" on:click={toggleExpanded} checked={expanded} />
+  <input type="checkbox" aria-label={$t('common.expandOrCollapse')} on:click={toggleExpanded} checked={expanded} />
   <div class={titleClasses}>
     {title}
     <div class="not-rotated-icon {expanded ? 'rotated-icon' : ''} ml-[0.4rem] {iconClass}">
