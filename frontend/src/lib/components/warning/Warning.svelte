5e872fa7d0671bb2f2e9083cb837b79092233689
kallelongjuhani <kalle.jarvenpaa@gmail.com>
2025-02-02 13:00:16 +0200

    feat: enable prop forwarding for `Warning`

--- frontend/src/lib/components/warning/Warning.svelte
+++ frontend/src/lib/components/warning/Warning.svelte
@@ -2,39 +2,32 @@
 @component
 A simple warning component to be used with BasicPage
 
-´´
-
 ### Properties
 
-- `display`: A boolean value to set whether to display the warning or not.
+- Any valid attributes of a `div` element.
 
 ### Usage
 
 ```tsx
 <Warning>
-  Warning content, such as text or HTML element.
+  Caveat emptor!
 </Warning>
 ```
 -->
 
 <script lang="ts">
   import { Icon } from '$lib/components/icon';
+  import { concatClass } from '$lib/utils/components';
   import type { WarningProps } from './Warning.type';
 
   type $$Props = WarningProps;
-
-  export let display: $$Props['display'] = true;
 </script>
 
-<div class="text-warning">
-  {#if display}
-    <div class="flex items-start gap-6">
-      <div class="w-60">
-        <Icon name="warning" />
-      </div>
-      <div class="flex-grow text-left">
-        <slot />
-      </div>
-    </div>
-  {/if}
+<div {...concatClass($$restProps, 'flex items-start gap-6 text-warning')}>
+  <div>
+    <Icon name="warning" />
+  </div>
+  <div class="flex-grow text-left">
+    <slot />
+  </div>
 </div>
