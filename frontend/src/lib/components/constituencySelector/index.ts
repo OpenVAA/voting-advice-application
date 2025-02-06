2073a8a7cdf5c0d3361209545f07aa376b0fb3a7
kallelongjuhani <kalle.jarvenpaa@gmail.com>
2025-02-06 20:26:44 +0200

    feat: `ConstituencySelector` component
    Move `Constituency` selection to a reusable component.

---
+++ frontend/src/lib/components/constituencySelector/index.ts
@@ -0,0 +1,4 @@
+export { default as ConstituencySelector } from './ConstituencySelector.svelte';
+export * from './ConstituencySelector.type';
+export { default as SingleGroupConstituencySelector } from './SingleGroupConstituencySelector.svelte';
+export * from './SingleGroupConstituencySelector.type';
--- frontend/src/routes/[[lang=locale]]/(voters)/constituencies/+page.svelte
+++ frontend/src/routes/[[lang=locale]]/(voters)/constituencies/+page.svelte
@@ -10,6 +10,7 @@ See `+page.ts` for possible redirects.
 <script lang="ts">
   import { goto } from '$app/navigation';
   import { Button } from '$lib/components/button';
+  import { ConstituencySelector } from '$lib/components/constituencySelector';
   import { HeroEmoji } from '$lib/components/heroEmoji';
   import { getVoterContext } from '$lib/contexts/voter';
   import MainContent from '../../MainContent.svelte';
@@ -26,13 +27,12 @@ See `+page.ts` for possible redirects.
   ////////////////////////////////////////////////////////////////////
 
   /** The selected constituencyIds per election */
-  const selected: {
+  let selected: {
     [electionId: Id]: Id;
   } = {};
 
   // The selection can be submitted when all elections have a constituency selected.
   let canSubmit = false;
-  $: canSubmit = $elections.length > 0 && $elections.every(({ id }) => selected[id]);
 
   function handleSubmit(): void {
     if (!canSubmit) return;
@@ -53,56 +53,10 @@ See `+page.ts` for possible redirects.
     )}
   </p>
 
-  <div class="mb-md grid gap-lg self-stretch">
-    {#each $elections as election, electionIndex}
-      {@const groups = election.constituencyGroups}
-      <div class="mt-md grid gap-md">
-        {#if $elections.length > 1}
-          <h3 class="relative pl-[2rem]">
-            <span class="circled">{electionIndex + 1}</span>
-            {election.shortName}
-          </h3>
-        {/if}
-        <!-- Use a special list layout if the election has multiple constituency groups -->
-        {#if groups.length > 1}
-          <div>
-            <p class="text-secondary">
-              {$t('constituencies.multipleGroupsInfo')}
-            </p>
-            {#each groups as group, groupIndex}
-              <div class="mb-lg grid gap-sm">
-                {#if group.name}
-                  <h4>{group.name}</h4>
-                {/if}
-                {#if group.info}
-                  <p class="m-0">{group.info}</p>
-                {/if}
-                <select class="select w-full max-w-md place-self-center" bind:value={selected[election.id]}>
-                  <option disabled selected value="">
-                    {$t('constituencies.selectPrompt')}
-                  </option>
-                  {#each group.constituencies as { id, name }}
-                    <option value={id}>{name}</option>
-                  {/each}
-                </select>
-              </div>
-              {#if groupIndex < groups.length - 1}
-                <div class="divider">{$t('common.or')}</div>
-              {/if}
-            {/each}
-          </div>
-          <!-- Use a simple ayout if there is only one constituency group -->
-        {:else}
-          <select class="select w-full max-w-md place-self-center" bind:value={selected[election.id]}>
-            <option disabled selected value="">{$t('constituencies.selectPrompt')}</option>
-            {#each groups[0].constituencies as { id, name }}
-              <option value={id}>{name}</option>
-            {/each}
-          </select>
-        {/if}
-      </div>
-    {/each}
-  </div>
+  <ConstituencySelector
+    elections={$elections}
+    bind:selected
+    bind:selectionComplete={canSubmit}/>
 
   <Button
     slot="primaryActions"
