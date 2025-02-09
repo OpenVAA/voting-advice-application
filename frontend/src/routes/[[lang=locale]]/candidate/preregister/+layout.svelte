8acbe25fff6c3d5977a0f4191656768020416a5c
Rina Shmeleva <rina@mjukhome.com>
2025-02-09 23:46:13 +0200

    Handle userData & idTokenClaims states in layout (draft)

--- frontend/src/routes/[[lang=locale]]/candidate/preregister/+layout.svelte
+++ frontend/src/routes/[[lang=locale]]/candidate/preregister/+layout.svelte
@@ -1,1 +1,45 @@
-<slot />
+<script lang="ts">
+  import { Button } from '$lib/components/button';
+  import { getCandidateContext } from '$lib/contexts/candidate';
+  import { getLayoutContext } from '$lib/contexts/layout';
+  import { goto } from '$app/navigation';
+  import { onDestroy } from 'svelte';
+  import { sanitizeHtml } from '$lib/utils/sanitize';
+  import MainContent from '../../MainContent.svelte';
+
+  ////////////////////////////////////////////////////////////////////
+  // Get contexts
+  ////////////////////////////////////////////////////////////////////
+
+  const { appCustomization, darkMode, getRoute, t, userData } = getCandidateContext();
+  const { pageStyles, topBarSettings } = getLayoutContext(onDestroy);
+
+  ///////////////////////////////////////////////////////////////////
+  // Top bar and styling
+  ////////////////////////////////////////////////////////////////////
+
+  pageStyles.push({ drawer: { background: 'bg-base-300' } });
+  topBarSettings.push({
+    imageSrc: $darkMode
+      ? ($appCustomization.candPoster?.urlDark ?? $appCustomization.candPoster?.url ?? '/images/hero-candidate.png')
+      : ($appCustomization.candPoster?.url ?? '/images/hero-candidate.png')
+  });
+</script>
+
+<svelte:head>
+  <title>{$t('candidateApp.preregister.identification.start.title')} – {$t('dynamic.appName')}</title>
+</svelte:head>
+
+{#if $userData}
+  <MainContent title={$t('candidateApp.preregister.identification.start.title')}>
+    <div class="mb-md text-center text-warning">
+      {@html sanitizeHtml($t('candidateApp.preregister.identification.error.loggedIn.content'))}
+    </div>
+    <Button
+      text={$t('common.continue')}
+      variant="main"
+      on:click={() => goto($getRoute('CandAppHome'), { invalidateAll: true })} />
+  </MainContent>
+{:else}
+  <slot />
+{/if}
