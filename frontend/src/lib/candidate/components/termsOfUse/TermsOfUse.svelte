5b1dccda5871445a071d7071fee1acb57beab4b6
kallelongjuhani <kalle.jarvenpaa@gmail.com>
2025-02-14 22:50:45 +0200

    feat: Candidate App privacy page
    Add page for the Registry and Privacy Statement, which pre-registering
    candidates must accept.
    
    TODO: This should be added for Candidates using the normal registration
    procedure as well.

---
+++ frontend/src/lib/candidate/components/termsOfUse/TermsOfUse.svelte
@@ -0,0 +1,50 @@
+<!--@component
+
+# Candidate app terms of use
+
+A utility component for displaying candidate app terms of use and privacy statement.
+
+
+-->
+
+<script lang="ts">
+  import { getComponentContext } from '$lib/contexts/component';
+  import { sanitizeHtml } from '$lib/utils/sanitize';
+
+  ////////////////////////////////////////////////////////////////////
+  // Get contexts
+  ////////////////////////////////////////////////////////////////////
+
+  const { t } = getComponentContext();
+
+  ////////////////////////////////////////////////////////////////////
+  // Build content
+  ////////////////////////////////////////////////////////////////////
+  
+  let registryStatement: string;
+  let termsOfUse: string;
+
+  $: {
+    const appName = $t('dynamic.appName');
+    registryStatement = $t('dynamic.candidateAppPrivacy.registryStatement.content', {  appName });
+    termsOfUse = $t('dynamic.candidateAppPrivacy.termsOfUse.content', {  appName });
+  }
+</script>
+
+{#if $t('dynamic.candidateAppPrivacy.registryStatement.title') && registryStatement}
+  <section>
+    <h2>{$t('dynamic.candidateAppPrivacy.registryStatement.title')}</h2>
+    <div class="prose">
+      {@html sanitizeHtml(registryStatement)}
+    </div>
+  </section>
+{/if}
+{#if $t('dynamic.candidateAppPrivacy.termsOfUse.title') && termsOfUse}
+  <section>
+    <h2>{$t('dynamic.candidateAppPrivacy.termsOfUse.title')}</h2>
+    <div class="prose">
+      {@html sanitizeHtml(termsOfUse)}
+
+    </div>
+  </section>
+{/if}
