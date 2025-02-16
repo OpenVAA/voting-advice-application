c063a441276b581f1b99c1ecd41b537e490a1cdb
kallelongjuhani <kalle.jarvenpaa@gmail.com>
2025-02-16 11:15:41 +0200

    fix: umami analytics source url

--- frontend/src/lib/components/analytics/umami/UmamiAnalytics.svelte
+++ frontend/src/lib/components/analytics/umami/UmamiAnalytics.svelte
@@ -6,6 +6,7 @@
   type $$Props = UmamiAnalyticsProps;
 
   export let websiteId: $$Props['websiteId'];
+  export let scriptSrc: $$Props['scriptSrc'] = 'https://cloud.umami.is/script.js';
   export const trackEvent: $$Props['trackEvent'] = sendUmamiEvent;
 
   function sendUmamiEvent({ name, data }: TrackingEvent<Record<string, JSONData>>) {
@@ -22,7 +23,7 @@
 <svelte:head>
   <script
     defer
-    src="https://analytics.eu.umami.is/script.js"
+    src={scriptSrc}
     data-website-id={websiteId}
     data-auto-track="true"></script>
 </svelte:head>
