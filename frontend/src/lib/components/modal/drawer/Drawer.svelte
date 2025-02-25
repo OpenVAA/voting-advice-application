c15a794dd0b6354966255a8d6ec939d06cdeef25
Aleksi Lassila <aleksi.emil.lassila@gmail.com>
2025-02-25 15:27:45 +0200

    feat: `Drawer` component and `modalStack` store
    Add `Drawer` component which can be used to display modal, full-screen
    popups. Refactor `Modal` component to accomodate different layouts.
    
    Add `modalStack` store to `AppContext` which is used to queue modal
    popups.

---
+++ frontend/src/lib/components/modal/drawer/Drawer.svelte
@@ -0,0 +1,89 @@
+<!--
+@component
+A modal dialog that looks like a drawer.
+
+### Slots
+
+- default: The content of the modal.
+
+### Properties
+
+- `title`: The title of the modal
+- Any valid properties of a `<ModalContainer>` component.
+
+### Bindable functions
+
+- `openModal`: Opens the modal
+- `closeModal`: Closes the modal
+
+### Events
+
+- `open`: Fired after the modal is opened. Note that the modal may still be transitioning from `hidden`.
+- `close`: Fired when the modal is closed by any means. Note that the modal may still be transitioning to `hidden`.
+- Neither event has any details.
+
+### Accessibility
+
+See the [`<ModalContainer>` component](../ModalContainer.svelte) documentation for more information.
+
+### Usage
+
+```tsx
+<Drawer title="Drawer">
+  <p>Drawer content</p>
+</Drawer>
+```
+-->
+
+<script lang="ts">
+  import { onMount } from 'svelte';
+  import { fly } from 'svelte/transition';
+  import { Button } from '$lib/components/button';
+  import { getComponentContext } from '$lib/contexts/component';
+  import { ModalContainer } from '..';
+  import type { DrawerProps } from './Drawer.type';
+
+  type $$Props = DrawerProps;
+
+  export let title: $$Props['title'];
+  export let isOpen: $$Props['isOpen'] = false;
+  export let closeModal: $$Props['closeModal'] = undefined;
+  export let openModal: $$Props['openModal'] = undefined;
+
+  const { t } = getComponentContext();
+
+  onMount(() => {
+    openModal?.();
+  });
+</script>
+
+<ModalContainer
+  closeOnBackdropClick={true}
+  {...$$restProps}
+  {title}
+  bind:isOpen
+  bind:closeModal
+  bind:openModal
+  on:open
+  on:close>
+  <div
+    class="relative col-span-1 col-start-1 row-span-1 row-start-1 h-[calc(100vh-3rem)] max-w-2xl rounded-t-lg bg-base-100 p-24 pt-40 sm:translate-y-[1rem]"
+    in:fly={{ y: '100%', duration: 200 }}
+    out:fly={{ y: '100%', duration: 200 }}>
+    <h2 class="mb-lg text-center">{title}</h2>
+    <slot />
+    <form method="dialog">
+      <button class="btn btn-circle btn-ghost btn-sm absolute right-2 top-2">
+        <span aria-hidden="true">✕</span>
+        <span class="sr-only">{$t('common.closeDialog')}</span>
+      </button>
+    </form>
+    <Button
+      type="button"
+      variant="floating-icon"
+      text="close"
+      icon="close"
+      on:click={closeModal}
+      class="!absolute bottom-0 right-0" />
+  </div>
+</ModalContainer>
