9f0ef4c3daf9d0f4a0ea25b2c67c22f653b5ceb2
Aleksi Lassila <aleksi.emil.lassila@gmail.com>
2025-03-01 17:16:38 +0200

    feat: add tooltip component

---
+++ frontend/src/lib/components/tooltip/Tooltip.svelte
@@ -0,0 +1,30 @@
+<!--
+@component
+Show a tooltip when hovering over text.
+
+### Properties
+
+- `title`: The title of the tooltip (Optional).
+- `tip`: The text to show in the tooltip.
+
+### Usage
+
+```tsx
+<Tooltip tip="Tip">
+  Hover over me
+</Tooltip>
+```
+-->
+
+<script lang="ts">
+  import type { TooltipProps } from './Tooltip.type';
+
+  type $$Props = TooltipProps;
+
+  export let title: $$Props['title'] = '';
+  export let tip: $$Props['tip'] = '';
+</script>
+
+<span class="tooltip underline decoration-dotted" data-tip={title ? `${title}: ${tip}` : tip}>
+  <slot />
+</span>
