771f813f83b6a01e8c503b72ee833732244e0c79
edthu <127602296+edthu@users.noreply.github.com>
2025-03-06 16:21:19 +0200

    feat: Add example prompts and LLM output template (#689)
    * feat: llm settings to dynamic settings
    
    ---------
    
    Co-authored-by: edthu <eetu.jalava@hotmail.fi>

--- packages/app-shared/src/settings/dynamicSettings.ts
+++ packages/app-shared/src/settings/dynamicSettings.ts
@@ -69,6 +69,66 @@ export const dynamicSettings: DynamicSettings = {
     showElectionTags: true,
     startFromConstituencyGroup: undefined
   },
+  llm: {
+    prompt:
+      '1. Read all the examples of election machine statements below, with which the user can either agree or disagree, as well as the related brief background information. 2. Create a short text of a few sentences for the statement that explains what the current state of the matter presented in the statement is. 4. If the statement contains terms that may be unclear to ordinary people, add a one-sentence explanation for each term. Add them under terms. Add all strings that contain the term or its inflected forms under triggers. 5. Present these texts according to the JSON format below. 6. Return only the JSON-formatted response. Answer only with a valid JSON-formatted response. Do not add formatting to the response. In the case of no ',
+    answerFormat: `
+    {
+      "infoSections": [
+        {
+          "background": {
+            "text": {
+              "en": "Generated background here",
+              "fi": "Generated background here suomeksi",
+              "sv": "Generated background here in swedish"
+            },
+            "title": {
+              "en": "Background"
+            },
+            "visible": true
+          },
+          "situation": {
+            "text": {
+              "en": "Generated situation summary here",
+              "fi": "Generated situation summary here in Finnish",
+              "sv": "Generated situation summary here in Swedish"
+            },
+            "title": {
+              "en": "Current Situation",
+              "fi": "Current Situation",
+              "sv": "Current Situation"
+            },
+            "visible": true
+          },
+          "terms": [
+            {
+              "triggers": [
+                "trigger1", "trigger2", "trigger3"
+              ],
+              "title": "Title",
+              "content": {
+                "en": "Term in english",
+                "fi": "Termi suomeksi",
+                "sv": "Term på svenska"
+              }
+            },
+            {
+              "triggers": [
+                "trigger1", "trigger2", "trigger3"
+              ],
+              "title": "Title",
+              "content": {
+                "en": "Term in english",
+                "fi": "Termi suomeksi",
+                "sv": "Term på svenska"
+              }
+            }
+          ]
+        }
+      ]
+    }
+    `
+  },
   access: {
     candidateApp: true,
     voterApp: true,
