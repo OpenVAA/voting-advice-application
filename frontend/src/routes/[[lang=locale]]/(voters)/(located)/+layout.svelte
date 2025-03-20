910dd1a5b6650a1fa791a60e9630ba727900f942
kallelongjuhani <kalle.jarvenpaa@gmail.com>
2025-03-20 15:03:39 +0200

    fix: no nominations check

--- frontend/src/routes/[[lang=locale]]/(voters)/(located)/+layout.svelte
+++ frontend/src/routes/[[lang=locale]]/(voters)/(located)/+layout.svelte
@@ -25,6 +25,7 @@ Displays a warning if the selected constituency does not have nominations in all
   import { getVoterContext } from '$lib/contexts/voter';
   import { sanitizeHtml } from '$lib/utils/sanitize.js';
   import type { DPDataType } from '$lib/api/base/dataTypes';
+  import { tick } from 'svelte';
 
   export let data;
 
@@ -39,8 +40,8 @@ Displays a warning if the selected constituency does not have nominations in all
     // If data is updated, we want to prevent loading the slot until the promises resolve
     error = undefined;
     ready = false;
-    Promise.all([data.questionData, data.nominationData]).then((data) => {
-      error = update(data);
+    Promise.all([data.questionData, data.nominationData]).then(async (data) => {
+      error = await update(data);
     });
   }
 
@@ -48,10 +49,10 @@ Displays a warning if the selected constituency does not have nominations in all
    * Handle the update inside a function so that we don't track $dataRoot, which would result in an infinite loop.
    * @returns `Error` if the data is invalid, `undefined` otherwise.
    */
-  function update([questionData, nominationData]: [
+  async function update([questionData, nominationData]: [
     DPDataType['questions'] | Error,
     DPDataType['nominations'] | Error
-  ]): Error | undefined {
+  ]): Promise<Error | undefined> {
     if (!isValidResult(questionData, { allowEmpty: true })) return new Error('Error loading question data');
     if (!isValidResult(nominationData, { allowEmpty: true })) return new Error('Error loading nomination data');
     $dataRoot.update(() => {
@@ -59,11 +60,13 @@ Displays a warning if the selected constituency does not have nominations in all
       $dataRoot.provideEntityData(nominationData.entities);
       $dataRoot.provideNominationData(nominationData.nominations);
     });
-    ready = true;
+    // Allow time for the nominationsAvailable store to be updated, which may be delayed on some browsers
+    await tick();
     if (Object.values($nominationsAvailable).every(Boolean)) hasNominations = 'all';
     else if (Object.values($nominationsAvailable).some(Boolean)) hasNominations = 'some';
     else hasNominations = 'none';
     if (hasNominations !== 'all') openModal?.();
+    ready = true;
   }
 </script>
 
