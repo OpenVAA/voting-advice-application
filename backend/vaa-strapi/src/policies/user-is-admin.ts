168df4d55ca5b500df47d82bb159b95cbff1eec2
Edthu <eetu.jalava@hotmail.fi>
2025-03-20 16:02:08 +0200

    feat: user-is-admin policy and extended Strapi request.state.role type StrapiRole

--- backend/vaa-strapi/src/policies/user-is-admin.ts
+++ backend/vaa-strapi/src/policies/user-is-admin.ts
@@ -1,14 +1,16 @@
-import { StrapiContext } from '../../types/customStrapiTypes';
+import { StrapiContext } from '../../types/customStrapiTypes'
+import { warn } from '../util/logger';
 
 /**
  * Policy that only allows admin users to use LLM functions.
+ * NB! Admin role is different from Strapi admin role
  */
 export default function isAdmin(ctx: StrapiContext): boolean {
-  const role = ctx.state.user.role;
   const userIsAuthenticated = ctx.state.isAuthenticated;
-
-  if (role == 'admin' && userIsAuthenticated) {
+  const role = ctx.state.user.role.type;
+  if (role === 'admin' && userIsAuthenticated) {
     return true;
   }
+  warn('[global::user-is-admin] triggered by:', ctx.request);
   return false;
 }
