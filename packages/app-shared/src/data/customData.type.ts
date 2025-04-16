a29dcccab36d2427ec226858275f7897d78c1009
0xdaali <danielwilenius35@gmail.com>
2025-04-16 12:38:27 +0300

    Remove conflict

--- packages/app-shared/src/data/customData.type.ts
+++ packages/app-shared/src/data/customData.type.ts
@@ -53,20 +53,6 @@ export type QuestionInfoSection = {
  */
 export type TermDefinition = {
   /** The strings that trigger the popup. There may in theory be many different forms in the same question. */
-  triggers: Array<string>;
-
-  /** Title of the term explanation (the term) */
-  title?: string;
-
-  /** Term explanation */
-  content: string;
-};
-
-/**
- * The properties for defining term definitions in customData
- */
-export type TermDefinition = {
-  /** The strings that trigger the popup. There may in theory be many different forms in the same question. */
   triggers?: Array<string>;
 
   /** Title of the term explanation (the term) */
