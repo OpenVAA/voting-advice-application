0800bb8115636ae38296c1540fa0423b4e8a8f4b
samilaa <samilaakkonen2@gmail.com>
2025-04-26 22:19:55 +0300

    remove card component

--- frontend/src/lib/components/card/Card.type.ts
+++
@@ -1,8 +0,0 @@
-import type { SvelteHTMLElements } from 'svelte/elements';
-
-export type CardProps = SvelteHTMLElements['div'] & {
-  /**
-   * Additional classes to apply to the card.
-   */
-  class?: string;
-};
