8face35481e7edcd2a4d486e68fe6cdd69ece51a
maximusvitutus <max.leo.lehtinen@gmail.com>
2025-06-09 15:40:33 +0300

    export metaevaluation types through an index.ts file

---
+++ packages/argument-condensation/new_src/evaluation/metaEvaluation/types/index.ts
@@ -0,0 +1,4 @@
+// Export all meta-evaluation types
+export type { MetaEvaluationCase } from './testCase';
+export type { MetaEvaluationDataset } from './testInput';
+export type { MetaEvaluationResult } from './testOutput';
