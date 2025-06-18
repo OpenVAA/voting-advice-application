c63c542f2f884270f51ad586755a751ba17f3b3c
maximusvitutus <max.leo.lehtinen@gmail.com>
2025-06-18 23:53:24 +0300

    create interface for VAA question

---
+++ packages/argument-condensation/new_src/core/types/vaaQuestion.ts
@@ -0,0 +1,17 @@
+import { VAAComment } from "./condensationInput";
+
+/**
+ * Represents a question from the VAA.
+ * @param id - The unique identifier for the question
+ * @param topic - The topic of the question
+ * @param electionId - The unique identifier for the election
+ * @param answerType - The type of answer for the question
+ * @param comments - The comments for the question
+ */
+export interface VAAQuestion {
+  id: string;
+  topic: string;
+  electionId: string;
+  answerType: string | number;
+  comments: VAAComment[];
+}
