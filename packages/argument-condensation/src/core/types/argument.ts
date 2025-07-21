cc30bbc5c062c70641f58dbb433aacff5f4e3c1d
maximusvitutus <max.leo.lehtinen@gmail.com>
2025-07-21 16:49:09 +0300

    remove old src
    it has been deprecated for a long while now

--- packages/argument-condensation/src/core/types/argument.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Represents a condensed argument extracted from an array of comments.
- * @interface Argument
- */
-export interface Argument {
-  /** The condensed argument */
-  argument: string;
-}
--- packages/argument-condensation/src/core/types/condensationType.ts
+++
@@ -1,7 +0,0 @@
-export const CONDENSATION_TYPE = {
-  General: 'general',
-  Supporting: 'supporting',
-  Opposing: 'opposing'
-} as const;
-
-export type CondensationType = (typeof CONDENSATION_TYPE)[keyof typeof CONDENSATION_TYPE];
