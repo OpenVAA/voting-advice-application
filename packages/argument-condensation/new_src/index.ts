871df3319803b0d4b02513bf0fd71391c05cff50
maximusvitutus <max.leo.lehtinen@gmail.com>
2025-07-29 18:51:19 +0300

    feat[arg-cond]: simply export everything

--- packages/argument-condensation/new_src/index.ts
+++ packages/argument-condensation/new_src/index.ts
@@ -1,9 +1,5 @@
 // Export main functionality
-export { Condenser } from './core/condensation/condenser';
-
-// Export types
-export type { CondensationRunInput, CondensationRunResult } from './core/types';
-export type { Argument } from './core/types/base/argument';
-export type { VAAComment } from './core/types/condensation/condensationInput';
-export type { CondensationOutputType } from './core/types/condensation/condensationType';
-export { CONDENSATION_TYPE } from './core/types/condensation/condensationType';
+export { Condenser } from './core/condensation/condenser'; // Main condensation engine class
+export * from './core/main.ts' // API 
+export * from './core/types'; 
+export * from './core/utils'; 
