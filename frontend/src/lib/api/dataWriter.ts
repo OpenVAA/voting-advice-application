9215fd0e761c5dfaa86d0f203bbf230fc38d70c0
kallelongjuhani <kalle.jarvenpaa@gmail.com>
2025-07-30 09:33:43 +0300

    fix: unsupported adapter error when using `local` adapter
    Only throw an error when attempting to use an unsupported `DataWriter`,
    not immediately upon module import.
    
    This is needed to prevent errors when using the `local` data adapter,
    even when only accessing the Voter App, because it conditionally uses the
    `CandidateContext`. Due to the import logic, the context module is
    imported and `$lib/api/dataWriter` along with it, even though the
    context is only instantiated when accessing the Candidate App, which the
    `local` adapter does not support.

--- frontend/src/lib/api/dataWriter.ts
+++ frontend/src/lib/api/dataWriter.ts
@@ -1,4 +1,5 @@
 import { staticSettings } from '@openvaa/app-shared';
+import { logDebugError } from '$lib/utils/logger';
 import type { UniversalDataWriter } from './base/universalDataWriter';
 
 let module: Promise<{ dataWriter: UniversalDataWriter }>;
@@ -10,7 +11,12 @@ switch (type) {
     module = import('./adapters/strapi/dataWriter');
     break;
   default:
-    throw new Error(`Unsupported data writer: ${type}`);
+    module = new Promise(() => {
+      logDebugError(
+        `DataWriter imported when using an unsupported data adapter (${type}). Any attempts to use the dataWriter will fail.`
+      );
+      return { dataWriter: undefined };
+    });
 }
 
 export const dataWriter = module.then((m) => m.dataWriter);
