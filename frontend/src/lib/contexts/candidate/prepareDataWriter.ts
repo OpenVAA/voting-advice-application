c6625b0c243b690c257bae3ae58c87220b6d25bc
kallelongjuhani <kalle.jarvenpaa@gmail.com>
2025-07-30 09:33:43 +0300

    fix: unsupported adapter error when using `local` adapter
    Only throw an error when attempting to use an unsupported `DataWriter`,
    not immediately upon module import.
    
    This is needed to prevent errors when using the `local` data adapter,
    even when only accessing the Voter App, because it conditionally uses the
    `CandidateContext`. Due to the import logic, the context module is
    imported and `$lib/api/dataWriter` along with it, even though the
    context is only instantiated when accessing the Candidate App, which the
    `local` adapter does not support.

--- frontend/src/lib/contexts/candidate/prepareDataWriter.ts
+++ frontend/src/lib/contexts/candidate/prepareDataWriter.ts
@@ -1,3 +1,4 @@
+import { staticSettings } from '@openvaa/app-shared';
 import { browser } from '$app/environment';
 import type { UniversalDataWriter } from '$lib/api/base/universalDataWriter';
 
@@ -7,6 +8,10 @@ import type { UniversalDataWriter } from '$lib/api/base/universalDataWriter';
 export async function prepareDataWriter(dataWriterPromise: Promise<UniversalDataWriter>): Promise<UniversalDataWriter> {
   if (!browser) throw new Error('DataWriter methods in CandidateContext can only be called in a browser environment');
   const dataWriter = await dataWriterPromise;
+  if (!dataWriter)
+    throw new Error(
+      `Failed to initialize DataWriter. Perhaps the adapter (${staticSettings.dataAdapter.type}) does not support dataWriter?`
+    );
   dataWriter.init({ fetch });
   return dataWriter;
 }
