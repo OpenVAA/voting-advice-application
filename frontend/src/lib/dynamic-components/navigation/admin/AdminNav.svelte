44e85612bf3089e4c92879729cbd6ce0481234f0
kallelongjuhani <kalle.jarvenpaa@gmail.com>
2025-08-13 11:12:04 +0300

    feat: Admin App
    Add a scaffold for the Admin App on the `/admin` route.
    
    Add `AdminContext` for use with the app.
    
    Reorganize common translations used in both Candidate and Admin Apps.
    
    Within the Admin App, add an incomplete draft of the argument
    condensation function.
    
    Add preliminary translations for some parts of the Admin App, which are
    not yet available.

---
+++ frontend/src/lib/dynamic-components/navigation/admin/AdminNav.svelte
@@ -0,0 +1,59 @@
+<!--
+@component
+A template part that outputs the navigation menu for the Admin App for use in `Layout`.
+
+### Dynamic component
+
+- Accesses the `AdminContext` in the future.
+
+### Properties
+
+- Any valid properties of a `Navigation` component.
+
+### Usage
+
+```tsx
+<AdminNav>
+  <NavItem slot="close" on:click={closeMenu} icon="close" text="Close"/>
+</AdminNav>
+```
+-->
+
+<script lang="ts">
+  import { onDestroy } from 'svelte';
+  import { getAppContext } from '$lib/contexts/app';
+  import { getLayoutContext } from '$lib/contexts/layout';
+  import { NavGroup, Navigation, NavItem } from '$lib/dynamic-components/navigation';
+  import { LanguageSelection } from '../languages';
+  import { getAdminContext } from '$lib/contexts/admin';
+
+  const { navigation } = getLayoutContext(onDestroy);
+  const { authToken, t, getRoute } = getAdminContext();
+</script>
+
+<Navigation slot="nav" on:navFocusOut {...$$restProps}>
+  <NavItem
+    on:click={navigation.close}
+    icon="close"
+    text={$t('common.closeMenu')}
+    class="pt-16"
+    id="drawerCloseButton" />
+
+  {#if $authToken}
+    <NavGroup>
+      <NavItem href={$getRoute('AdminAppHome')} icon="home" text={$t('adminApp.common.home')} />
+      <NavItem href={$getRoute('AdminAppFactorAnalysis')} icon="create" text={$t('adminApp.factorAnalysis.title')} />
+      <NavItem href={$getRoute('AdminAppQuestionInfo')} icon="create" text={$t('adminApp.questionInfo.title')} />
+      <NavItem
+        href={$getRoute('AdminAppArgumentCondensation')}
+        icon="create"
+        text={$t('adminApp.argumentCondensation.title')} />
+    </NavGroup>
+  {:else}
+    <NavGroup>
+      <NavItem href={$getRoute('AdminAppLogin')} icon="login" text={$t('common.login')} />
+    </NavGroup>
+  {/if}
+
+  <LanguageSelection />
+</Navigation>
---
+++ frontend/src/routes/[[lang=locale]]/admin/(protected)/+page.svelte
@@ -0,0 +1,39 @@
+<!--@component
+# Admin Home Page
+
+Main landing page for admin section showing available tools
+-->
+
+<script lang="ts">
+  import { Button } from '$lib/components/button';
+  import { getAppContext } from '$lib/contexts/app';
+  import MainContent from '../../MainContent.svelte';
+
+  const { t, getRoute } = getAppContext();
+</script>
+
+<MainContent title={$t('adminApp.common.home')}>
+  <p class="text-center">{$t('adminApp.common.description')}</p>
+
+  <div class="flex flex-col gap-sm">
+    <Button
+      href={$getRoute('AdminAppArgumentCondensation')}
+      text={$t('adminApp.argumentCondensation.title')}
+      icon="create"
+      iconPos="left" />
+
+    <Button
+      href={$getRoute('AdminAppFactorAnalysis')}
+      text={$t('adminApp.factorAnalysis.title')}
+      icon="create"
+      iconPos="left"
+      disabled />
+
+    <Button
+      href={$getRoute('AdminAppQuestionInfo')}
+      text={$t('adminApp.questionInfo.title')}
+      icon="create"
+      iconPos="left"
+      disabled />
+  </div>
+</MainContent>
