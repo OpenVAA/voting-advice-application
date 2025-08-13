17f8720fa5cc9c5d8c74e1215620de0b4f7443d5
kallelongjuhani <kalle.jarvenpaa@gmail.com>
2025-08-13 11:10:04 +0300

    refactor: `CandidateLoginError` messages

--- frontend/src/lib/candidate/utils/loginError.ts
+++ frontend/src/lib/candidate/utils/loginError.ts
@@ -3,14 +3,14 @@ import type { TranslationKey } from '$types';
 /**
  * Get the translation key for the given error code.
  */
-export function getErrorTranslationKey(error: CandidateLoginError | string | null): TranslationKey | undefined {
+export function getErrorTranslationKey(error: CandidateLoginError | null): TranslationKey | undefined {
   if (error == null || !(error in CANDIDATE_LOGIN_ERROR)) return undefined;
   return CANDIDATE_LOGIN_ERROR[error];
 }
 
-const CANDIDATE_LOGIN_ERROR: Record<string, TranslationKey> = {
+const CANDIDATE_LOGIN_ERROR: Record<CandidateLoginError, TranslationKey> = {
   candidateNoNomination: 'candidateApp.error.candidateNoNomination',
-  loginFailed: 'candidateApp.error.loginFailed',
+  loginFailed: 'error.loginFailed',
   nominationNoElection: 'candidateApp.error.nominationNoElection',
   userNoCandidate: 'candidateApp.error.userNoCandidate',
   userNotAuthorized: 'error.403'
@@ -19,4 +19,9 @@ const CANDIDATE_LOGIN_ERROR: Record<string, TranslationKey> = {
 /**
  * The allowed error codes for candidate login to be displayed on the login page. These are subkeys of `candidateApp.error.` translations.
  */
-export type CandidateLoginError = keyof typeof CANDIDATE_LOGIN_ERROR;
+export type CandidateLoginError =
+  | 'candidateNoNomination'
+  | 'loginFailed'
+  | 'nominationNoElection'
+  | 'userNoCandidate'
+  | 'userNotAuthorized';
