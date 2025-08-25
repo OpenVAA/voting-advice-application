a551e45d1ae5dd9b34673df11035a2677dd5c0f9
kallelongjuhani <kalle.jarvenpaa@gmail.com>
2025-08-25 14:14:26 +0300

    chore: fix formatting

--- frontend/src/routes/[[lang=locale]]/candidate/login/+page.svelte
+++ frontend/src/routes/[[lang=locale]]/candidate/login/+page.svelte
@@ -25,7 +25,7 @@
   import { applyAction, enhance } from '$app/forms';
   import { goto } from '$app/navigation';
   import { page } from '$app/stores';
-  import { type CandidateLoginError,getErrorTranslationKey } from '$candidate/utils/loginError';
+  import { type CandidateLoginError, getErrorTranslationKey } from '$candidate/utils/loginError';
   import { PasswordField } from '$lib/candidate/components/passwordField';
   import { Button } from '$lib/components/button';
   import { ErrorMessage } from '$lib/components/errorMessage';
