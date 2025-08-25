e0121d64941da5a1299f750ca04757f5cd1fb9ac
kallelongjuhani <kalle.jarvenpaa@gmail.com>
2025-08-25 14:09:46 +0300

    refactor: move client-side jobStores to `AdminContext`

--- frontend/src/lib/contexts/admin/adminContext.type.ts
+++ frontend/src/lib/contexts/admin/adminContext.type.ts
@@ -2,6 +2,7 @@ import type { Writable } from 'svelte/store';
 import type { BasicUserData } from '$lib/api/base/dataWriter.type';
 import type { AppContext } from '../app';
 import type { AuthContext } from '../auth';
+import type { JobStores } from './jobStores.type';
 
 export type AdminContext = AppContext &
   AuthContext & {
@@ -9,4 +10,8 @@ export type AdminContext = AppContext &
      * Store for user data
      */
     userData: Writable<BasicUserData | undefined>;
+    /**
+     * Stores and utilities for handling admin jobs.
+     */
+    jobs: JobStores;
   };
