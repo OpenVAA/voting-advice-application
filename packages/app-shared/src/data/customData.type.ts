63e65dc08065bd4c1f290c654382026a28caa8bc
kallelongjuhani <kalle.jarvenpaa@gmail.com>
2025-09-01 14:47:32 +0300

    feat: remove `InfoSection.visible` custom data property
    The `visible` property is not really used.

--- packages/app-shared/src/data/customData.type.ts
+++ packages/app-shared/src/data/customData.type.ts
@@ -163,10 +163,6 @@ export type QuestionInfoSection = {
    * The (html) content of the section.
    */
   content: string;
-  /**
-   * Whether the section should be visible.
-   */
-  visible: boolean;
 };
 
 /**
