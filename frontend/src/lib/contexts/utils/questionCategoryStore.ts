088e82894cb87a1687820661f004eb3e3033a4ea
kallelongjuhani <kalle.jarvenpaa@gmail.com>
2025-09-16 18:56:27 +0300

    fix: filter questions by entity type in Candidate App
    Filter questions inside `CandidateContext` to show only the ones
    applicable to candidates.
    
    Note that all questions are still fetched from the backend by the
    `DataProvider`, which currently does not support filtering by entity
    type.

--- frontend/src/lib/contexts/utils/questionCategoryStore.ts
+++ frontend/src/lib/contexts/utils/questionCategoryStore.ts
@@ -2,6 +2,8 @@ import {
   type Constituency,
   type DataRoot,
   type Election,
+  type EntityType,
+  type FilterValue,
   QUESTION_CATEGORY_TYPE,
   type QuestionCategory
 } from '@openvaa/data';
@@ -9,24 +11,26 @@ import { parsimoniusDerived } from './parsimoniusDerived';
 import type { Readable } from 'svelte/store';
 
 /**
- * Create a derived store containing all `QuestionCategory`s that apply to the selected elections and constituencies.
+ * Create a derived store containing all `QuestionCategory`s that apply to the selected elections and constituencies as well as possible entity types.
  */
 export function questionCategoryStore({
   dataRoot,
   selectedElections,
-  selectedConstituencies
+  selectedConstituencies,
+  entityType
 }: {
   dataRoot: Readable<DataRoot>;
   selectedElections: Readable<Array<Election>>;
   selectedConstituencies: Readable<Array<Constituency>>;
+  entityType?: FilterValue<EntityType>;
 }): Readable<Array<QuestionCategory>> {
   return parsimoniusDerived(
     [dataRoot, selectedElections, selectedConstituencies],
     ([dataRoot, elections, constituencies]) =>
       dataRoot.questionCategories?.filter(
         (c) =>
-          c.appliesTo({ elections, constituencies }) &&
-          c.getApplicableQuestions({ elections, constituencies }).length > 0
+          c.appliesTo({ elections, constituencies, entityType }) &&
+          c.getApplicableQuestions({ elections, constituencies, entityType }).length > 0
       ) ?? [],
     { initialValue: [] }
   );
