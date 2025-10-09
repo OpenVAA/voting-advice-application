d21258fac2af70b8008a77c90f6cac21ffb5eac0
maximusvitutus <max.leo.lehtinen@gmail.com>
2025-10-09 15:50:09 +0300

    feat[vector-store]: add exports

---
+++ packages/vector-store/src/index.ts
@@ -0,0 +1,13 @@
+// Core vector store exports
+export { ChromaVectorStore } from './core/chromaVectorStore';
+export { devEmbedder, OpenAIEmbedder } from './core/openAIEmbedder';
+export { CharacterSegmenter } from './core/segmenters';
+
+// Types
+export type { Embedder, EmbedderOptions, EmbedderResponse } from './core/embedder.type';
+export type { SearchResult, TextSegment, VectorStoreConfig } from './core/vectorStore.type';
+export type { SourceDocument } from './types/sourceDocument';
+
+// Base class
+export { Embedder as EmbedderBase } from './core/embedder.type';
+export { VectorStore } from './core/vectorStore.type';
