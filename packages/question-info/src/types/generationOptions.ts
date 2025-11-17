b2bffb746aa9bb606848ab397dba59d563c2b8eb
maximusvitutus <max.leo.lehtinen@gmail.com>
2025-11-17 11:59:04 -0500

    feat[llm]: add output language as a common param

--- packages/question-info/src/types/generationOptions.ts
+++ packages/question-info/src/types/generationOptions.ts
@@ -50,9 +50,6 @@ export interface QuestionInfoOptions extends CommonLLMParams {
   /** Which operations to perform. Can contain either or both of the operations */
   operations: Array<QuestionInfoOperation>;
 
-  /** Language of the prompt to use */
-  language: string;
-
   /** Context to help guide generation. Should contain at least basic details: country and basic election context.
    * Good context increases quality drastically. */
   questionContext?: string;
