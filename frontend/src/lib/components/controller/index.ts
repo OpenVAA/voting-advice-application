8ef3383b0ee29377bfd6232a25ef4db74782b28a
kallelongjuhani <kalle.jarvenpaa@gmail.com>
2025-11-30 13:02:34 +0200

    refactor: handle `abortJob` within `JobDetails`
    Remove the `onAbort` handlers from the job details component chain.

---
+++ frontend/src/lib/components/controller/index.ts
@@ -0,0 +1,3 @@
+export { default as InfoMessages } from './InfoMessages.svelte';
+export { default as ProgressBar } from './ProgressBar.svelte';
+export { default as WarningMessages } from './WarningMessages.svelte';
