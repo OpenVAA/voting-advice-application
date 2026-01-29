849f7a6210d70ffbc16600141b32ae6297f230c9
kallelongjuhani <kalle.jarvenpaa@gmail.com>
2026-01-29 09:57:27 +0200

    feat[docs]: mobile navigation
    Add full mobile navigation for `docs`.
    
    Remove `fixed` from `Header`.

--- docs/src/lib/components/Header.svelte
+++ docs/src/lib/components/Header.svelte
@@ -5,13 +5,14 @@
   import { getFirstChild } from '$lib/utils/navigation';
   import GithubIcon from '$lib/components/GithubIcon.svelte';
   import { OpenVAALogo } from '$lib/components/openVAALogo';
+  import { DRAWER_ID } from '../consts';
 
   function isActiveSection(section: NavigationSection): boolean {
     return page.url.pathname.startsWith(section.route);
   }
 </script>
 
-<header class="fixed top-0 right-0 left-0 z-50 h-(--spacing-headerHeight) max-h-(--spacing-headerHeight) bg-base-300">
+<header class="h-(--spacing-headerHeight) max-h-(--spacing-headerHeight) bg-base-300">
   <div class="navbar px-lg">
     <div class="navbar-start">
       <a href="/">
@@ -35,24 +36,16 @@
     </div>
     <div class="navbar-end hidden justify-end md:flex"><GithubIcon /></div>
 
-    <!-- Mobile menu button -->
-    <div class="navbar-end md:hidden">
-      <details class="dropdown dropdown-end">
-        <summary class="btn btn-circle btn-ghost" aria-label="Open menu">
+    <!-- Mobile menu -->
+    <div class="navbar-end gap-md md:hidden">
+      <GithubIcon />
+      <div class="flex-none lg:hidden">
+        <label for={DRAWER_ID} aria-label="open sidebar" class="btn btn-circle btn-ghost">
           <svg class="h-24 w-24" fill="none" stroke="currentColor" viewBox="0 0 24 24">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
           </svg>
-        </summary>
-        <ul
-          class="dropdown-content menu mt-md flex w-max max-w-[80vw] flex-col gap-md rounded-md bg-base-100 p-2 py-lg text-lg shadow-lg">
-          {#each navigation as section}
-            {@const isActive = isActiveSection(section)}
-            {@const firstChild = getFirstChild(section)}
-            <li><a href={firstChild} class="flex justify-end">{section.title}</a></li>
-          {/each}
-          <li class="flex justify-end"><GithubIcon /></li>
-        </ul>
-      </details>
+        </label>
+      </div>
     </div>
   </div>
 </header>
